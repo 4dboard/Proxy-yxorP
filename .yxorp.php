8bd76934d2ca53753005d4806f5942371f442d0a
don Pablo <don@obeyi.com>
2022-04-02 07:12:32 +0200

    update

--- .yxorp.php
+++ .yxorp.php
@@ -3,6 +3,9 @@
 use Bugsnag\Client;
 use Bugsnag\Handler;
 use yxorp\Http;
+use yxorP\Http\ProxyEvent;
+use yxorP\Http\Request;
+use yxorP\Http\Response;
 
 require (@$GLOBALS['PLUGIN_DIR'] = __DIR__) . '/cache/Cache.php';
 require @$GLOBALS['PLUGIN_DIR'] . '/guzzle.phar';
@@ -17,6 +20,14 @@ header("Access-Control-Allow-Origin: *");
 
 class yxorp
 {
+
+    private $request;
+
+    private bool $output_buffering = true;
+    private string $output_buffer = '';
+
+    private array $listeners = array();
+
     public function __construct($TARGET_URL)
     {
         @ini_set('default_charset', 'utf-8');
@@ -24,16 +35,20 @@ class yxorp
         @$GLOBALS['SITE_HOST'] = @parse_url(@$GLOBALS['SITE_URL'] = 'https://' . @$_SERVER['HTTP_HOST'], PHP_URL_HOST);
         @$GLOBALS['TARGET_HOST'] = @parse_url(@$GLOBALS['TARGET_URL'] = @$TARGET_URL, PHP_URL_HOST);
         @$GLOBALS['CACHE_KEY'] = @base64_encode(@$GLOBALS['REQUEST_URI'] = @$_SERVER['REQUEST_URI']);
+
         if (!file_exists(@$GLOBALS['CACHE_DIR'] = @$GLOBALS['PLUGIN_DIR'] . '/.cache/'))
             mkdir($concurrentDirectory = @$GLOBALS['CACHE_DIR'], 0777, true);
+
         $_types = array('txt' => 'text/plain', 'htm' => 'text/html', 'html' => 'text/html', 'php' => 'text/html', 'css' => 'text/css', 'js' => 'application/javascript', 'json' => 'application/json', 'xml' => 'application/xml', 'swf' => 'application/x-shockwave-flash', 'flv' => 'video/x-flv', 'png' => 'image/png', 'jpe' => 'image/jpeg', 'jpeg' => 'image/jpeg', 'jpg' => 'image/jpeg', 'gif' => 'image/gif', 'bmp' => 'image/bmp', 'ico' => 'image/vnd.microsoft.icon', 'tiff' => 'image/tiff', 'tif' => 'image/tiff', 'svg' => 'image/svg+xml', 'svgz' => 'image/svg+xml', 'zip' => 'application/zip', 'rar' => 'application/x-rar-compressed', 'exe' => 'application/x-msdownload', 'msi' => 'application/x-msdownload', 'cab' => 'application/vnd.ms-cab-compressed', 'mp3' => 'audio/mpeg', 'qt' => 'video/quicktime', 'mov' => 'video/quicktime', 'pdf' => 'application/pdf', 'psd' => 'image/vnd.adobe.photoshop', 'ai' => 'application/postscript', 'eps' => 'application/postscript', 'ps' => 'application/postscript', 'doc' => 'application/msword', 'rtf' => 'application/rtf', 'xls' => 'application/vnd.ms-excel', 'ppt' => 'application/vnd.ms-powerpoint', 'odt' => 'application/vnd.oasis.opendocument.text', 'ods' => 'application/vnd.oasis.opendocument.spreadsheet');
         $_ext = @pathinfo(@strtok(@$GLOBALS['REQUEST_URI'], '?'), PATHINFO_EXTENSION);
+
         @$GLOBALS['MIME'] = (@array_key_exists(@$_ext, @$_types)) ? @$_types[@$_ext] : 'text/html';
         @header('Content-Type: ' . @$GLOBALS['MIME'] . '; charset=UTF-8');
         @$GLOBALS['CACHE_ADAPTER'] = new yxorP\cache\Cache();
+
         if (@$_GET["DONCLEAR"] !== null) @$GLOBALS['CACHE_ADAPTER']->clean();
         if (!(@$GLOBALS['CACHE_ADAPTER'])->isExisting(@$GLOBALS['CACHE_KEY'])) $this->FETCH();
-            else echo @$GLOBALS['CACHE_ADAPTER']->get(@$GLOBALS['CACHE_KEY']);
+        else echo @$GLOBALS['CACHE_ADAPTER']->get(@$GLOBALS['CACHE_KEY']);
 
     }
 
@@ -47,34 +62,33 @@ class yxorp
         $this->FILES_CHECK(@$GLOBALS['OVERRIDE_DIR'] . '/assets', false);
         $this->FILES_CHECK(@$GLOBALS['PLUGIN_DIR'] . '/override/default/assets', false);
 
-
         try {
+
             foreach (@file(@$GLOBALS['PLUGIN_DIR'] . '/.env') as $line) {
-                if (trim(stripos(trim(@$line), '#') === 0)) continue;
+                if (@trim(@strpos(@trim(@$line), '#') === 0)) continue;
                 [$name, $value] = explode('=', @$line, 2);
                 @$GLOBALS[$name] = @$value;
             }
+
             Handler::register(@$GLOBALS['BUGSNAG'] = Client::make(@$GLOBALS['BUG_SNAG_KEY']));
 
-            foreach ((array)json_decode(file_get_contents(@$GLOBALS['OVERRIDE_DIR'].'/overrides.json'),
+            foreach ((array)@json_decode(@file_get_contents(@$GLOBALS['OVERRIDE_DIR'] . '/overrides.json'),
                 false, 512, JSON_THROW_ON_ERROR) as $key => $value) @$GLOBALS[$key] = @$value;
 
             foreach (array('/helper', '/http') as $_asset) $this->FILES_CHECK(@$GLOBALS['PLUGIN_DIR'] . $_asset, true);
 
-
-            $proxy = new Http\Punisher();
-
             foreach (@$GLOBALS['PLUGINS'] as $plugin) {
-                if (file_exists(@$GLOBALS['PLUGIN_DIR'] . '/plugin/' . @$plugin . '.php')) require(@$GLOBALS['PLUGIN_DIR'] . '/plugin/' . @$plugin . '.php');
-                    elseif (class_exists('\\yxorP\\plugin\\' . @$plugin)) @$plugin = '\\yxorP\\plugin\\' . @$plugin;
-                $proxy->addSubscriber(new $plugin());
+                if (@file_exists(@$GLOBALS['PLUGIN_DIR'] . '/plugin/' . @$plugin . '.php')) require(@$GLOBALS['PLUGIN_DIR'] . '/plugin/' . @$plugin . '.php');
+                elseif (@class_exists('\\yxorP\\plugin\\' . @$plugin)) @$plugin = '\\yxorP\\plugin\\' . @$plugin;
+                $this->addSubscriber(new $plugin());
             }
 
-            echo $_content = @$proxy->forward(Http\Request::createFromGlobals(), @$GLOBALS['PROXY_URL'] = @$GLOBALS['TARGET_URL'] . @$GLOBALS['REQUEST_URI'] = @$_SERVER['REQUEST_URI'])->getContent();
-            @$GLOBALS['CACHE_ADAPTER']->set(@$GLOBALS['CACHE_KEY'], preg_replace_callback('(<p>(.*?)</p>)', function ($m) {
-                    return '<p>' . str_replace(fgetcsv(fopen(@$GLOBALS['PLUGIN_DIR'] . '/override/default/search_rewrite.csv', 'rb')),
-                            fgetcsv(fopen(@$GLOBALS['PLUGIN_DIR'] . '/override/default/replace_rewrite.csv', 'rb')), @$m[1]);
-                }, @$_content) . '</p>', @$GLOBALS['CACHE_TIME'] = time() + (60 * 60 * 24 * 31));
+            echo $_content = @$this->forward(Http\Request::createFromGlobals(), @$GLOBALS['PROXY_URL'] = @$GLOBALS['TARGET_URL'] . @$GLOBALS['REQUEST_URI'] = @$_SERVER['REQUEST_URI'])->getContent();
+            @$GLOBALS['CACHE_ADAPTER']->set(@$GLOBALS['CACHE_KEY'], @preg_replace_callback('(<p>(.*?)</p>)', function ($m) {
+                    return '<p>' . @str_replace(fgetcsv(fopen(@$GLOBALS['PLUGIN_DIR'] . '/override/default/search_rewrite.csv', 'rb')),
+                            @fgetcsv(@fopen(@$GLOBALS['PLUGIN_DIR'] . '/override/default/replace_rewrite.csv', 'rb')), @$m[1]);
+                }, @$_content) . '</p>', @$GLOBALS['CACHE_TIME'] = @time() + (60 * 60 * 24 * 31));
+
         } catch (exception $e) {
             @$GLOBALS['BUGSNAG']->notifyException(@$e->__toString());
             if (@$GLOBALS['MIME'] !== 'text/html') @header("Location: " . @$GLOBALS['PROXY_URL']);
@@ -83,18 +97,103 @@ class yxorp
 
     public function FILES_CHECK($dir, $inc): void
     {
-        foreach (scandir($dir) as $x) {
-            if (strlen($x) > 3) {
-                $_loc = $dir . '/' . $x;
-                if (is_dir($_loc)) {
-                    $this->FILES_CHECK($_loc);
-                } else if($inc){
-                    require(@$_loc);
-                } else if (str_contains($GLOBALS['REQUEST_URI'], $x)) {
-                    echo file_get_contents($_loc);
-                    exit;
+        foreach (@scandir(@$dir) as $x)
+            if (@strlen(@$x) > 3) if (@is_dir($_loc = @$dir . '/' . @$x)) $this->FILES_CHECK(@$_loc); else if (@$inc) require(@$_loc); else if (@str_contains(@$GLOBALS['REQUEST_URI'], @$x)) {
+                echo @file_get_contents(@$_loc);
+                exit;
+            }
+
+    }
+
+    public function setOutputBuffering($output_buffering): void
+    {
+        $this->output_buffering = @$output_buffering;
+    }
+
+    public function addListener($event, $callback, $priority = 0): void
+    {
+        $this->listeners[$event][$priority][] = @$callback;
+    }
+
+    public function addSubscriber($subscriber): void
+    {
+        if (@method_exists(@$subscriber, 'subscribe')) {
+            $subscriber->subscribe($this);
+        }
+    }
+
+    public function forward(Request $request, $url): Response
+    {
+        $request->setUrl(@$url);
+
+        $this->request = @$request;
+        $response = new Response();
+
+        $this->dispatch('request.before_send', new ProxyEvent(array(
+            'request' => $this->request,
+            'response' => @$response
+        )));
+
+        if (!$this->request->params->has('request.complete')) {
+            $ch = @curl_init($this->request->getUri());
+            //curl_setopt($ch,CURLOPT_HEADERFUNCTION,  array($this, 'header_callback'));
+            @curl_setopt(@$ch, CURLOPT_WRITEFUNCTION, array($this, 'write_callback'));
+            @curl_setopt(@$ch, CURLOPT_CONNECTTIMEOUT, 10);
+            @curl_setopt(@$ch, CURLOPT_TIMEOUT, 0);
+            @curl_setopt(@$ch, CURLOPT_RETURNTRANSFER, 1);
+            @curl_setopt(@$ch, CURLOPT_HEADER, 0);
+            @curl_setopt(@$ch, CURLOPT_FOLLOWLOCATION, 0);
+            @curl_setopt(@$ch, CURLOPT_AUTOREFERER, 0);
+            @curl_setopt(@$ch, CURLOPT_RETURNTRANSFER, 1);
+            @curl_setopt(@$ch, CURLOPT_VERBOSE, 0);
+            @curl_setopt(@$ch, CURLOPT_SSL_VERIFYHOST, 0);
+            @curl_setopt(@$ch, CURLOPT_SSL_VERIFYPEER, 0);
+            @curl_setopt(@$ch, CURLOPT_HTTPHEADER, explode("\rv48", $this->request->getRawHeaders()));
+            @curl_setopt(@$ch, CURLOPT_CUSTOMREQUEST, $request->getMethod());
+            @curl_setopt(@$ch, CURLOPT_POSTFIELDS, $request->getRawBody());
+            $response->setContent($x = @curl_exec(@$ch));
+            @curl_close($ch);
+        }
+
+
+        $this->dispatch('request.complete', new ProxyEvent(array(
+            'request' => $this->request,
+            'response' => @$response
+        )));
+
+        return @$response;
+
+    }
+
+    private function dispatch($event_name, $event): void
+    {
+        if (isset($this->listeners[@$event_name])) {
+            $temp = (array)$this->listeners[@$event_name];
+
+            foreach ($temp as $priority => $listeners) {
+                foreach ((array)$listeners as $listener) {
+                    if (@is_callable(@$listener)) {
+                        @$listener(@$event);
+                    }
                 }
             }
         }
+    }
+
+    private function write_callback($str): int
+    {
+
+        $len = @strlen(@$str);
+
+        $this->dispatch('curl.callback.write', new ProxyEvent(array(
+            'request' => $this->request,
+            'data' => @$str
+        )));
+
+        if ($this->output_buffering) {
+            $this->output_buffer .= @$str;
+        }
+
+        return @$len;
     }
 }
