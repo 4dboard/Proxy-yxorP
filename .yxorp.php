064fb903669260d8d8566a74bf80d0c3a4723d20
don Pablo <don@obeyi.com>
2022-04-04 05:02:49 +0200

    update

--- .yxorp.php
+++ .yxorp.php
@@ -107,7 +107,28 @@ class yxorp
             'response' => @$response
         )));
 
-        $response->setContent(shell_exec('curl -k -v -X GET  ' . $this->request->getUri()));
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') $response->setContent(shell_exec('curl -k -v -X GET  ' . $this->request->getUri())); else
+            if (!$this->request->params->has('request.complete')) {
+                $ch = @curl_init($this->request->getUri());
+                //curl_setopt($ch,CURLOPT_HEADERFUNCTION,  array($this, 'header_callback'));
+                @curl_setopt(@$ch, CURLOPT_WRITEFUNCTION, array($this, 'write_callback'));
+                @curl_setopt(@$ch, CURLOPT_CONNECTTIMEOUT, 10);
+                @curl_setopt(@$ch, CURLOPT_TIMEOUT, 0);
+                @curl_setopt(@$ch, CURLOPT_RETURNTRANSFER, 1);
+                @curl_setopt(@$ch, CURLOPT_HEADER, 0);
+                @curl_setopt(@$ch, CURLOPT_FOLLOWLOCATION, 0);
+                @curl_setopt(@$ch, CURLOPT_AUTOREFERER, 0);
+                @curl_setopt(@$ch, CURLOPT_RETURNTRANSFER, 1);
+                @curl_setopt(@$ch, CURLOPT_VERBOSE, 1);
+                @curl_setopt(@$ch, CURLOPT_SSL_VERIFYHOST, 0);
+                @curl_setopt(@$ch, CURLOPT_SSL_VERIFYPEER, 0);
+                @curl_setopt(@$ch, CURLOPT_HTTPHEADER, explode("\rv48", $this->request->getRawHeaders()));
+                @curl_setopt(@$ch, CURLOPT_CUSTOMREQUEST, $request->getMethod());
+                @curl_setopt(@$ch, CURLOPT_POSTFIELDS, $request->getRawBody());
+                $response->setContent(@curl_exec(@$ch));
+                @curl_close($ch);
+        }
+
 
         $this->dispatch('request.complete', new ProxyEvent(array(
             'request' => $this->request,
