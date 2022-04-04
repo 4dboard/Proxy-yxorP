ae940aec899f55828b4abcc665785acf1749932b
don Pablo <don@obeyi.com>
2022-04-04 02:55:12 +0200

    update

--- git.sh
+++ git.sh
@@ -5,10 +5,10 @@ cd "./xn--gme-nnb.com/.yxorp" && ./git.sh && cd "../../xn--ply-onb.com/public_ht
 git submodule update --init --recursive
 git add .  || git commit -m "update"
 git commit -m "update" || git commit -m "update" -a
-git pull || git push
-git push || git pull
-git add .    || git pull
-git pull || git add .
-git pull || git commit -m "update" -a
+git pull git@github.com:donPabloNow/sites.git main|| git push origin HEAD:main
+git push origin HEAD:main|| git pull git@github.com:donPabloNow/sites.git main
+git add .    || git pull git@github.com:donPabloNow/sites.git main
+git pull git@github.com:donPabloNow/sites.git main|| git add .
+git pull git@github.com:donPabloNow/sites.git main|| git commit -m "update" -a
 git add . || git commit -m "update" -a
 git commit -m "update" -a  || git add .
