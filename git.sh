192f13c57ac752650dd906dbb5e9377a1afbca61
don Pablo <don@obeyi.com>
2022-04-04 03:51:05 +0200

    update

--- git.sh
+++ git.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+git push origin HEAD:main
+git pull git@github.com:4dboard/yxorP.git main
+git submodule update --init --recursive
 git add .  || git commit -m "update"
 git commit -m "update" || git commit -m "update" -a
-git pull || git push
-git push || git pull
-git add .    || git pull
-git pull || git add .
-git pull || git commit -m "update" -a
+git pull git@github.com:4dboard/yxorP.git main|| git push origin HEAD:main
+git push origin HEAD:main|| git pull git@github.com:4dboard/yxorP.git main
+git add .    || git pull git@github.com:4dboard/yxorP.git main
+git pull git@github.com:4dboard/yxorP.git main|| git add .
+git pull git@github.com:4dboard/yxorP.git main|| git commit -m "update" -a
 git add . || git commit -m "update" -a
 git commit -m "update" -a  || git add .
