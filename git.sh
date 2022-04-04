f7ced08f5738070822c980d0e5a14bdf4140eb43
don Pablo <don@obeyi.com>
2022-04-04 03:06:57 +0200

    update

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+git pull git@github.com:donPabloNow/sites.git origin master --allow-unrelated-histories
 git push origin HEAD:main
 git pull git@github.com:donPabloNow/sites.git main
-cd "./xn--gme-nnb.com/.yxorp" && ./git.sh && cd "../../xn--ply-onb.com/public_html" && ./git.sh && cd "../../"
 git submodule update --init --recursive
 git add .  || git commit -m "update"
 git commit -m "update" || git commit -m "update" -a
@@ -12,3 +12,4 @@ git pull git@github.com:donPabloNow/sites.git main|| git add .
 git pull git@github.com:donPabloNow/sites.git main|| git commit -m "update" -a
 git add . || git commit -m "update" -a
 git commit -m "update" -a  || git add .
+cd "./xn--gme-nnb.com/.yxorp" && ./git.sh && cd "../../xn--ply-onb.com/public_html" && ./git.sh && cd "../../"
