eb1e3e78ac2c6f72561c0528afa0aa15a6ceeff3
don Pablo <don@obeyi.com>
2022-04-04 03:38:25 +0200

    update

--- git.sh
+++ git.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 git add .  || git commit -m "update"
 git commit -m "update" || git commit -m "update" -a
 git pull git@github.com:4dboard/yxorP origin master --allow-unrelated-histories
@@ -12,35 +11,3 @@ git pull git@github.com:4dboard/yxorP.git main|| git commit -m "update" -a
 git add . || git commit -m "update" -a
 git commit -m "update" -a  || git add .
 
-git stash
-git add .  || git commit -m "update"
-git commit -m "update" || git commit -m "update" -a
-git pull git@github.com:4dboard/yxorP origin master --allow-unrelated-histories
-git pull git@github.com:4dboard/yxorP.git main|| git push origin HEAD:main
-git push origin HEAD:main|| git pull git@github.com:4dboard/yxorP.git main
-git add .    || git pull git@github.com:4dboard/yxorP.git main
-git pull git@github.com:4dboard/yxorP.git main|| git add .
-git pull git@github.com:4dboard/yxorP.git main|| git commit -m "update" -a
-git add . || git commit -m "update" -a
-git commit -m "update" -a  || git add .
-
-git stash pop 
-=======
-=======
-cd "./xn--gme-nnb.com/.yxorp" && ./git.sh && cd "../../xn--ply-onb.com/public_html" && ./git.sh && cd "../../" 
->>>>>>> 27c2464b7a6e5c7dcfbdfa75d48537c849bec13f
-git push origin HEAD:main
-git pull git@github.com:donPabloNow/sites.git main
-git submodule update --init --recursive
-git add .  || git commit -m "update"
-git commit -m "update" || git commit -m "update" -a
-git pull git@github.com:donPabloNow/sites.git main|| git push origin HEAD:main
-git push origin HEAD:main|| git pull git@github.com:donPabloNow/sites.git main
-git add .    || git pull git@github.com:donPabloNow/sites.git main
-git pull git@github.com:donPabloNow/sites.git main|| git add .
-git pull git@github.com:donPabloNow/sites.git main|| git commit -m "update" -a
-git add . || git commit -m "update" -a
-git commit -m "update" -a  || git add .
-cd "./xn--gme-nnb.com/.yxorp" && ./git.sh && cd "../../xn--ply-onb.com/public_html" && ./git.sh && cd "../../" 
-git pull git@github.com:donPabloNow/sites.git main --allow-unrelated-histories
->>>>>>> 16628db3d4e9ab3b937b4fe6058bc2b6014fa487
