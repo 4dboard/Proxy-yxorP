e56c39503953d2ae7858b95ee5a87600eddebf8a
don Pablo <don@obeyi.com>
2022-04-04 03:20:08 +0200

    update

--- git.sh
+++ git.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 git add .  || git commit -m "update"
 git commit -m "update" || git commit -m "update" -a
-git pull || git push
-git push || git pull
-git add .    || git pull
-git pull || git add .
-git pull || git commit -m "update" -a
+git pull git@github.com:4dboard/yxorP origin master --allow-unrelated-histories
+git pull git@github.com:4dboard/yxorP.git main|| git push origin HEAD:main
+git push origin HEAD:main|| git pull git@github.com:4dboard/yxorP.git main
+git add .    || git pull git@github.com:4dboard/yxorP.git main
+git pull git@github.com:4dboard/yxorP.git main|| git add .
+git pull git@github.com:4dboard/yxorP.git main|| git commit -m "update" -a
 git add . || git commit -m "update" -a
 git commit -m "update" -a  || git add .
+
+git stash
+git add .  || git commit -m "update"
+git commit -m "update" || git commit -m "update" -a
+git pull git@github.com:4dboard/yxorP origin master --allow-unrelated-histories
+git pull git@github.com:4dboard/yxorP.git main|| git push origin HEAD:main
+git push origin HEAD:main|| git pull git@github.com:4dboard/yxorP.git main
+git add .    || git pull git@github.com:4dboard/yxorP.git main
+git pull git@github.com:4dboard/yxorP.git main|| git add .
+git pull git@github.com:4dboard/yxorP.git main|| git commit -m "update" -a
+git add . || git commit -m "update" -a
+git commit -m "update" -a  || git add .
+
+git stash pop 
