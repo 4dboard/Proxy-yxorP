6058202fcdb0a7807be8c16f38119080bdc673ad
don Pablo <don@obeyi.com>
2022-04-07 00:32:51 +0200

    update

--- proxy/Exception/InvalidArgumentException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Proxy\Exception;
-
-class InvalidArgumentException extends \InvalidArgumentException
-{
-}
--- proxy/Exception/UnexpectedValueException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Proxy\Exception;
-
-class UnexpectedValueException extends \UnexpectedValueException
-{
-}
