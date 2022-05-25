a1d18925efd95a996fe56e5f44f4143677fae054
don Pablo <don@obeyi.com>
2022-05-25 23:56:00 +0200

    update

---
+++ git.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git push origin HEAD:main
+git pull git@github.com:4dboard/yxorP.git main
+git submodule update --init --recursive
+git add .  || git commit -m "update"
+git commit -m "update" || git commit -m "update" -a
+git add .  || git commit -m "update"
+git commit -m "update" || git commit -m "update" -a
+git pull git@github.com:4dboard/yxorP origin master --allow-unrelated-histories
+git pull git@github.com:4dboard/yxorP.git main|| git push origin HEAD:main
+git push origin HEAD:main|| git pull git@github.com:4dboard/yxorP.git main
+git add .    || git pull git@github.com:4dboard/yxorP.git main
+git pull git@github.com:4dboard/yxorP.git main|| git add .
+git pull git@github.com:4dboard/yxorP.git main|| git commit -m "update" -a
+git add . || git commit -m "update" -a
+git commit -m "update" -a  || git add .
+
