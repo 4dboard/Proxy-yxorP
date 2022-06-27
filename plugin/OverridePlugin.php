2c292e1dcbd030d5fd819734a19eb4cceb314e54
don Pablo <don@obeyi.com>
2022-06-27 07:46:07 +0200

    update

--- plugin/OverridePlugin.php
+++ plugin/OverridePlugin.php
@@ -8,13 +8,9 @@ class OverridePlugin extends EventWrapper
     {
         if (yxorP::get('MIME') !== 'text/html' && yxorP::get('MIME') !== 'application/javascript' && yxorP::get('MIME') !== 'text/css' && yxorP::get('MIME') !== 'application/xml' && !str_contains(yxorP::get('MIME'), 'text')) return;
         $GLOBAL_SEARCH_MERGE = GeneralHelper::array_merge_ignore(array_keys((array)yxorP::get('GLOBAL_REPLACE')), array_keys((array)yxorP::get('TARGET')['replace']), array(preg_replace("#^[^:/.]*[:/]+#", "", preg_replace("{/$}", "", urldecode(yxorP::get('TARGET_DOMAIN'))))));
-        print_r($GLOBAL_SEARCH_MERGE);
         $GLOBAL_REPLACE_MERGE = GeneralHelper::array_merge_ignore(array_values((array)yxorP::get('GLOBAL_REPLACE')), array_values((array)yxorP::get('TARGET')['replace']), array(preg_replace("#^[^:/.]*[:/]+#", "", preg_replace("{/$}", "", urldecode(yxorP::get('SITE_DOMAIN'))))));
-        print_r($GLOBAL_REPLACE_MERGE);
         $PATTERN_SEARCH_MERGE = GeneralHelper::array_merge_ignore(array_keys((array)yxorP::get('GLOBAL_PATTERN')), array_keys((array)yxorP::get('TARGET')['pattern']));
-        print_r($PATTERN_SEARCH_MERGE);
         $PATTERN_REPLACE_MERGE = GeneralHelper::array_merge_ignore(array_values((array)yxorP::get('GLOBAL_PATTERN')), array_keys((array)yxorP::get('TARGET')['pattern']));
-        print_r($PATTERN_REPLACE_MERGE);
 
         yxorP::get('RESPONSE')->setContent($this->REWRITE(str_replace($GLOBAL_SEARCH_MERGE, $GLOBAL_REPLACE_MERGE, preg_replace($PATTERN_SEARCH_MERGE, $PATTERN_REPLACE_MERGE, yxorP::get('RESPONSE')->getContent()))));
     }
