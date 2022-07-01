b0c79b2b4389424ce2f83d83eb326749e0029f2f
don Pablo <don@obeyi.com>
2022-07-01 23:14:05 +0200

    update

--- inc/constants.php
+++ inc/constants.php
@@ -213,7 +213,7 @@ class constants
         define('TOKEN_RESPONSE', 'RESPONSE');
         /* Defining a constant called TOKEN_REQUEST and setting it to the string TOKEN_REQUEST. */
         define('TOKEN_REQUEST', 'REQUEST');
-        define('TOKEN_constants::get(TOKEN_COCKPIT_APP)', 'constants::get(TOKEN_COCKPIT_APP)');
+        define('TOKEN_COCKPIT_APP', 'COCKPIT_APP');
 
 
         //ENV
