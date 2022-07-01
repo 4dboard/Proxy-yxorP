79910c438b275a223f2e56b8c00ebd01daeeeb44
don Pablo <don@obeyi.com>
2022-07-01 23:43:44 +0200

    update

--- minify/whitespacesRemover.php
+++ minify/whitespacesRemover.php
@@ -1,7 +1,4 @@
-<?php
-
-namespace yxorP\minify;
-
+<?php namespace yxorP\minify;
 
 use function implode;
 use function preg_match_all;
@@ -12,117 +9,35 @@ use function strlen;
 
 class whitespacesRemover implements minfyInterface
 {
-    private const BLOCK_ELEMENTS = [
-        'address',
-        'article',
-        'aside',
-        'audio',
-        'blockquote',
-        'body',
-        'canvas',
-        'caption',
-        'colgroup',
-        'datalist',
-        'details',
-        'div',
-        'dl',
-        'dt',
-        'embed',
-        'fieldset',
-        'figcaption',
-        'figure',
-        'footer',
-        'form',
-        'h[1-6]',
-        'head',
-        'header',
-        'hr',
-        'html',
-
-        'li',
-        'link',
-        'main',
-        'map',
-        'meta',
-        'meter',
-        'nav',
-        'noframes',
-        'noscript',
-        'object',
-        'ol',
-        'optgroup',
-        'option',
-        'p',
-        'param',
-        'pre',
-        'progress',
-        'ruby',
-        'script',
-        'section',
-        'select',
-        'source',
-        'style',
-        'summary',
-        'table',
-        'tbody',
-        'td',
-        'tfoot',
-        'th',
-        'thead',
-        'title',
-        'tr',
-        'track',
-        'ul',
-        'video'
-    ];
+    private const BLOCK_ELEMENTS = ['address', 'article', 'aside', 'audio', 'blockquote', 'body', 'canvas', 'caption', 'colgroup', 'datalist', 'details', 'div', 'dl', 'dt', 'embed', 'fieldset', 'figcaption', 'figure', 'footer', 'form', 'h[1-6]', 'head', 'header', 'hr', 'html', 'li', 'link', 'main', 'map', 'meta', 'meter', 'nav', 'noframes', 'noscript', 'object', 'ol', 'optgroup', 'option', 'p', 'param', 'pre', 'progress', 'ruby', 'script', 'section', 'select', 'source', 'style', 'summary', 'table', 'tbody', 'td', 'tfoot', 'th', 'thead', 'title', 'tr', 'track', 'ul', 'video'];
 
     public function process(string $buffer): string
     {
         if (strlen($buffer) === 0) {
             return '';
         }
-
-        // Find all the <pre>,<code>,<textarea>, and <javascript> tags
-        // We'll want to return them to this unprocessed state later.
         preg_match_all('{<pre.+</pre>}msU', $buffer, $pres_source);
         preg_match_all('{<code.+</code>}msU', $buffer, $codes_source);
         preg_match_all('{<textarea.+</textarea>}msU', $buffer, $textareas_source);
         preg_match_all('{<script.+</script>}msU', $buffer, $javascript_source);
-
-        // Replace multiple spaces with a single space.
         $buffer = preg_replace('/\s{2,}/', ' ', $buffer);
-
-        // Remove spaces around block-level elements.
-        $buffer = preg_replace(
-            '/\s*(<\/?(' . implode('|', self::BLOCK_ELEMENTS) . ')[^>]*>)\s*/is',
-            '$1',
-            $buffer
-        );
-
-        // Replace edited <pre> tags with unprocessed ones.
+        $buffer = preg_replace('/\s*(<\/?(' . implode('|', self::BLOCK_ELEMENTS) . ')[^>]*>)\s*/is', '$1', $buffer);
         if (sizeof($pres_source)) {
             preg_match_all('{<pre.+</pre>}msU', $buffer, $pres_edited);
             $buffer = str_replace($pres_edited[0], $pres_source[0], $buffer);
         }
-
-        // Replace edited <code> tags with unprocessed ones.
         if (sizeof($codes_source)) {
             preg_match_all('{<code.+</code>}msU', $buffer, $codes_edited);
             $buffer = str_replace($codes_edited[0], $codes_source[0], $buffer);
         }
-
-        // Replace edited <textarea> tags with unprocessed ones.
         if (sizeof($textareas_source)) {
             preg_match_all('{<textarea.+</textarea>}msU', $buffer, $textareas_edited);
             $buffer = str_replace($textareas_edited[0], $textareas_source[0], $buffer);
         }
-
-        // Replace edited <script> tags with unprocessed ones.
         if (sizeof($javascript_source)) {
             preg_match_all('{<script.+</script>}msU', $buffer, $javascript_edited);
             $buffer = str_replace($javascript_edited[0], $javascript_source[0], $buffer);
         }
-
         return $buffer;
     }
 }
