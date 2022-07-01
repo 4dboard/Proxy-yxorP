f198af5f947f6c04c16fa550fd658f3046bb43f0
don Pablo <don@obeyi.com>
2022-07-01 23:40:32 +0200

    update

--- minify/areplacer.php
+++ minify/areplacer.php
@@ -1,6 +1,4 @@
-<?php
-
-namespace yxorP\minify;
+<?php namespace yxorP\minify;
 
 use function array_keys;
 use function array_values;
@@ -9,31 +7,19 @@ use function strlen;
 
 require DIR_ROOT . 'minify/minfyInterface.php';
 
-abstract /**
- *
- */
-class areplacer
+abstract class areplacer
 {
     public function process(string $buffer): string
     {
         if (strlen($buffer) === 0) {
             return '';
         }
-
-        return $this->replace(
-            $this->getReplacePatternData(),
-            $buffer
-        );
+        return $this->replace($this->getReplacePatternData(), $buffer);
     }
 
     public function replace(array $replace, string $buffer): string
     {
-        $result = preg_replace(
-            array_keys($replace),
-            array_values($replace),
-            $buffer
-        );
-
+        $result = preg_replace(array_keys($replace), array_values($replace), $buffer);
         return ($result === null) ? $buffer : $result;
     }
 
