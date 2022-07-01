a3d1e489db2c2b26f6ca2a0df013192f66406095
don Pablo <don@obeyi.com>
2022-07-01 23:42:50 +0200

    update

--- minify/attributesSimplifier.php
+++ minify/attributesSimplifier.php
@@ -9,13 +9,6 @@ class attributesSimplifier extends areplacer implements minfyInterface
 {
     #[ArrayShape(['/ method=("get"|get)/i' => "string", '/ disabled=[^ >]*(.*?)/' => "string", '/ readonly=[^ >]*(.*?)/' => "string", '/ selected=[^ >]*(.*?)/' => "string", '/ async=[^ >]*(.*?)/' => "string", '/ defer=[^ >]*(.*?)/' => "string"])] public function getReplacePatternData(): array
     {
-        return [
-            '/ method=("get"|get)/i' => '',
-            '/ disabled=[^ >]*(.*?)/' => ' disabled',
-            '/ readonly=[^ >]*(.*?)/' => ' readonly',
-            '/ selected=[^ >]*(.*?)/' => ' selected',
-            '/ async=[^ >]*(.*?)/' => ' async',
-            '/ defer=[^ >]*(.*?)/' => ' defer',
-        ];
+       
     }
 }
