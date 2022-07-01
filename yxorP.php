fa4542f90deaadebf1096f069de2be4d6cbef2c0
don Pablo <don@obeyi.com>
2022-07-01 23:33:02 +0200

    update

--- yxorP.php
+++ yxorP.php
@@ -57,6 +57,39 @@ class yxorP
     }
 
     /**
+     * It checks if the file exists in the plugin directory, if it does, it requires it, if it doesn't, it checks if the
+     * class exists in the yxorP namespace, if it does, it creates an instance of it
+     *
+     * @param action The name of the action to be executed.
+     */
+    private function subscribe($dir, $action): void
+    {
+        /* It's removing the `.php` extension from the `$action` variable. */
+        $action = str_replace(EXT_PHP, '', $action);
+        /* It's checking if the length of the `$action` variable is less than 3, and if it is, it returns. */
+        if (strlen($action) < 3) return;
+        /* It's checking if the file exists in the plugin directory, if it does, it requires it, if it doesn't, it checks
+        if the class exists in the yxorP namespace, if it does, it creates an instance of it */
+        if (file_exists(DIR_ROOT . $dir . $action . EXT_PHP)) require(DIR_ROOT . $dir . $action . EXT_PHP); elseif ('\\yxorP\\' . $action) $plugin = '\\yxorP\\' . $action;
+        /* It's creating an instance of the class that's in the `$action` variable, and passing it to the `addSubscriber()`
+        function. */
+        $this->addSubscriber(new $action());
+    }
+
+    /**
+     * > If the subscriber has a subscribe method, call it and pass the event manager to it
+     *
+     * @param subscriber The subscriber to add to the event dispatcher.
+     */
+    private function addSubscriber($subscriber): void
+    {
+        /* It's checking if the `subscribe()` method exists in the `$subscriber` object, and if it does, it's calling it,
+        and
+        passing the `$this` object to it. */
+        if (method_exists($subscriber, SUBSCRIBE_METHOD)) $subscriber->subscribe($this);
+    }
+
+    /**
      * It's looping through all the events in the `init()` function and dispatching them to the `yxorP()` function
      * @param $_req
      * @return void
@@ -65,20 +98,6 @@ class yxorP
     {
         /* It's looping through all the events in the `init()` function and dispatching them to the `yxorP()` function */
         foreach (self::init() as $_event) self::yxorP($_req ?: $_SERVER)->dispatch($_event);
-    }
-
-    /**
-     * > `yxorP` is a function that returns a `yxorP` object
-     *
-     * @param _req The request object.
-     *
-     * @return yxorP The yxorP object.
-     */
-    public static function yxorP($_req = null): yxorP
-    {
-        /* It's checking if the `$yxorP` variable is set, and if it is, it returns it, if it isn't, it creates a new
-        instance of the `yxorP` class and sets the `$yxorP` variable to it. */
-        return (self::$yxorP) ?: self::$yxorP = new self($_req ?: $_SERVER);
     }
 
     /**
@@ -158,51 +177,6 @@ class yxorP
     }
 
     /**
-     * > This function adds a listener to the listeners array
-     *
-     * @param event The name of the event to listen for.
-     * @param callback The callback function to be executed when the event is triggered.
-     * @param priority The priority of the listener. Higher priority listeners are called before lower priority listeners.
-     */
-    public function addListener($event, $callback): void
-    {/* It's adding a listener to the listeners array. */
-        $this->listeners[$event][0][] = $callback;
-    }
-
-    /**
-     * It checks if the file exists in the plugin directory, if it does, it requires it, if it doesn't, it checks if the
-     * class exists in the yxorP namespace, if it does, it creates an instance of it
-     *
-     * @param action The name of the action to be executed.
-     */
-    private function subscribe($dir, $action): void
-    {
-        /* It's removing the `.php` extension from the `$action` variable. */
-        $action = str_replace(EXT_PHP, '', $action);
-        /* It's checking if the length of the `$action` variable is less than 3, and if it is, it returns. */
-        if (strlen($action) < 3) return;
-        /* It's checking if the file exists in the plugin directory, if it does, it requires it, if it doesn't, it checks
-        if the class exists in the yxorP namespace, if it does, it creates an instance of it */
-        if (file_exists(DIR_ROOT . $dir . $action . EXT_PHP)) require(DIR_ROOT . $dir . $action . EXT_PHP); elseif ('\\yxorP\\' . $action) $plugin = '\\yxorP\\' . $action;
-        /* It's creating an instance of the class that's in the `$action` variable, and passing it to the `addSubscriber()`
-        function. */
-        $this->addSubscriber(new $action());
-    }
-
-    /**
-     * > If the subscriber has a subscribe method, call it and pass the event manager to it
-     *
-     * @param subscriber The subscriber to add to the event dispatcher.
-     */
-    private function addSubscriber($subscriber): void
-    {
-        /* It's checking if the `subscribe()` method exists in the `$subscriber` object, and if it does, it's calling it,
-        and
-        passing the `$this` object to it. */
-        if (method_exists($subscriber, SUBSCRIBE_METHOD)) $subscriber->subscribe($this);
-    }
-
-    /**
      * "If there are any listeners for the event, call them."
      *
      * The first thing the function does is check if there are any listeners for the event. If there are, it loops through
@@ -216,5 +190,31 @@ class yxorP
         them. */
         if (isset($this->listeners[$event_name])) foreach ((array)$this->listeners[$event_name] as $priority => $listeners) foreach ((array)$listeners as $listener)
             if (is_callable($listener)) $listener();
+    }
+
+    /**
+     * > `yxorP` is a function that returns a `yxorP` object
+     *
+     * @param _req The request object.
+     *
+     * @return yxorP The yxorP object.
+     */
+    public static function yxorP($_req = null): yxorP
+    {
+        /* It's checking if the `$yxorP` variable is set, and if it is, it returns it, if it isn't, it creates a new
+        instance of the `yxorP` class and sets the `$yxorP` variable to it. */
+        return (self::$yxorP) ?: self::$yxorP = new self($_req ?: $_SERVER);
+    }
+
+    /**
+     * > This function adds a listener to the listeners array
+     *
+     * @param event The name of the event to listen for.
+     * @param callback The callback function to be executed when the event is triggered.
+     * @param priority The priority of the listener. Higher priority listeners are called before lower priority listeners.
+     */
+    public function addListener($event, $callback): void
+    {/* It's adding a listener to the listeners array. */
+        $this->listeners[$event][0][] = $callback;
     }
 }
