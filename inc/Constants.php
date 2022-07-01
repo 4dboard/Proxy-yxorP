2d09772935e3bf962147baf8c786bbc1d879b762
don Pablo <don@obeyi.com>
2022-07-01 22:43:35 +0200

    update

--- inc/Constants.php
+++ inc/Constants.php
@@ -212,7 +212,7 @@ class Constants
         /* Defining a constant called TOKEN_RESPONSE and setting it to the string TOKEN_RESPONSE. */
         define('TOKEN_RESPONSE', 'RESPONSE');
         /* Defining a constant called TOKEN_REQUEST and setting it to the string TOKEN_REQUEST. */
-        define('TOKEN_REQUEST', TOKEN_REQUEST);
+        define('TOKEN_REQUEST', 'REQUEST');
 
         //ENV
         /* Defining a constant called ENV_ADMIN_USER and setting it to the value of ADMIN_USER. */
