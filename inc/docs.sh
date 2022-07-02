e46485ecd572d915a262b6ca818be5bcc456a386
don Pablo <don@obeyi.com>
2022-07-02 09:54:25 +0200

    update

--- inc/docs.sh
+++ inc/docs.sh
@@ -1,1 +1,3 @@
+ phpdoc  -d './' -t docs/ --template="xml"
+
  php ./inc/phpDocumentor.phar --directory './action' --directory './http' --directory './minify' --directory './plugin' -t './docs/' --template 'vendor/cvuorinen/phpdoc-markdown-public/data/templates/markdown-public'
