91334f0d2a49c3d5498c99611fc4a53c2e67abd7
don Pablo <don@obeyi.com>
2022-07-02 23:45:26 +0200

    update

---
+++ action/checkUrlPattern.php
@@ -0,0 +1,24 @@
+<?php
+/* Importing the wrapper class from the yxorP\http namespace. */
+
+use yxorP\inc\constants;
+use yxorP\inc\generalHelper;
+use yxorP\inc\wrapper;
+
+/* Extending the wrapper class. */
+
+class checkUrlPattern extends wrapper
+{
+    public function buildIncludes()
+    {
+        /* Getting the current URL. */
+        $url = constants::get(YXORP_REQUEST)->getUri();
+
+        /* Used to filter the events. */
+        if ($this->url_pattern) if (generalHelper::starts_with($this->url_pattern, CHAR_SLASH) && preg_match($this->url_pattern, $url) !== 1)
+            return; else if (stripos($url, $this->url_pattern) === false) return;
+
+        //Todo: Final Event
+
+    }
+}
