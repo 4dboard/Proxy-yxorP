63bf260701126ba64e70af11a7c01a10964700dd
don Pablo <don@obeyi.com>
2022-07-02 20:10:51 +0200

    update

--- yxorP.php
+++ yxorP.php
@@ -56,9 +56,9 @@ class yxorP
         if ((constants::get(YXORP_SERVER))[YXORP_REQUEST_URI])
             if (str_contains((constants::get(YXORP_SERVER))[YXORP_REQUEST_URI], CHAR_SLASH . DIR_COCKPIT))
                 require PATH_COCKPIT_INDEX;
-        
+        $YXORP_TARGET = constants::get(YXORP_TARGET);
         /* Getting the `plugins` key from the `TARGET` array. If it is not set, it will set it to an empty array. */
-        $_plugins = (constants::get(YXORP_TARGET))[YXORP_PLUGINS] ?: [];
+        $_plugins = $YXORP_TARGET[YXORP_PLUGINS] ?: [];
         /* Adding the default plugins to the `$_plugins` array. */
         array_push($_plugins, 'blockListPluginAction', 'cookiePluginAction', 'dailyMotionPluginAction', 'headerRewritePluginAction', 'logPluginAction', 'overridePluginAction', 'proxifyPluginAction', 'streamPluginAction', 'twitterPluginAction', 'youtubePluginAction');
         /* It's looping through all the plugins in the `$_plugins` array, and calling the `subscribe()` function. */
