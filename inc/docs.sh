d32dc16e26d061ae4c91fdd3a5d4289934ab1c27
don Pablo <don@obeyi.com>
2022-07-02 05:01:01 +0200

    update

--- inc/docs.sh
+++ inc/docs.sh
@@ -1,2 +1,3 @@
 php ./inc/phpDocumentor.phar -d ./action -d ./example -d ./http -d ./minify -d ./override -d ./plugin  -t ./docs/app
-php inc/phpDocumentor.phar --directory=./ --target=docs --template=vendor/onspli/phpdoc-markdown/templates/public-onefile
+ php inc/phpDocumentor.phar --directory=./action --directory=./example --directory=./http --directory=./minify --directory=./plugin --directory=./yxorP.php --target=docs --template=vendor/onspli/ph
+pdoc-markdown/templates/public-onefile
