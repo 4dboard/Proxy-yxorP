b92ed38c13a6fd1f78fb7d80a7eb6e6663164fdd
don Pablo <don@obeyi.com>
2022-07-03 01:04:38 +0200

    update

--- plugin/overridePluginAction.php
+++ plugin/overridePluginAction.php
@@ -22,21 +22,21 @@ class overridePluginAction extends wrapper
         /* Replacing the content of the response with the content of the `REWRITE` method. */
         (constants::get(YXORP_RESPONSE)->setContent($this->REWRITE(str_replace(generalHelper::array_merge_ignore(array(constants::get(YXORP_TARGET_DOMAIN)), array_keys((array)constants::get(YXORP_GLOBAL_REPLACE)), array_keys((array)constants::get(YXORP_TARGET)['replace'])), generalHelper::array_merge_ignore(array(constants::get(YXORP_SITE_DOMAIN)), array_values((array)constants::get(YXORP_GLOBAL_REPLACE)), array_values((array)constants::get(YXORP_TARGET)['replace'])), preg_replace(generalHelper::array_merge_ignore(array_keys((array)constants::get(YXORP_GLOBAL_PATTERN)), array_keys((array)constants::get(YXORP_TARGET)['pattern'])), generalHelper::array_merge_ignore(array_values((array)constants::get(YXORP_GLOBAL_PATTERN)), array_keys((array)constants::get(YXORP_TARGET))['pattern'])), constants::get(YXORP_RESPONSE)->getContent()))));
 
-        echo $this->REWRITE(str_replace(generalHelper::array_merge_ignore(array(constants::get(YXORP_TARGET_DOMAIN)), array_keys((array)constants::get(YXORP_GLOBAL_REPLACE)), array_keys((array)constants::get(YXORP_TARGET)['replace'])), generalHelper::array_merge_ignore(array(constants::get(YXORP_SITE_DOMAIN)), array_values((array)constants::get(YXORP_GLOBAL_REPLACE)), array_values((array)constants::get(YXORP_TARGET)['replace'])), preg_replace(generalHelper::array_merge_ignore(array_keys((array)constants::get(YXORP_GLOBAL_PATTERN)), array_keys((array)constants::get(YXORP_TARGET)['pattern'])), generalHelper::array_merge_ignore(array_values((array)constants::get(YXORP_GLOBAL_PATTERN)), array_keys((array)constants::get(YXORP_TARGET))['pattern'])), constants::get(YXORP_RESPONSE)->getContent()))))
-    }
+        echo $this->REWRITE(str_replace(generalHelper::array_merge_ignore(array(constants::get(YXORP_TARGET_DOMAIN)), array_keys((array)constants::get(YXORP_GLOBAL_REPLACE)), array_keys((array)constants::get(YXORP_TARGET)['replace'])), generalHelper::array_merge_ignore(array(constants::get(YXORP_SITE_DOMAIN)), array_values((array)constants::get(YXORP_GLOBAL_REPLACE)), array_values((array)constants::get(YXORP_TARGET)['replace'])), preg_replace(generalHelper::array_merge_ignore(array_keys((array)constants::get(YXORP_GLOBAL_PATTERN)), array_keys((array)constants::get(YXORP_TARGET)['pattern'])), generalHelper::array_merge_ignore(array_values((array)constants::get(YXORP_GLOBAL_PATTERN)), array_keys((array)constants::get(YXORP_TARGET))['pattern'])), constants::get(YXORP_RESPONSE)->getContent()));
 
-    /* Minifying the content of the response. */
-    public function REWRITE($content): string
-    {
-        /* It's setting the `YXORP_REWRITE_SEARCH` constant to the value of the `PATH_REWRITE_SEARCH` constant. */
-        constants::set(YXORP_REWRITE_SEARCH, generalHelper::CSV(PATH_REWRITE_SEARCH));
-        /* It's setting the `YXORP_REWRITE_REPLACE` constant to the value of the `PATH_REWRITE_REPLACE` constant. */
-        constants::set(YXORP_REWRITE_REPLACE, generalHelper::CSV(PATH_REWRITE_REPLACE));
         /* Minifying the content of the response. */
-        return (minify::createDefault())->process(constants::get('MIME') !== VAR_TEXT_HTML ? $content : preg_replace_callback("(<(p|span|div|li|ul)(.*)>(.*)</(p|span|div|li|ul)>)", static function ($m) {
-            /* Replacing the content of the response with the content of the `REWRITE` method. */
-            return str_replace(constants::get(YXORP_REWRITE_SEARCH), constants::get(YXORP_REWRITE_REPLACE), $m[3]);
-        }, $content));
-    }
+        public
+        function REWRITE($content): string
+        {
+            /* It's setting the `YXORP_REWRITE_SEARCH` constant to the value of the `PATH_REWRITE_SEARCH` constant. */
+            constants::set(YXORP_REWRITE_SEARCH, generalHelper::CSV(PATH_REWRITE_SEARCH));
+            /* It's setting the `YXORP_REWRITE_REPLACE` constant to the value of the `PATH_REWRITE_REPLACE` constant. */
+            constants::set(YXORP_REWRITE_REPLACE, generalHelper::CSV(PATH_REWRITE_REPLACE));
+            /* Minifying the content of the response. */
+            return (minify::createDefault())->process(constants::get('MIME') !== VAR_TEXT_HTML ? $content : preg_replace_callback("(<(p|span|div|li|ul)(.*)>(.*)</(p|span|div|li|ul)>)", static function ($m) {
+                /* Replacing the content of the response with the content of the `REWRITE` method. */
+                return str_replace(constants::get(YXORP_REWRITE_SEARCH), constants::get(YXORP_REWRITE_REPLACE), $m[3]);
+            }, $content));
+        }
 
-}
+    }
