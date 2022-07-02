5b8f4aba52ec70501b98064d9b0eb610001a9ac6
don Pablo <don@obeyi.com>
2022-07-02 22:45:18 +0200

    update

--- http/request.php
+++ http/request.php
@@ -163,7 +163,7 @@ class request
     {
         $method = (constants::get(YXORP_SERVER))[YXORP_REQUEST_METHOD];
         $scheme = (isset(constants::get(YXORP_SERVER)[YXORP_HTTPS]) && (constants::get(YXORP_SERVER))[YXORP_HTTPS]) ? YXORP_HTTPS : VAR_HTTP;
-        $url = $scheme . ':' . constants::get(YXORP_PROXY_URL)->__toString();
+        $url = $scheme . ':' . constants::get(YXORP_PROXY_URL);
         $request = new request($method, $url);
         foreach (constants::get(YXORP_SERVER) as $name => $value) if (str_starts_with($name, YXORP_HTTP_)) {
             $name = substr($name, 5);
