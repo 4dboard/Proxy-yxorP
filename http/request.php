dabb8c27a9f56a295ca1db43c4c49f239749fe1e
don Pablo <don@obeyi.com>
2022-07-02 20:12:46 +0200

    update

--- http/request.php
+++ http/request.php
@@ -4,7 +4,6 @@
 
 use yxorP;
 use yxorP\inc\constants;
-use yxorP\inc\generalHelper;
 
 /* A class that represents an HTTP request. */
 
@@ -183,37 +182,6 @@ class request
         return $request;
     }
 
-    /* Creating a new request from the global variables. */
-
-    public static function onBuildContext(): void
-    {
-        /* Setting the `SITE_URL` variable to the value of the `SERVER_NAME` key in the `YXORP_SERVER` array. */
-        constants::set(YXORP_SITE_URL, ((constants::get(YXORP_SERVER))[YXORP_SERVER_NAME]));
-        /* Setting the `SITE_DOMAIN` variable to the result of the `extractDomain` method. */
-        constants::set(YXORP_SITE_DOMAIN, generalHelper::extractDomain(constants::get(YXORP_SITE_URL)));
-        /* Setting the `TARGET` variable to the result of the `findOne` method. */
-        print_r(constants::get(YXORP_SITE_DOMAIN));
-        exit;
-        constants::set(YXORP_TARGET, constants::get(constants::get(YXORP_COCKPIT_APP))->storage->findOne(COCKPIT_COLLECTIONS . CHAR_SLASH . COCKPIT_SITES, [COCKPIT_HOST => constants::get(YXORP_SITE_DOMAIN)]));
-        /* Setting the `SITE_SUB_DOMAIN` variable to the result of the `extractSubdomains` method. */
-        constants::set(YXORP_SITE_SUB_DOMAIN, generalHelper::extractSubdomains(constants::get(YXORP_SITE_URL)));
-        /* Setting the `TARGET_URL` variable to the value of the `target` key in the `TARGET` array. */
-        constants::set(YXORP_TARGET_URL, (constants::get(YXORP_TARGET))[YXORP_TARGET]);
-        /* Setting the `TARGET_SUB_DOMAIN` variable to the result of the `extractSubdomains` method. */
-        constants::set(YXORP_TARGET_SUB_DOMAIN, generalHelper::extractSubdomains(constants::get(YXORP_TARGET_URL)));
-        /* Setting the `TARGET_DOMAIN` variable to the result of the `extractDomain` method. */
-        constants::set(YXORP_TARGET_DOMAIN, generalHelper::extractDomain(constants::get(YXORP_TARGET_URL)));
-        /* Setting the `FETCH` variable to the value of the `SITE_SUB_DOMAIN` variable, if it is not null, and the
-        `TARGET_DOMAIN` variable, with the `https://` protocol. */
-        constants::set(YXORP_FETCH, VAR_HTTPS . CHAR_SLASH . CHAR_SLASH . ((!is_null(constants::get(YXORP_SITE_SUB_DOMAIN))) ? (constants::get(YXORP_SITE_SUB_DOMAIN)->__toString() . ".") : null) . constants::get(YXORP_TARGET_DOMAIN)->__toString());
-        /* Setting the `PROXY_URL` variable to the value of the `FETCH` variable, with the value of the `YXORP_REQUEST_URI`
-        variable appended to it. */
-        constants::set(YXORP_PROXY_URL, constants::get(YXORP_FETCH)->__toString() . constants::get(YXORP_REQUEST_URI)->__toString());
-        /* Setting the `DIR_FULL` variable to the value of the `DIR_ROOT` constant, with the `override` string appended
-        to it, with the `CHAR_SLASH` constant appended to it, with the value of the `files` key in the `TARGET`
-        array appended to it. */
-        constants::set(YXORP_DIR_FULL, DIR_ROOT . DIR_OVERRIDE . (constants::get(YXORP_TARGET))[YXORP_FILES]);
-    }
 
     /* A getter method for the `$method` property. */
 
