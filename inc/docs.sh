0f437a1dd5defba67a3f7819d6f95f4cdd30a1de
don Pablo <don@obeyi.com>
2022-07-02 05:35:11 +0200

    update

--- inc/docs.sh
+++ inc/docs.sh
@@ -2,4 +2,6 @@ php ./inc/phpDocumentor.phar -d ./action -d ./example -d ./http -d ./minify -d .
 
 php ./inc/phpDocumentor.phar --directory=./action --directory=./example --directory=./http --directory=./minify --directory=./plugin --directory=./yxorP.php  --target=docs --template="vendor/saggre/phpdocumentor-markdown/themes/markdown"
 
+php ./inc/phpDocumentor.phar --directory=./  --target=docs --template="vendor/saggre/phpdocumentor-markdown/themes/markdown"
+
 php ./inc/phpDocumentor.phar docs/structure.xml
