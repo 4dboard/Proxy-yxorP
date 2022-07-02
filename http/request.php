00d21ac7c0d2d573bd24e7b13500cdacb9d599be
don Pablo <don@obeyi.com>
2022-07-02 19:18:43 +0200

    update

--- http/request.php
+++ http/request.php
@@ -192,7 +192,7 @@ class request
         /* Setting the `SITE_DOMAIN` variable to the result of the `extractDomain` method. */
         constants::set(YXORP_SITE_DOMAIN, generalHelper::extractDomain(constants::get(YXORP_SITE_URL)));
         /* Setting the `TARGET` variable to the result of the `findOne` method. */
-        print_r((array)constants::get(YXORP_SERVER)[0]);
+        print_r((array)constants::get(YXORP_SERVER)[5]);
         exit;
         constants::set(YXORP_TARGET, constants::get(constants::get(YXORP_COCKPIT_APP))->storage->findOne(COCKPIT_COLLECTIONS . CHAR_SLASH . COCKPIT_SITES, [COCKPIT_HOST => constants::get(YXORP_SITE_DOMAIN)]));
         /* Setting the `SITE_SUB_DOMAIN` variable to the result of the `extractSubdomains` method. */
