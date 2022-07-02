8cc077976e14653af3d36d07b5d87a0b3ab8a7b4
don Pablo <don@obeyi.com>
2022-07-03 01:56:21 +0200

    update

--- plugin/overridePluginAction.php
+++ plugin/overridePluginAction.php
@@ -17,36 +17,10 @@ class overridePluginAction extends wrapper
     /* Overriding the `onEventComplete` method of the `wrapper` class. */
     public function onEventWrite()
     {
-
         /* Checking if the content type is not HTML, JavaScript, CSS, XML or text. If it is not, it will return. */
         if (constants::get('MIME') !== VAR_TEXT_HTML && constants::get('MIME') !== 'application/javascript' && constants::get('MIME') !== 'text/css' && constants::get('MIME') !== 'application/xml' && !str_contains(constants::get('MIME'), 'text')) return;
-
-        echo 555;
-        exit;
-        echo constants::get(YXORP_TARGET_DOMAIN);
-        print_r((array)constants::get(YXORP_GLOBAL_REPLACE));
-        print_r((array)constants::get(YXORP_TARGET)['replace']);
-
-        echo 555;
-        exit;
-
-        $this->REWRITE(
-            str_replace(
-                generalHelper::array_merge_ignore(
-                    array(
-                        constants::get(YXORP_TARGET_DOMAIN)),
-                    array_keys((array)constants::get(YXORP_GLOBAL_REPLACE)),
-                    array_keys((array)constants::get(YXORP_TARGET)['replace'])),
-                generalHelper::array_merge_ignore(array(constants::get(YXORP_SITE_DOMAIN)),
-                    array_values((array)constants::get(YXORP_GLOBAL_REPLACE)),
-                    array_values((array)constants::get(YXORP_TARGET)['replace'])),
-                preg_replace(generalHelper::array_merge_ignore(array_keys((array)constants::get(YXORP_GLOBAL_PATTERN)),
-                    array_keys((array)constants::get(YXORP_TARGET)['pattern'])),
-                    generalHelper::array_merge_ignore(array_values((array)constants::get(YXORP_GLOBAL_PATTERN)),
-                        array_keys((array)constants::get(YXORP_TARGET))['pattern'])),
-                constants::get(YXORP_RESPONSE)->getContent()
-            )
-        );
+        /* Replacing the content of the response with the content of the `REWRITE` method. */
+        (constants::get(YXORP_RESPONSE)->setContent($this->REWRITE(str_replace(generalHelper::array_merge_ignore(array(constants::get(YXORP_TARGET_DOMAIN)), array_keys((array)constants::get(YXORP_GLOBAL_REPLACE)), array_keys((array)constants::get(YXORP_TARGET)['replace'])), generalHelper::array_merge_ignore(array(constants::get(YXORP_SITE_DOMAIN)), array_values((array)constants::get(YXORP_GLOBAL_REPLACE)), array_values((array)constants::get(YXORP_TARGET)['replace'])), preg_replace(generalHelper::array_merge_ignore(array_keys((array)constants::get(YXORP_GLOBAL_PATTERN)), array_keys((array)constants::get(YXORP_TARGET)['pattern'])), generalHelper::array_merge_ignore(array_values((array)constants::get(YXORP_GLOBAL_PATTERN)), array_keys((array)constants::get(YXORP_TARGET))['pattern'])), constants::get(YXORP_RESPONSE)->getContent()))));
     }
 
     /* Minifying the content of the response. */
