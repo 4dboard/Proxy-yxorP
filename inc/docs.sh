eda7a91befebd37144f8e0c94fc489fddee67ba9
don Pablo <don@obeyi.com>
2022-07-02 09:36:35 +0200

    update

--- inc/docs.sh
+++ inc/docs.sh
@@ -1,1 +1,1 @@
- php ./inc/phpDocumentor.phar --directory './action' --directory './http' --directory './minify' --directory './plugin'--directory '/cockpit' -t './docs/' --template 'vendor/cvuorinen/phpdoc-markdown-public/data/templates/markdown-public'
+ php ./inc/phpDocumentor.phar --directory './action' --directory './http' --directory './minify' --directory './plugin' -t './docs/' --template 'vendor/cvuorinen/phpdoc-markdown-public/data/templates/markdown-public'
