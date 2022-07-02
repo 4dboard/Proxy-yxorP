04d979a203cc826a332ea1fb71bac69bf042d638
don Pablo <don@obeyi.com>
2022-07-03 00:23:33 +0200

    update

--- inc/constants.php
+++ inc/constants.php
@@ -588,6 +588,7 @@ class constants
         constants::set(YXORP_DIR_FULL, DIR_ROOT . DIR_OVERRIDE . (constants::get(YXORP_TARGET))[YXORP_FILES]);
 
         /* Creating a unique key for the cache file. */
+        echo generalHelper::base64_url_encode(constants::get(YXORP_PROXY_URL)) . EXT_TMP;
         define('CACHE_KEY', generalHelper::base64_url_encode(constants::get(YXORP_PROXY_URL)) . EXT_TMP);
 
     }
