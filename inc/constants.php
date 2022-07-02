8c23725b111e842fa38bbd89cbb137f0c9148fdc
don Pablo <don@obeyi.com>
2022-07-02 02:56:30 +0200

    update

--- inc/constants.php
+++ inc/constants.php
@@ -325,7 +325,7 @@ class constants
         /* Setting the token to the bugsnag key. */
         self::set(YXORP_BUGSNAG, \Bugsnag\Client::make(ENV_BUG_SNAG_KEY));
         /* Setting the token GUZZLE to a new instance of the GuzzleHttp\Client class. */
-        self::set(YXORP_GUZZLE, new \GuzzleHttp\Client([VAR_ALLOW_REDIRECTS => true, VAR_HTTP_ERRORS => true, VAR_DECODE_CONTENT => true, VAR_VERIFY => false, VAR_COOKIES => true, 'idn_conversion' => true]));
+        self::set(YXORP_GUZZLE, new \GuzzleHttp\Client([VAR_ALLOW_REDIRECTS => true, VAR_HTTP_ERRORS => true, VAR_DECODE_CONTENT => true, VAR_VERIFY => false, VAR_COOKIES => true, VAR_IDN_CONVERSION => true]));
         /* It's setting the `YXORP_COCKPIT_APP` constant to the `cockpit()` function. */
         constants::set(YXORP_COCKPIT_APP, cockpit());
 
