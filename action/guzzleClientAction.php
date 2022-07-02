2a9949079a22e3c066fefa3adefca4ea01d25887
don Pablo <don@obeyi.com>
2022-07-02 23:46:15 +0200

    update

--- action/guzzleClientAction.php
+++ action/guzzleClientAction.php
@@ -1,8 +1,8 @@
 <?php
 /* Importing the wrapper class from the yxorP\http namespace. */
 
-use yxorP\inc\wrapper;
 use yxorP\inc\constants;
+use yxorP\inc\wrapper;
 
 /* Extending the `wrapper` class, which is a class that is used to wrap the `Event` class. */
 
@@ -16,6 +16,6 @@ class guzzleClientAction extends wrapper
     {
         /* Creating a new `GuzzleHttp\Client` object, and then it is sending a request to the `constants::get(YXORP_FETCH)` URL,
         with the `constants::get(YXORP_REQUEST)->getMethod()` method, and the `$_REQUEST` array as the body. */
-        constants::get(YXORP_RESPONSE)->setContent(GUZZLE->request(constants::get(YXORP_REQUEST)->getMethod(), constants::get(YXORP_FETCH), json_decode(json_encode($_REQUEST), true, 512, JSON_THROW_ON_ERROR))->getBody());
+        constants::get(YXORP_RESPONSE)->setContent(constants::get(YXORP_GUZZLE)->request(constants::get(YXORP_REQUEST)->getMethod(), constants::get(YXORP_FETCH), json_decode(json_encode($_REQUEST), true, 512, JSON_THROW_ON_ERROR))->getBody());
     }
 }
