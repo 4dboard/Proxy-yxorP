833194011e0fd12227b0f25efc422bc2309e1e2e
don Pablo <don@obeyi.com>
2022-07-02 19:28:14 +0200

    update

--- inc/constants.php
+++ inc/constants.php
@@ -544,7 +544,7 @@ class constants
     {
         /* Checking if the argument already exists in the global scope and if it does, it throws an exception. If it
         doesn't, it adds the argument to the global scope . */
-        return (array_key_exists($_name, $GLOBALS)) ? throw new RuntimeException(ACCESS_ALREADY_DEFINED) : $GLOBALS[$_name] = json_encode($_value);
+        return (array_key_exists($_name, $GLOBALS)) ? throw new RuntimeException(ACCESS_ALREADY_DEFINED) : $GLOBALS[$_name] = json_encode(get_object_vars($_value));
     }
 
     /* A function that is being called to localise constants. */
