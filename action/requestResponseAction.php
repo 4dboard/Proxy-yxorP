65a60a01fffcea5c000d403ce1f161a33c2ff804
don Pablo <don@obeyi.com>
2022-07-02 23:09:06 +0200

    update

--- action/requestResponseAction.php
+++ action/requestResponseAction.php
@@ -1,6 +1,7 @@
 <?php
 /* Importing the wrapper class from the yxorP\http namespace. */
 
+use yxorP\inc\constants;
 use yxorP\inc\wrapper;
 
 /* Extending the wrapper class, which is a class that is used to wrap the request and response objects. */
@@ -10,9 +11,9 @@ class requestResponseAction extends wrapper
     /* This function is used to build the request and response objects. */
     public function buildIncludes(): void
     {
-//        /* Setting the response object to the key RESPONSE. */
-//        constants::set(YXORP_RESPONSE, new Response());
-//        /* Setting the request object to the key REQUEST. */
-//        constants::set(YXORP_REQUEST, Request::createFromGlobals());
+        /* Setting the response object to the key RESPONSE. */
+        constants::set(YXORP_RESPONSE, new Response());
+        /* Setting the request object to the key REQUEST. */
+        constants::set(YXORP_REQUEST, Request::createFromGlobals());
     }
 }
