0f25dfd8d27086e3288a426eca5aa402713ea699
don Pablo <don@obeyi.com>
2022-07-02 23:13:53 +0200

    update

--- inc/wrapper.php
+++ inc/wrapper.php
@@ -4,8 +4,6 @@
 
 
 use yxorP;
-use yxorP\http\request;
-use yxorP\http\response;
 
 /* Importing the class `request` from the namespace `yxorP\http`. */
 
@@ -83,11 +81,6 @@ abstract class wrapper
     {
         /* Used to catch exceptions. */
         try {
-            /* Setting the response object to the key RESPONSE. */
-            constants::set(YXORP_RESPONSE, new response());
-            /* Setting the request object to the key REQUEST. */
-            constants::set(YXORP_REQUEST, request::createFromGlobals());
-
             /* Getting the current URL. */
             $url = constants::get(YXORP_REQUEST)->getUri();
 
