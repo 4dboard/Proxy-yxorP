803191017f38116ba407762abbde24d733bcc6f2
don Pablo <don@obeyi.com>
2022-07-03 07:13:33 +0200

    update

--- protocols/protocolInterface.php
+++ protocols/protocolInterface.php
@@ -2,10 +2,14 @@
 
 use yxorP\connection\connectionInterface;
 
+/* Defining the methods that must be implemented by any protocol. */
+
 interface protocolInterface
 {
+    /* The method that will be called when a new message is received. */
     public static function input($recv_buffer, connectionInterface $connection);
 
+    /* Decoding the data received from the client. */
     public static function decode($recv_buffer, connectionInterface $connection);
 
     public static function encode($data, connectionInterface $connection);
