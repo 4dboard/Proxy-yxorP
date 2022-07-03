c4aa52969ab6705627603534fbe1b717394a078a
don Pablo <don@obeyi.com>
2022-07-03 03:20:47 +0200

    update

--- inc/generalHelper.php
+++ inc/generalHelper.php
@@ -241,7 +241,7 @@ class generalHelper
         foreach (scandir($dir) as $x) if (strlen($x) > 3) {
             if (str_contains($x, VAR_INTERFACE)) continue;
             if (is_dir($_loc = $dir . DIRECTORY_SEPARATOR . $x)) return self::fileCheck($_loc, $inc);
-            if (str_contains(constants::get(YXORP_PROXY_URL), $x)) return cache::cache()->set(file_get_contents($_loc));
+            if (!$inc && str_contains(constants::get(YXORP_PROXY_URL), $x)) return cache::cache()->set(file_get_contents($_loc));
             if ($inc) require_once($_loc);
         }
     }
