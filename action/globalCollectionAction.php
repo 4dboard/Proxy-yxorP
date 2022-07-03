9c550c282886c7725ab659ef33adcd8a616ab895
don Pablo <don@obeyi.com>
2022-07-03 04:04:59 +0200

    update

--- action/globalCollectionAction.php
+++ action/globalCollectionAction.php
@@ -1,7 +1,6 @@
 <?php
 /* Importing the wrapper class from the yxorP\http namespace. */
 
-use yxorP\inc\constants;
 use yxorP\inc\wrapper;
 
 /* Extending the `wrapper` class. */
@@ -12,10 +11,6 @@ class globalCollectionAction extends wrapper
     public function onBuildContext(): void
     {
         /* Setting the `REPLACE` context variable to the value of the `replace` type in the `global` collection. */
-        constants::set('YXORP_GLOBAL_REPLACE', constants::get(YXORP_COCKPIT_APP)->storage->findOne(COCKPIT_COLLECTIONS . CHAR_SLASH . 'global', [VAR_TYPE => 'replace']) ?
-            (constants::get(YXORP_COCKPIT_APP)->storage->findOne(COCKPIT_COLLECTIONS . CHAR_SLASH . 'global', [VAR_TYPE => 'replace']))['value'] : null);
-        /* Setting the `PATTERN` context variable to the value of the `pattern` type in the `global` collection. */
-        constants::set('YXORP_GLOBAL_PATTERN', constants::get(YXORP_COCKPIT_APP)->storage->findOne(COCKPIT_COLLECTIONS . CHAR_SLASH . 'global', [VAR_TYPE => 'pattern']) ?
-            (constants::get(YXORP_COCKPIT_APP)->storage->findOne(COCKPIT_COLLECTIONS . CHAR_SLASH . 'global', [VAR_TYPE => 'pattern']))['value'] : null);
+
     }
 }
