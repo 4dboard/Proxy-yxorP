03acb5e9aade637aa0efded6f84beb808459da89
don Pablo <don@obeyi.com>
2022-07-05 01:55:44 +0200

    update

--- index.php
+++ index.php
@@ -6,5 +6,8 @@ namespace yxorP;
 /* Including the file `yxorP.php` in the current file. */
 require 'yxorP.php';
 
-/* A function that is called to start the proxy. */
+/**
+ * Proxy to the yxorP::proxy() method. 
+ *  A function that is called to start the proxy. 
+ */
 yxorP::proxy();
