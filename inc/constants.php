031dcc5e0d5bad4f4dc9d577015a8fd2cf1d38e4
don Pablo <don@obeyi.com>
2022-07-05 05:08:51 +0200

    update

--- inc/constants.php
+++ inc/constants.php
@@ -629,10 +629,10 @@ class constants
     /* A static method that returns the value of the $_name variable. */
     /**
      * @param string $_name
-     * @param string|array|null $return = null
+     * @param string|array $return
      * @return string|array
      */
-    public static function get(string $_name, string|array|null $return = null): string|array
+    public static function get(string $_name, string|array $return): string|array
     {
         /* Checking if the key exists in the global array. If it does, it returns the value of the key. If it doesn't, it
         returns false . */
