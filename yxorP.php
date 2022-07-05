df46fd56e754ac4e9fda24000d6d7f22e8567123
don Pablo <don@obeyi.com>
2022-07-05 06:01:56 +0200

    update

--- yxorP.php
+++ yxorP.php
@@ -114,7 +114,53 @@ class yxorP
     public static function proxy($request = null): void
     {
         /* It's looping through all the events in the `init()` function and dispatching them to the `yxorP()` function */
-        foreach (constants::get(YXORP_EVENT_LIST) as $event) self::yxorP($request ?: $_SERVER)->dispatch($event);
+        foreach (self::init() as $event) self::yxorP($request ?: $_SERVER)->dispatch($event);
+    }
+
+    /**
+     * It creates the plugin's directory if it doesn't exist, and installs the plugin if it's not already installed.
+     */
+    private static function init(): void
+    {
+        /* It's creating the constants that are used in the plugin. */
+        constants::create(__DIR__);
+        /* It's checking if the `http` and `minify` directories exist in the plugin directory, and if they don't, it
+        creates them. */
+        foreach (array(DIR_HTTP, DIR_MINIFY) as $_asset) generalHelper::fileCheck(DIR_ROOT . $_asset, true);
+        /* It's checking if the plugin directory exists, and if it doesn't, it creates it. */
+        foreach ([DIR_PLUGIN, PATH_DIR_TMP] as $_dir)
+            if (!is_dir($_dir)) if (!mkdir($_dir) && !is_dir($_dir))
+                throw new RuntimeException(sprintf(RUNTIME_EXCEPTION, $_dir));
+        /* It's checking if there are any users in the `cockpit_accounts` collection, and if there aren't, it's calling the
+        `install()` function. */
+        if (!constants::get(YXORP_COCKPIT_APP)->storage->getCollection(COCKPIT_ACCOUNTS)->count())
+            self::install();
+        return constants::get(YXORP_EVENT_LIST);
+    }
+
+    /**
+     * It creates a new user with the credentials defined in the `.env` file
+     */
+    private static function install(): void
+    {
+        /* It's defining the `YXORP_COCKPIT_INSTALL` constant as `true`. */
+        define(YXORP_COCKPIT_INSTALL, true);
+
+        /* It's copying the files from the `local` directory to the `cockpit` directory. */
+        self::migrate(PATH_COCKPIT_LOCAL, PATH_DIR_COCKPIT);
+
+        /* It's creating an array of user data. */
+        $_account = [VAR_USER => constants::get(ENV_ADMIN_USER), VAR_NAME => constants::get(ENV_ADMIN_NAME), VAR_EMAIL => constants::get(ENV_ADMIN_EMAIL), VAR_ACTIVE => true, VAR_GROUP => VAR_ADMIN, VAR_PASSWORD => constants::get(YXORP_COCKPIT_APP)->hash(constants::get(ENV_ADMIN_PASSWORD)), VAR_I18N => constants::get(YXORP_COCKPIT_APP)->helper(VAR_I18N)->locale, VAR_CREATED => time(), VAR_MODIFIED => time()];
+        /* It's inserting a new user into the `cockpit_accounts` collection. */
+        constants::get(YXORP_COCKPIT_APP)->storage->insert(COCKPIT_ACCOUNTS, $_account);
+    }
+
+    public static function migrate($src, $dst)
+    {
+        $dir = opendir($src);
+        @mkdir($dst);
+        foreach (scandir($src) as $file) if (($file != CHAR_PERIOD) && ($file != CHAR_PERIOD . CHAR_PERIOD)) if (is_dir($src . DIRECTORY_SEPARATOR . $file)) self::migrate($src . DIRECTORY_SEPARATOR . $file, $dst . DIRECTORY_SEPARATOR . $file); else  copy($src . DIRECTORY_SEPARATOR . $file, $dst . DIRECTORY_SEPARATOR . $file);
+        closedir($dir);
     }
 
     /**
@@ -145,51 +191,6 @@ class yxorP
         /* It's checking if the `$yxorP` variable is set, and if it is, it returns it, if it isn't, it creates a new
         instance of the `yxorP` class and sets the `$yxorP` variable to it. */
         return (self::$yxorP) ?: self::$yxorP = new self($request ?: $_SERVER);
-    }
-
-    /**
-     * It creates the plugin's directory if it doesn't exist, and installs the plugin if it's not already installed.
-     */
-    private static function init(): void
-    {
-        /* It's creating the constants that are used in the plugin. */
-        constants::create(__DIR__);
-        /* It's checking if the `http` and `minify` directories exist in the plugin directory, and if they don't, it
-        creates them. */
-        foreach (array(DIR_HTTP, DIR_MINIFY) as $_asset) generalHelper::fileCheck(DIR_ROOT . $_asset, true);
-        /* It's checking if the plugin directory exists, and if it doesn't, it creates it. */
-        foreach ([DIR_PLUGIN, PATH_DIR_TMP] as $_dir)
-            if (!is_dir($_dir)) if (!mkdir($_dir) && !is_dir($_dir))
-                throw new RuntimeException(sprintf(RUNTIME_EXCEPTION, $_dir));
-        /* It's checking if there are any users in the `cockpit_accounts` collection, and if there aren't, it's calling the
-        `install()` function. */
-        if (!constants::get(YXORP_COCKPIT_APP)->storage->getCollection(COCKPIT_ACCOUNTS)->count())
-            self::install();
-    }
-
-    /**
-     * It creates a new user with the credentials defined in the `.env` file
-     */
-    private static function install(): void
-    {
-        /* It's defining the `YXORP_COCKPIT_INSTALL` constant as `true`. */
-        define(YXORP_COCKPIT_INSTALL, true);
-
-        /* It's copying the files from the `local` directory to the `cockpit` directory. */
-        self::migrate(PATH_COCKPIT_LOCAL, PATH_DIR_COCKPIT);
-
-        /* It's creating an array of user data. */
-        $_account = [VAR_USER => constants::get(ENV_ADMIN_USER), VAR_NAME => constants::get(ENV_ADMIN_NAME), VAR_EMAIL => constants::get(ENV_ADMIN_EMAIL), VAR_ACTIVE => true, VAR_GROUP => VAR_ADMIN, VAR_PASSWORD => constants::get(YXORP_COCKPIT_APP)->hash(constants::get(ENV_ADMIN_PASSWORD)), VAR_I18N => constants::get(YXORP_COCKPIT_APP)->helper(VAR_I18N)->locale, VAR_CREATED => time(), VAR_MODIFIED => time()];
-        /* It's inserting a new user into the `cockpit_accounts` collection. */
-        constants::get(YXORP_COCKPIT_APP)->storage->insert(COCKPIT_ACCOUNTS, $_account);
-    }
-
-    public static function migrate($src, $dst)
-    {
-        $dir = opendir($src);
-        @mkdir($dst);
-        foreach (scandir($src) as $file) if (($file != CHAR_PERIOD) && ($file != CHAR_PERIOD . CHAR_PERIOD)) if (is_dir($src . DIRECTORY_SEPARATOR . $file)) self::migrate($src . DIRECTORY_SEPARATOR . $file, $dst . DIRECTORY_SEPARATOR . $file); else  copy($src . DIRECTORY_SEPARATOR . $file, $dst . DIRECTORY_SEPARATOR . $file);
-        closedir($dir);
     }
 
     /**
