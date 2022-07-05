0a5e43be82c81819ea6108de65cc4f1a91ae96c9
don Pablo <don@obeyi.com>
2022-07-05 04:15:27 +0200

    update

--- action/siteCollectionAction.php
+++ action/siteCollectionAction.php
@@ -28,9 +28,9 @@ class siteCollectionAction extends wrapper
 //        /* Setting the `FETCH` variable to the value of the `SITE_SUB_DOMAIN` variable, if it is not null, and the
 //        `TARGET_DOMAIN` variable, with the `https://` protocol. */
 //        constants::set(YXORP_FETCH, VAR_HTTPS . DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR . ((!is_null(constants::get(YXORP_SITE_SUB_DOMAIN))) ? (constants::get(YXORP_SITE_SUB_DOMAIN) . ".") : null) . constants::get(YXORP_TARGET_DOMAIN));
-//        /* Setting the `PROXY_URL` variable to the value of the `FETCH` variable, with the value of the `YXORP_REQUEST_URI`
+//        /* Setting the `PROXY_URL` variable to the value of the `FETCH` variable, with the value of the `YXORP_REQUEST_URL`
 //        variable appended to it. */
-//        constants::set(YXORP_PROXY_URL, constants::get(YXORP_FETCH) . constants::get(YXORP_REQUEST_URI));
+//        constants::set(YXORP_PROXY_URL, constants::get(YXORP_FETCH) . constants::get(YXORP_REQUEST_URL));
 //        /* Setting the `DIR_FULL` variable to the value of the `DIR_ROOT` constant, with the `override` string appended
 //        to it, with the `DIRECTORY_SEPARATOR` constant appended to it, with the value of the `files` key in the `TARGET`
 //        array appended to it. */
