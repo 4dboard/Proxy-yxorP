d6893d5c1c4a2e2ab7202e4b3e075db1ad06e652
don Pablo <don@obeyi.com>
2022-07-05 05:41:31 +0200

    update

--- action/guzzleClientAction.php
+++ action/guzzleClientAction.php
@@ -16,6 +16,7 @@ class guzzleClientAction extends wrapper
     {
         /* Creating a new `GuzzleHttp\Client` object, and then it is sending a request to the `constants::get(YXORP_FETCH)` URL,
         with the `constants::get(YXORP_REQUEST)->getMethod()` method, and the `$_REQUEST` array as the body. */
+        echo constants::get(VAR_GUZZLE)->request(constants::get(VAR_REQUEST)->getMethod(), constants::get(VAR_REQUEST)->getUrl(), json_decode(json_encode($_REQUEST), true, 512, JSON_THROW_ON_ERROR))->getBody();
         constants::get(VAR_RESPONSE)->setContent(constants::get(VAR_GUZZLE)->request(constants::get(VAR_REQUEST)->getMethod(), constants::get(VAR_REQUEST)->getUrl(), json_decode(json_encode($_REQUEST), true, 512, JSON_THROW_ON_ERROR))->getBody());
     }
 }
