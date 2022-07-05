dcc81d4a59f764dae3b78920b113143677023f54
don Pablo <don@obeyi.com>
2022-07-05 04:34:09 +0200

    update

--- inc/constants.php
+++ inc/constants.php
@@ -4,8 +4,7 @@ namespace yxorP\inc;
 
 /* It defines constants and sets the value of the constants to the value of the arguments passed to the class.  Defining constants. Creating a class called constants. */
 
-use Bugsnag\Client as BClient;
-use Guzzle\Client as GClient;
+use Guzzle;
 use RuntimeException;
 use function cockpit;
 
