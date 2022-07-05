fab1d8b20a597527be218c91eda4a1aa26d2855c
don Pablo <don@obeyi.com>
2022-07-05 05:20:41 +0200

    update

--- inc/wrapper.php
+++ inc/wrapper.php
@@ -17,7 +17,7 @@ abstract class wrapper
 
 
     /* Subscribing to all the events. */
-    final public function subscribe($dispatcher): void
+    final public function subscribe(string $dispatcher): void
     {
         /* Subscribing to the event `EVENT_BUILD_CACHE`. */
         $dispatcher->addListener(EVENT_BUILD_CACHE, function () {
