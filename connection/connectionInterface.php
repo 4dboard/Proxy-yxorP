b2a3cc8966c73418086b7b72304184e33b44bfd6
don Pablo <don@obeyi.com>
2022-07-05 05:38:40 +0200

    update

--- connection/connectionInterface.php
+++ connection/connectionInterface.php
@@ -1,19 +1,6 @@
 <?php namespace yxorP\connection;
 abstract class connectionInterface extends tcpConnection, tcpConnection, tcpConnection
 {
-    const CONNECT_FAIL = 1;
-    const SEND_FAIL = 2;
-    public
-    static $statistics = ['connection_count' => 0, 'total_request' => 0, 'throw_exception' => 0, 'send_fail' => 0,];
-    public
-    $onMessage = null;
-    public
-    $onClose = null;
-    public
-    $onError = null;
-    public
-    $protocolContext = [];
-
     abstract public function send($send_buffer)
 
     abstract public function getRemoteIp()
