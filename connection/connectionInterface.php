35fc3570608687a6b8788ff2ae74c20bf8501bc2
don Pablo <don@obeyi.com>
2022-07-05 05:39:13 +0200

    update

--- connection/connectionInterface.php
+++ connection/connectionInterface.php
@@ -1,23 +1,23 @@
 <?php namespace yxorP\connection;
 abstract class connectionInterface extends tcpConnection
 {
-    abstract public function send($send_buffer)
+    abstract public function send($send_buffer);
 
-    abstract public function getRemoteIp()
+    abstract public function getRemoteIp();
 
-    abstract public function getRemotePort()
+    abstract public function getRemotePort();
 
-    abstract public function getRemoteAddress()
+    abstract public function getRemoteAddress();
 
-    abstract public function getLocalIp()
+    abstract public function getLocalIp();
 
-    abstract public function getLocalPort()
+    abstract public function getLocalPort();
 
-    abstract public function getLocalAddress()
+    abstract public function getLocalAddress();
 
-    abstract public function isIPv4()
+    abstract public function isIPv4();
 
-    abstract public function isIPv6()
+    abstract public function isIPv6();
 
-    abstract public function close($data = null)
+    abstract public function close($data = null);
 }
