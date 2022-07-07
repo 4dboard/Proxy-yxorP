eeb09a7b4d92938f94125b51741c73c97776ef2f
don Pablo <don@obeyi.com>
2022-07-07 11:18:25 +0200

    update

--- inc/constants.php
+++ inc/constants.php
@@ -650,8 +650,8 @@ class constants
         constants::set(YXORP_REQUEST_URI, (constants::get(VAR_SERVER))[YXORP_REQUEST_URI]);
         /* Setting the `SITE_DOMAIN` variable to the result of the `extractDomain` method. */
         $YXORP_SITE_URL = constants::get(YXORP_SITE_URL);
-        $parser = parser->parseUrl($YXORP_SITE_URL);
-        constants::set($parser,);
+        $parser = Parser->parseUrl($YXORP_SITE_URL);
+        constants::set($parser, $parser);
         d(constants::get(YXORP_SITE_URL_PARSE));
         /* Setting the `YXORP_SITE_DOMAIN` variable to the result of the `extractDomain` method. */
         constants::set(YXORP_SITE_DOMAIN, generalHelper::extractDomain(constants::get(YXORP_SITE_URL)));
