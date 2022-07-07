0c61dee40259c4ce89c559ad3dcdc77beb7e7a09
don Pablo <don@obeyi.com>
2022-07-07 11:22:40 +0200

    update

--- parser/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.php
+++ parser/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.php
@@ -12,10 +12,16 @@ use function array_slice;
 use function count;
 use function explode;
 use function implode;
+use function is_array;
+use function parse_url;
 use function preg_match;
 use function preg_replace;
+use function preg_replace_callback;
+use function sprintf;
 use function strlen;
 use function strpos;
+use function urldecode;
+use function urlencode;
 
 class XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 {
@@ -29,6 +35,44 @@ class XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
     {
         $this->publicSuffixList = $publicSuffixList;
         $this->punycodeWrapper = new punycodeWrapper();
+    }
+
+    function pdp_parse_url($url, $component = -1)
+    {
+        $enc_url = preg_replace_callback(
+            '%([a-zA-Z][a-zA-Z0-9+\-.]*)?(:?//)?([^:/@?&=#\[\]]+)%u',
+            static function ($matches) {
+                $encoded = urlencode($matches[3]);
+
+                return sprintf('%s%s%s', $matches[1], $matches[2], $encoded);
+            },
+            $url
+        );
+
+        if ($enc_url === null) {
+            return false;
+        }
+
+        /** @var array|false|string $parts */
+        $parts = parse_url($enc_url, $component);
+
+        if ($parts === false) {
+            return $parts;
+        }
+
+        if (is_array($parts)) {
+            foreach ($parts as $name => &$value) {
+                if ($name === 'scheme') {
+                    continue;
+                }
+
+                $value = urldecode($value);
+            }
+        } else {
+            $parts = urldecode($parts);
+        }
+
+        return $parts;
     }
 
     public function parseUrl($url): url
