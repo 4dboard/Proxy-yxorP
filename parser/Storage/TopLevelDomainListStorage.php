fb4f30dc66fa61591cc43f75477d41008816bd7c
don Pablo <don@obeyi.com>
2022-07-07 20:35:18 +0200

    update

--- parser/Storage/TopLevelDomainListStorage.php
+++ parser/Storage/TopLevelDomainListStorage.php
@@ -1,10 +1,1 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Pdp\Storage;
-
-interface TopLevelDomainListStorage extends TopLevelDomainListClient
-{
-    public function delete(string $uri): bool;
-}
+<?php declare(strict_types=1);namespace Pdp\Storage;interface TopLevelDomainListStorage extends TopLevelDomainListClient{public function delete(string $uri):bool;}
--- parser/Storage/TopLevelDomainListStorageFactory.php
+++ parser/Storage/TopLevelDomainListStorageFactory.php
@@ -1,17 +1,1 @@
-<?php
-
-
-declare(strict_types=1);
-
-namespace Pdp\Storage;
-
-use DateInterval;
-use DateTimeInterface;
-
-interface TopLevelDomainListStorageFactory
-{
-    /**
-     * @param DateInterval|DateTimeInterface|object|int|string|null $cacheTtl storage TTL object should implement the __toString method
-     */
-    public function createTopLevelDomainListStorage(string $cachePrefix = '', $cacheTtl = null): TopLevelDomainListStorage;
-}
+<?php declare(strict_types=1);namespace Pdp\Storage;use DateInterval;use DateTimeInterface;interface TopLevelDomainListStorageFactory{public function createTopLevelDomainListStorage(string $cachePrefix='',$cacheTtl=null):TopLevelDomainListStorage;}
