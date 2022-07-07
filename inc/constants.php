96e1f9f9b1c5e6b763aae24a7d3c04038f4a7af0
don Pablo <don@obeyi.com>
2022-07-07 18:56:46 +0200

    update

--- inc/constants.php
+++ inc/constants.php
@@ -7,14 +7,9 @@ namespace yxorP\inc;
 use Bugsnag;
 use GuzzleHttp;
 use RuntimeException;
+use yxorP\parse\parse;
 use yxorP\parser\parseUrl;
 use function cockpit;
-use function is_array;
-use function parse_url;
-use function preg_replace_callback;
-use function sprintf;
-use function urldecode;
-use function urlencode;
 
 class constants
 {
@@ -715,40 +710,8 @@ class constants
 
     public static function pdp_parse_url($url, $component = -1)
     {
-        $enc_url = preg_replace_callback(
-            '%([a-zA-Z][a-zA-Z0-9+\-.]*)?(:?//)?([^:/@?&=#\[\]]+)%u',
-            static function ($matches) {
-                $encoded = urlencode($matches[3]);
-
-                return sprintf('%s%s%s', $matches[1], $matches[2], $encoded);
-            },
-            $url
-        );
-
-        if ($enc_url === null) {
-            return false;
-        }
-
-        /** @var array|false|string $parts */
-        $parts = parse_url($enc_url, $component);
-
-        if ($parts === false) {
-            return $parts;
-        }
-
-        if (is_array($parts)) {
-            foreach ($parts as $name => &$value) {
-                if ($name === 'scheme') {
-                    continue;
-                }
-
-                $value = urldecode($value);
-            }
-        } else {
-            $parts = urldecode($parts);
-        }
-
-        return $parts;
+        $parse = new parse();
+        retrun = $parse->urldecode($value);
     }
 
 }
