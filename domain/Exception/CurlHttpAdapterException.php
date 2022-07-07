f2e5e88c464e53569f11199acd820d265eae901f
don Pablo <don@obeyi.com>
2022-07-07 09:47:38 +0200

    update

--- domain/Exception/CurlHttpAdapterException.php
+++ domain/Exception/CurlHttpAdapterException.php
@@ -1,4 +1,12 @@
-<?php namespace yxorp\domain\Exception;
+<?php
+
+
+namespace yxorp\domain\Exception;
+
+
+require Constants::get(ROOT_DIR) . "domain/exception/pdpException.php";
+require Constants::get(ROOT_DIR) . "domain/httpAdapter/httpAdapterInterface.php";
+
 class curlHttpAdapterException extends httpAdapterException
 {
 }
