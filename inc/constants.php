7bbc1d7bc55fbeb7d6a3215ace532fadc3e2d5c1
don Pablo <don@obeyi.com>
2022-07-07 11:23:44 +0200

    update

--- inc/constants.php
+++ inc/constants.php
@@ -711,4 +711,42 @@ class constants
         return $GLOBALS[$_name];
     }
 
+    public static function pdp_parse_url($url, $component = -1)
+    {
+        $enc_url = \preg_replace_callback(
+            '%([a-zA-Z][a-zA-Z0-9+\-.]*)?(:?//)?([^:/@?&=#\[\]]+)%u',
+            static function ($matches) {
+                $encoded = \urlencode($matches[3]);
+
+                return \sprintf('%s%s%s', $matches[1], $matches[2], $encoded);
+            },
+            $url
+        );
+
+        if ($enc_url === null) {
+            return false;
+        }
+
+        /** @var array|false|string $parts */
+        $parts = \parse_url($enc_url, $component);
+
+        if ($parts === false) {
+            return $parts;
+        }
+
+        if (\is_array($parts)) {
+            foreach ($parts as $name => &$value) {
+                if ($name === 'scheme') {
+                    continue;
+                }
+
+                $value = \urldecode($value);
+            }
+        } else {
+            $parts = \urldecode($parts);
+        }
+
+        return $parts;
+    }
+
 }
