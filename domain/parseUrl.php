806b3bf6571e7f10ac19dca634d1b474dd297793
don Pablo <don@obeyi.com>
2022-07-07 09:39:16 +0200

    update

--- domain/parseUrl.php
+++ domain/parseUrl.php
@@ -1,29 +1,30 @@
-<?php namespace {
-    if (!function_exists('pdp_parse_url')) {
-        function pdp_parse_url($url, $component = -1)
-        {
-            $enc_url = preg_replace_callback('%([a-zA-Z][a-zA-Z0-9+\-.]*)?(:?//)?([^:/@?&=#\[\]]+)%u', static function ($matches) {
-                $encoded = urlencode($matches[3]);
-                return sprintf('%s%s%s', $matches[1], $matches[2], $encoded);
-            }, $url);
-            if ($enc_url === null) {
-                return false;
-            }
-            $parts = parse_url($enc_url, $component);
-            if ($parts === false) {
-                return $parts;
-            }
-            if (is_array($parts)) {
-                foreach ($parts as $name => &$value) {
-                    if ($name === 'scheme') {
-                        continue;
-                    }
-                    $value = urldecode($value);
-                }
-            } else {
-                $parts = urldecode($parts);
-            }
+<?php namespace yxorp\domain;
+
+class parseUrl
+{
+    function pdp_parse_url($url, $component = -1)
+    {
+        $enc_url = preg_replace_callback('%([a-zA-Z][a-zA-Z0-9+\-.]*)?(:?//)?([^:/@?&=#\[\]]+)%u', static function ($matches) {
+            $encoded = urlencode($matches[3]);
+            return sprintf('%s%s%s', $matches[1], $matches[2], $encoded);
+        }, $url);
+        if ($enc_url === null) {
+            return false;
+        }
+        $parts = parse_url($enc_url, $component);
+        if ($parts === false) {
             return $parts;
         }
+        if (is_array($parts)) {
+            foreach ($parts as $name => &$value) {
+                if ($name === 'scheme') {
+                    continue;
+                }
+                $value = urldecode($value);
+            }
+        } else {
+            $parts = urldecode($parts);
+        }
+        return $parts;
     }
 }
