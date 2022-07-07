7b0ad34e8964a03fd6f8a96d671000a407d2ade3
don Pablo <don@obeyi.com>
2022-07-07 11:24:27 +0200

    update

--- inc/constants.php
+++ inc/constants.php
@@ -9,6 +9,12 @@ use GuzzleHttp;
 use RuntimeException;
 use yxorP\parser\parseUrl;
 use function cockpit;
+use function is_array;
+use function parse_url;
+use function preg_replace_callback;
+use function sprintf;
+use function urldecode;
+use function urlencode;
 use const yxorP\domain\urlParser;
 use const yxorP\domain\XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX;
 use const yxorP\parser\parser;
@@ -713,12 +719,12 @@ class constants
 
     public static function pdp_parse_url($url, $component = -1)
     {
-        $enc_url = \preg_replace_callback(
+        $enc_url = preg_replace_callback(
             '%([a-zA-Z][a-zA-Z0-9+\-.]*)?(:?//)?([^:/@?&=#\[\]]+)%u',
             static function ($matches) {
-                $encoded = \urlencode($matches[3]);
+                $encoded = urlencode($matches[3]);
 
-                return \sprintf('%s%s%s', $matches[1], $matches[2], $encoded);
+                return sprintf('%s%s%s', $matches[1], $matches[2], $encoded);
             },
             $url
         );
@@ -728,22 +734,22 @@ class constants
         }
 
         /** @var array|false|string $parts */
-        $parts = \parse_url($enc_url, $component);
+        $parts = parse_url($enc_url, $component);
 
         if ($parts === false) {
             return $parts;
         }
 
-        if (\is_array($parts)) {
+        if (is_array($parts)) {
             foreach ($parts as $name => &$value) {
                 if ($name === 'scheme') {
                     continue;
                 }
 
-                $value = \urldecode($value);
+                $value = urldecode($value);
             }
         } else {
-            $parts = \urldecode($parts);
+            $parts = urldecode($parts);
         }
 
         return $parts;
