06f299aefb252eda97c9460d6b34ed41a26aafba
don Pablo <don@obeyi.com>
2022-07-08 02:57:55 +0200

    update

--- credentials.py
+++
@@ -1,7 +0,0 @@
-# credentials.py
-
-# Make sure this is a long random string
-SECRET_KEY = '8ewhr9y489yt98w4yr89f9w4y430y98ewfyrfy9'
-
-# Replace instances of <> with the database credentials
-DB_URL = 'mysql+pymysql://don:deviant69ne1*@192.168.1.47/DLU'
