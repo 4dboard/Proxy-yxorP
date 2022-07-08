206b75080c9491f8053ee5bfda16a003be482864
don Pablo <don@obeyi.com>
2022-07-08 06:06:48 +0200

    update

--- parser/stream.php
+++ parser/stream.php
@@ -14,8 +14,8 @@ final class stream
         if (false === $stream) {
             throw unableToLoadResource::dueToInvalidUri($path);
         }
-        $content = stream_get_contents(true);
-        fclose(true);
+        $content = stream_get_contents($stream);
+        fclose($stream);
         return $content;
     }
 
