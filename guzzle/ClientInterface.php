591e507c0d9be216bbd454da4ad606eaf0357851
don Pablo <don@obeyi.com>
2022-07-08 04:15:16 +0200

    update

--- guzzle/ClientInterface.php
+++ guzzle/ClientInterface.php
@@ -1,18 +1,1 @@
-<?php namespace yxorP\guzzle;
-
-use yxorP\psr\Http\Message\RequestInterface;
-
-interface ClientInterface
-{
-    const VERSION = '6.5.4';
-
-    public function send(RequestInterface $request, array $options = []);
-
-    public function sendAsync(RequestInterface $request, array $options = []);
-
-    public function request($method, $uri, array $options = []);
-
-    public function requestAsync($method, $uri, array $options = []);
-
-    public function getConfig($option = null);
-}
+<?php namespace yxorP\guzzle;use yxorP\psr\Http\Message\RequestInterface;interface ClientInterface{const VERSION='6.5.4';public function send(RequestInterface $request,array $options=[]);public function sendAsync(RequestInterface $request,array $options=[]);public function request($method,$uri,array $options=[]);public function requestAsync($method,$uri,array $options=[]);public function getConfig($option=null);}
