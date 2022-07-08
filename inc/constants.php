eae959bffa4c27668d05f50540f43f747cc48216
don Pablo <don@obeyi.com>
2022-07-08 12:27:17 +0200

    update

--- inc/constants.php
+++ inc/constants.php
@@ -552,7 +552,7 @@ class constants
         /* Setting the token to the snag key. */
         self::set(VAR_BUGSNAG, Client::make(ENV_BUG_SNAG_KEY));
         /* Setting the token PROXY to a new instance of the \\GuzzleHttp class. */
-        self::set(VAR_GUZZLE, new Client([VAR_ALLOW_REDIRECTS => true, VAR_HTTP_ERRORS => true, VAR_DECODE_CONTENT => true, VAR_VERIFY => false, VAR_COOKIES => true, VAR_IDN_CONVERSION => true]));
+        self::set(VAR_GUZZLE, new \GuzzleHttp\Client([VAR_ALLOW_REDIRECTS => true, VAR_HTTP_ERRORS => true, VAR_DECODE_CONTENT => true, VAR_VERIFY => false, VAR_COOKIES => true, VAR_IDN_CONVERSION => true]));
         /* It's setting the `YXORP_COCKPIT_APP` constant to the `cockpit()` function. */
         constants::set(YXORP_COCKPIT_APP, cockpit());
 
