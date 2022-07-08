b1066f857f4b8c8c3c410f885b794b4c55fba23b
don Pablo <don@obeyi.com>
2022-07-08 03:04:15 +0200

    update

--- inc/constants.php
+++ inc/constants.php
@@ -641,11 +641,9 @@ class constants
         self::set(VAR_SERVER, $req);
 
         /* Setting the `SITE_URL` variable to the value of the `SERVER_NAME` key in the `YXORP_SERVER` array. */
-        constants::set(YXORP_SITE_URL, (constants::get(VAR_SERVER)[YXORP_SERVER_NAME]));
-
-        exit(var_dump(constants::get(VAR_SERVER)));
+        constants::set(YXORP_SITE_URL, (constants::get(VAR_SERVER)[YXORP_HTTP_HOST]));
         /* Setting the `YXORP_TARGET_PLUGINS` variable to the result of the `YXORP_PLUGINS` method. */
-        constants::set(YXORP_REQUEST_URI, (constants::get(VAR_SERVER))[YXORP_HTTP_HOST]);
+        constants::set(YXORP_REQUEST_URI, (constants::get(VAR_SERVER))[YXORP_REQUEST_URI]);
         /* Setting the `SITE_DOMAIN` variable to the result of the `extractDomain` method. */
         constants::set(YXORP_SITE_URL_PARSE, self::publicSuffix((constants::get(YXORP_SITE_URL))));
         /* Setting the `YXORP_SITE_DOMAIN` variable to the result of the `extractDomain` method. */
