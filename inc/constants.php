563225e12d08d6e5b7d60ec4bdd0901928835565
don Pablo <don@obeyi.com>
2022-07-08 03:20:45 +0200

    update

--- inc/constants.php
+++ inc/constants.php
@@ -689,6 +689,8 @@ class constants
         /* Setting the `PATTERN` context variable to the value of the `pattern` type in the `global` collection. */
         constants::set(YXORP_GLOBAL_PATTERN, constants::get(YXORP_COCKPIT_APP)->storage->findOne(COCKPIT_COLLECTIONS . CHAR_SLASH . VAR_GLOBAL, [VAR_TYPE => VAR_PATTERN]) ?
             (constants::get(YXORP_COCKPIT_APP)->storage->findOne(COCKPIT_COLLECTIONS . CHAR_SLASH . VAR_GLOBAL, [VAR_TYPE => VAR_PATTERN]))[VAR_VALUE] : null);
+
+        print_r($GLOBALS);
     }
 
     /* A static method that returns the value of the $_name variable. */
