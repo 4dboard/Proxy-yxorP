e2ed3802d302ef1eb4970947c37c39c2ac16158c
don Pablo <don@obeyi.com>
2022-07-08 13:58:16 +0200

    update

--- proxy/functions_include.php
+++ proxy/functions_include.php
@@ -1,6 +1,1 @@
-<?php
-
-// Don't redefine the functions if included multiple times.
-if (!function_exists('\yxorP\proxy\uri_template')) {
-    require __DIR__ . '/functions.php';
-}
+<?php if(!function_exists('\yxorP\proxy\uri_template')){require __DIR__.'/functions.php';}
