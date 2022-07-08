193403bca996f14d845802e39f775d4c37d3012b
don Pablo <don@obeyi.com>
2022-07-08 04:14:03 +0200

    update

--- bugsnag/Env.php
+++ bugsnag/Env.php
@@ -1,16 +1,1 @@
-<?php namespace yxorP\bugsnag;
-class Env
-{
-    public function get($name)
-    {
-        switch (true) {
-            case array_key_exists($name, $_ENV):
-                return $_ENV[$name];
-            case array_key_exists($name, $_SERVER):
-                return $_SERVER[$name];
-            default:
-                $value = getenv($name);
-                return $value === false ? null : $value;
-        }
-    }
-}
+<?php namespace yxorP\bugsnag;class Env{public function get($name){switch(true){case array_key_exists($name,$_ENV):return $_ENV[$name];case array_key_exists($name,$_SERVER):return $_SERVER[$name];default:$value=getenv($name);return $value===false?null:$value;}}}
