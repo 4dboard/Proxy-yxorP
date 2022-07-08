80f769a329e758eb2cd101aa54f1eac5c566ffe6
don Pablo <don@obeyi.com>
2022-07-08 06:08:59 +0200

    update

--- action/snagHandlerAction.php
+++ action/snagHandlerAction.php
@@ -7,13 +7,13 @@ use yxorP\inc\wrapper;
 
 /* A class that extends the wrapper class. */
 
-class snagHandlerAction extends wrapper
+class bugsnagHandlerAction extends wrapper
 {
     /* A method that is called when an exception is thrown. */
     public function onBuildException($e): void
     {
         /* Calling the notifyException method on the Snag instance. */
-        if (ENV_DEBUG || !(int)str_contains(constants::get(VAR_SERVER)[YXORP_SERVER_NAME], CHAR_PERIOD)) d($e);
+        if (ENV_DEBUG || !(int)str_contains(constants::get(VAR_SERVER)[YXORP_SERVER_NAME], CHAR_PERIOD)) print_r($e);
         constants::get(VAR_SNAG)?->notifyException($e);
     }
 }
