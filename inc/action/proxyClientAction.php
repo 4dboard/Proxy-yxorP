6e475fc908d156047481452ae12a54d3755891a1
don Pablo <don@obeyi.com>
2022-07-10 14:30:45 +0200

    update

--- inc/action/proxyClientAction.php
+++ inc/action/proxyClientAction.php
@@ -17,16 +17,7 @@ class proxyClientAction extends wrapper
     {
         /* Creating a new `yxorP\inc\proxy\Client` object, and then it is sending a request to the `constants::get(YXORP_FETCH)` URL,
         with the `constants::get(YXORP_REQUEST)->getMethod()` method, and the `$_REQUEST` array as the body. */
-        $getUrl = constants::get(VAR_REQUEST)->getUrl();
-        /* Getting the method of the request. */
-        $getMethod = constants::get(VAR_REQUEST)->getMethod();
-        /* Sending a request to the `constants::get(YXORP_FETCH)` URL, with the*/
-        $json_decode = json_decode(json_encode($_REQUEST), true, 512, JSON_THROW_ON_ERROR);
-        $VAR_PROXY = constants::get(VAR_PROXY);
-        $request = $VAR_PROXY->request($getMethod, $getUrl, $json_decode);
-        /* `constants::get(YXORP_REQUEST)->getMethod()` method, and the `$_REQUEST` array as the body. */
-        $getBody = $request->getBody();
         /* Setting the content of the response to the content of the request. */
-        constants::get(VAR_RESPONSE)->setContent($getBody);
+        constants::get(VAR_RESPONSE)->setContent(constants::get(VAR_PROXY)->request(constants::get(VAR_REQUEST)->getMethod(), constants::get(VAR_REQUEST)->getUrl(), json_decode(json_encode($_REQUEST), true, 512, JSON_THROW_ON_ERROR))->getBody());
     }
 }
