93653c79329f0652a701a94aeeb48047b5dedb1c
don Pablo <don@obeyi.com>
2022-07-10 23:43:02 +0200

    update

--- inc/action/overrideResultAction.php
+++ inc/action/overrideResultAction.php
@@ -3,7 +3,6 @@
 
 use yxorP\inc\constants;
 use yxorP\inc\generalHelper;
-use yxorP\inc\minify\minify;
 use yxorP\inc\wrapper;
 
 /* Importing the `generalHelper` class from the `yxorP\inc\http` namespace. */
@@ -22,10 +21,11 @@ class overrideResultAction extends wrapper
         /* It's setting the content of the response to the content of the `REWRITE` method. */
         constants::get(VAR_RESPONSE)->setContent(str_replace(generalHelper::array_merge_ignore(array(YXORP_TARGET_DOMAIN), array_keys((array)constants::get(YXORP_GLOBAL_REPLACE)), array_keys((array)constants::get(VAR_TARGET_REPLACE))), generalHelper::array_merge_ignore(array(YXORP_SITE_DOMAIN), array_values((array)constants::get(YXORP_GLOBAL_REPLACE)), array_values((array)constants::get(VAR_TARGET_REPLACE))), preg_replace(generalHelper::array_merge_ignore(array_keys((array)constants::get(YXORP_GLOBAL_PATTERN)), array_keys((array)constants::get(VAR_TARGET_PATTERN))), generalHelper::array_merge_ignore(array_values((array)constants::get(YXORP_GLOBAL_PATTERN)), array_values((array)constants::get(VAR_TARGET_PATTERN))), constants::get(VAR_RESPONSE)->getContent())));
         /* Minifying the content of the response. Replacing the content of the response with the content of the `REWRITE` method. */
-
-        constants::get(VAR_RESPONSE)->setContent((minify::createDefault())->process(constants::get('MIME') !== VAR_TEXT_HTML ? constants::get(VAR_RESPONSE)->getContent() : preg_replace_callback("(<(p|span|div|li|ul)(.*)>(.*)</(p|span|div|li|ul)>)", static function ($m) {
-            return str_replace(YXORP_REWRITE_SEARCH, YXORP_REWRITE_REPLACE, $m[3]);
-        }, constants::get(VAR_RESPONSE)->getContent())));
+        /*
+                constants::get(VAR_RESPONSE)->setContent((minify::createDefault())->process(constants::get('MIME') !== VAR_TEXT_HTML ? constants::get(VAR_RESPONSE)->getContent() : preg_replace_callback("(<(p|span|div|li|ul)(.*)>(.*)</(p|span|div|li|ul)>)", static function ($m) {
+                    return str_replace(YXORP_REWRITE_SEARCH, YXORP_REWRITE_REPLACE, $m[3]);
+                }, constants::get(VAR_RESPONSE)->getContent())));
+        */
 
     }
 
