7d14708460d8a3c0dd8d257fd2463b1444f8aae7
don Pablo <don@obeyi.com>
2022-07-10 21:59:58 +0200

    update

--- inc/action/debugHandlerAction.php
+++ inc/action/debugHandlerAction.php
@@ -14,7 +14,7 @@ class debugHandlerAction extends wrapper
     {
         /* Checking if the `DEBUG` constant is set to `true` or if the server name does not contain a dot. If either of
         these conditions are met, the exception is printed. */
-        if (ENV_DEBUG || !(int)str_contains(constants::get(VAR_SERVER)[YXORP_HTTP_HOST], CHAR_PERIOD)) echo $e->__toString();
+        if (ENV_DEBUG || !(int)str_contains(constants::get(VAR_SERVER)[YXORP_HTTP_HOST], CHAR_PERIOD)) echo var_dump($e);
 
     }
 }
