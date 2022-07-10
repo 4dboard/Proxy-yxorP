3f7952d714dbd84f60b99cf44ec5cc25f8e00ea9
don Pablo <don@obeyi.com>
2022-07-10 21:18:10 +0200

    update

--- inc/yP.php
+++ inc/yP.php
@@ -40,7 +40,6 @@ class yP
         /* It's checking if the request URI contains the cockpit directory, and if it does, it requires the cockpit index
         file. */
 
-        exit('123');
         foreach ([DIR_INC . DIR_ACTION => scandir(DIR_ROOT . DIR_INC . DIR_ACTION), DIR_PLUGIN => constants::get(YXORP_TARGET_PLUGINS)] as $key => $value) foreach ($value as $action) $this->subscribe($key, $action);
     }
 
