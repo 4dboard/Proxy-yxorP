0bd6dcf6d87efc2bfcc90f423e5891268698aed2
don Pablo <don@obeyi.com>
2022-07-10 14:33:25 +0200

    update

--- inc/generalHelper.php
+++ inc/generalHelper.php
@@ -224,6 +224,7 @@ class generalHelper
 
     public static function CSV($filename): array
     {
+        echo $filename;
         $csvArray = array_map(VAR_GETCSV, file($filename));
         return array_merge(...$csvArray);
     }
