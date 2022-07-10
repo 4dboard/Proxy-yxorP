3628c891c823e62caabaaee41a3cdfc8a9c5b661
don Pablo <don@obeyi.com>
2022-07-10 20:55:44 +0200

    update

--- inc/constants.php
+++ inc/constants.php
@@ -624,7 +624,8 @@ class constants
         $siteDomain = self::publicSuffix(YXORP_SITE_URL ?: constants::get(ENV_DEFAULT_SITE));
         /* Setting the `YXORP_SITE_DOMAIN` variable to the result of the `extractDomain` method. */
         define('YXORP_SITE_DOMAIN', $siteDomain->registrableDomain()->toString() ?: $siteDomain->domain()->toString());
-        /* Defining a constant. */
+
+        // --
 
 
         /* Setting the `TARGET` variable to the result of the `findOne` method. */
@@ -673,7 +674,7 @@ class constants
         define('CACHE_KEY', generalHelper::base64_url_encode(YXORP_PROXY_URL) . EXT_TMP);
 
         // GLOBAL COLLECTIONS
-        
+
         /* Setting the `REPLACE` context variable to the value of the `replace` type in the `global` collection. */
         constants::set(YXORP_GLOBAL_REPLACE, constants::get(YXORP_COCKPIT_APP)->storage->findOne(COCKPIT_COLLECTIONS . CHAR_SLASH . VAR_GLOBAL, [VAR_TYPE => VAR_REPLACE]) ?
             (constants::get(YXORP_COCKPIT_APP)->storage->findOne(COCKPIT_COLLECTIONS . CHAR_SLASH . VAR_GLOBAL, [VAR_TYPE => VAR_REPLACE]))[VAR_VALUE] : null);
