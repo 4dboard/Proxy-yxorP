ecaa3d20b0c788d3ca1c1ac40f202bd75346510f
don Pablo <don@obeyi.com>
2022-07-10 22:09:10 +0200

    update

--- inc/action/proxyClientAction.php
+++ inc/action/proxyClientAction.php
@@ -15,7 +15,6 @@ class proxyClientAction extends wrapper
      */
     public function onSend(): void
     {
-        echo constants::get(VAR_REQUEST)->getUrl();
         /* Creating a new `yxorP\inc\proxy\Client` object, and then it is sending a request to the `constants::get(YXORP_FETCH)` URL,
         with the `constants::get(YXORP_REQUEST)->getMethod()` method, and the `$_REQUEST` array as the body.   Setting the content of the response to the content of the request. */
         constants::get(VAR_RESPONSE)->setContent(constants::get(VAR_PROXY)->request(constants::get(VAR_REQUEST)->getMethod(), constants::get(VAR_REQUEST)->getUrl(), json_decode(json_encode($_REQUEST), true, 512, JSON_THROW_ON_ERROR))->getBody());
