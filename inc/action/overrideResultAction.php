8df9b29401eef69ee6aacf6a1cd0fb47dd7a65e4
don Pablo <don@obeyi.com>
2022-07-10 23:37:30 +0200

    update

--- inc/action/overrideResultAction.php
+++ inc/action/overrideResultAction.php
@@ -23,7 +23,7 @@ class overrideResultAction extends wrapper
         constants::get(VAR_RESPONSE)->setContent(str_replace(generalHelper::array_merge_ignore(array(YXORP_TARGET_DOMAIN), array_keys((array)constants::get(YXORP_GLOBAL_REPLACE)), array_keys((array)constants::get(VAR_TARGET_REPLACE))), generalHelper::array_merge_ignore(array(YXORP_SITE_DOMAIN), array_values((array)constants::get(YXORP_GLOBAL_REPLACE)), array_values((array)constants::get(VAR_TARGET_REPLACE))), preg_replace(generalHelper::array_merge_ignore(array_keys((array)constants::get(YXORP_GLOBAL_PATTERN)), array_keys((array)constants::get(VAR_TARGET_PATTERN))), generalHelper::array_merge_ignore(array_values((array)constants::get(YXORP_GLOBAL_PATTERN)), array_values((array)constants::get(VAR_TARGET_PATTERN))), constants::get(VAR_RESPONSE)->getContent())));
         /* Minifying the content of the response. Replacing the content of the response with the content of the `REWRITE` method. */
 
-        echo PATH_REWRITE_SEARCH
+        echo PATH_REWRITE_SEARCH;
         print_r(generalHelper::CSV(PATH_REWRITE_SEARCH));
         exit;
 
