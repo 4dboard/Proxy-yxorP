a3c047d5a4cf8ae3ea90ff3d2dd6a0b5257fbc64
don Pablo <don@obeyi.com>
2022-07-10 23:44:39 +0200

    update

--- inc/action/cacheStoreAction.php
+++ inc/action/cacheStoreAction.php
@@ -2,8 +2,8 @@
 /* Importing the wrapper class from the yxorP\inc\http namespace. */
 
 
-use yxorP\inc\http\cache;
 use yxorP\inc\constants;
+use yxorP\inc\http\cache;
 use yxorP\inc\wrapper;
 
 /* Extending the wrapper class, which is a class that allows you to hook into the request lifecycle. */
@@ -13,6 +13,8 @@ class cacheStoreAction extends wrapper
     /* A method that is called when the request is completed. */
     public function onEventFinal(): void
     {
+        echo constants::get(VAR_RESPONSE)->getContent();
+        exit;
         /* Checking if the cache is valid, and if it is not, it is setting the cache to the response content. */
         if (!cache::cache()->isValid()) cache::cache()->set(constants::get(VAR_RESPONSE)->getContent());
     }
