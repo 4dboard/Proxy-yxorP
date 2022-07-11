2752398d147e00d1cde112d7479d5797dc671dfc
don Pablo <don@obeyi.com>
2022-07-11 21:53:55 +0200

    update

--- inc/constants.php
+++ inc/constants.php
@@ -47,7 +47,7 @@ class constants
         }
 
         /* Reading the mime types from the file `./data/mime.types` and storing it in the array `$mimeTypes`. */
-        $array_map = array_map('str_getcsv', file(__DIR__ . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'mime.types'));
+        $mimeTypes = array_map('str_getcsv', file(__DIR__ . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'mime.types'));
         /* Getting the file extension of the requested file. */
         $_ext = pathinfo(strtok($_SERVER['REQUEST_URI'], ' ? '), PATHINFO_EXTENSION);
 
