3983b7b0953f4d3586ffeb9f1406f7575c63b083
don Pablo <don@obeyi.com>
2022-07-12 00:05:15 +0200

    update

--- inc/http/cache.php
+++ inc/http/cache.php
@@ -70,15 +70,16 @@ class cache
     public function set($val): void
     {
         $fopen = fopen(PATH_DIR_CACHE_FULL, 'w') or die("Unable to open file!");
-        is_type($val, 'array') ? fwrite($fopen, '<?php $' . CACHE_KEY . ' = ' . var_export($val, true) . ';') : fwrite($fopen, '<?php $' . CACHE_KEY . ' = ' . $val . ';');
+        if (!is_type($fopen, 'resource')) {
 
-        if (!is_dir(PATH_DIR_CACHE)) {
-            /* It's creating the `tmp` directory. */
-            if (!mkdir($concurrentDirectory = PATH_DIR_CACHE, 0777, true) && !is_dir($concurrentDirectory)) {
-                throw new RuntimeException(sprintf('Directory "%s" was not created', $concurrentDirectory));
+            if (!is_dir(PATH_DIR_CACHE)) {
+                /* It's creating the `tmp` directory. */
+                if (!mkdir($concurrentDirectory = PATH_DIR_CACHE, 0777, true) && !is_dir($concurrentDirectory)) {
+                    throw new RuntimeException(sprintf('Directory "%s" was not created', $concurrentDirectory));
+                }
+                /* It's setting the permissions of the `tmp` directory to `777`. */
+                chmod(PATH_DIR_CACHE, 0777);
             }
-            /* It's setting the permissions of the `tmp` directory to `777`. */
-            chmod(PATH_DIR_CACHE, 0777);
         }
         fwrite($fopen, '<?=' . str_replace('stdClass::__set_state', '(object)', var_export($val, true)) . ';exit;?>');
         fclose($fopen);
