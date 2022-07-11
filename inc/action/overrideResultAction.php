5501bda77c5b13fdbe8e759871cb946b7bff8201
don Pablo <don@obeyi.com>
2022-07-12 01:02:50 +0200

    update

--- inc/action/overrideResultAction.php
+++ inc/action/overrideResultAction.php
@@ -21,16 +21,15 @@ class overrideResultAction extends wrapper
         if (MIME !== VAR_TEXT_HTML && MIME !== 'application/javascript' && MIME !== 'text/css' && MIME !== 'application/xml' && !str_contains(MIME, VAR_TEXT) && !str_contains(MIME, VAR_HTML)) return;
         /* It's setting the content of the response to the content of the `REWRITE` method. */
         self::replace(constants::get(VAR_RESPONSE)->setContent(str_replace(generalHelper::array_merge_ignore(array(YXORP_TARGET_DOMAIN), array_keys((array)constants::get(YXORP_GLOBAL_REPLACE)), array_keys((array)constants::get(VAR_TARGET_REPLACE))), generalHelper::array_merge_ignore(array(YXORP_SITE_DOMAIN), array_values((array)constants::get(YXORP_GLOBAL_REPLACE)), array_values((array)constants::get(VAR_TARGET_REPLACE))), preg_replace(generalHelper::array_merge_ignore(array_keys((array)constants::get(YXORP_GLOBAL_PATTERN)), array_keys((array)constants::get(VAR_TARGET_PATTERN))), generalHelper::array_merge_ignore(array_values((array)constants::get(YXORP_GLOBAL_PATTERN)), array_values((array)constants::get(VAR_TARGET_PATTERN))), constants::get(VAR_RESPONSE)->getContent()))));
-        /* Minifying the content of the response. Replacing the content of the response with the content of the `REWRITE` method. */
-
-
     }
 
     /* Overriding the `onEventWrite` method of the `wrapper` class. */
     private static function replace($content)
     {
+        /* Minifying the content of the response. Replacing the content of the response with the content of the `REWRITE` method. */
         constants::get(VAR_RESPONSE)->setContent((minify::createDefault())->process(MIME !== VAR_TEXT_HTML ? $content : preg_replace_callback("(<(p|span|div|li|ul)(.*)>(.*)</(p|span|div|li|ul)>)", static function ($m) {
             return str_replace(YXORP_REWRITE_SEARCH, YXORP_REWRITE_REPLACE, $m[3]);
         }, $content)));
 
     }
+}
