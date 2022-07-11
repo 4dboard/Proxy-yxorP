bb7501f3e213da5884b44c4d2b5eb7071f23c961
don Pablo <don@obeyi.com>
2022-07-11 23:13:00 +0200

    update

--- inc/parser/domainNameResolverInterface.php
+++ inc/parser/domainNameResolverInterface.php
@@ -3,5 +3,5 @@
 namespace yxorP\inc\parser;
 interface domainNameResolverInterface
 {
-    public function resolve(aHostInterface $host): resolvedInterfaceDomainNameInterface;
+    public function resolve(aHostInterface $host): mixed;
 }
