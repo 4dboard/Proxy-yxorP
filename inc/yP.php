0b1c06d4856c01f9ad279f9eb5453e153062dd7f
don Pablo <don@obeyi.com>
2022-07-11 19:16:42 +0200

    update

--- inc/yP.php
+++ inc/yP.php
@@ -90,6 +90,34 @@ class yP
     {
         /* It's checking if the `tmp` directory exists, and if it doesn't, it creates it. */
         cacheHelper::check($yxorp_root);
+
+        // REQUIRED
+        /* Requiring the Cockpit library. */
+        require PATH_COCKPIT_BOOTSTRAP;
+        /* Requiring the Wrapper. */
+        require PATH_INC_WRAPPER;
+        // ENV
+        /* Reading the file and then calling the env function on each line. */
+        foreach (file(DIR_ROOT . EXT_ENV) as $line) self::env($line);
+        /* It's checking if the `http` and `minify` directories exist in the plugin directory, and if they don't, it
+        creates them. */
+
+        foreach (array(DIR_PSR, DIR_PROXY, DIR_SNAG, DIR_HTTP, DIR_MINIFY, DIR_PARSER) as $_asset) generalHelper::fileCheck(DIR_ROOT . DIR_INC . $_asset, true);        // Reporting
+
+        /* Setting the token to the snag key. */
+        self::set(VAR_SNAG, snag\Client::make(ENV_BUG_SNAG_KEY));
+        /* Setting the token PROXY to a new instance of the \yxorP\inc\proxy class. */
+
+        self::set(VAR_PROXY, new proxy\Client([VAR_ALLOW_REDIRECTS => true, VAR_HTTP_ERRORS => true, VAR_DECODE_CONTENT => true, VAR_VERIFY => false, VAR_COOKIES => true, VAR_IDN_CONVERSION => true]));
+
+        /* It's setting the `YXORP_COCKPIT_APP` constant to the `cockpit()` function. */
+        constants::set(YXORP_COCKPIT_APP, cockpit());
+
+        // EVENTS
+        constants::set(YXORP_EVENT_LIST, [EVENT_BUILD_CACHE, EVENT_BUILD_CONTEXT, EVENT_BUILD_INCLUDES, EVENT_BUILD_HEADERS, EVENT_BUILD_REQUEST, EVENT_BEFORE_SEND, EVENT_SEND, EVENT_SENT, EVENT_WRITE, EVENT_COMPLETE, EVENT_FINAL]);
+
+        constants::set(YXORP_MIME_TYPES, generalHelper::CSV(PATH_FILE_MIME_TYPES));
+        
         /* It's looping through all the events in the `init()` function and dispatching them to the `yxorP()` function */
         foreach (self::init($yxorp_root) as $event) self::yxorP($request ?: $_SERVER)->dispatch($event);
     }
