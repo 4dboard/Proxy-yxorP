ddc3a2de4e4dac9df4f32ea55da637453caf783c
don Pablo <don@obeyi.com>
2022-07-11 19:03:29 +0200

    update

--- inc/cacheHelper.php
+++ inc/cacheHelper.php
@@ -3,59 +3,53 @@
 /* Defining a constant named `CHAR_SLASH` with the value `/`. */
 define('CHAR_SLASH', '/');
 
-class cacheHelper
-{
-    public static function check($yxorp_root)
+try {
+    /* Checking if the requested URI contains the string `$t`. */
+    /* A function that checks if the current request URI contains the given string. */
+    function checkUri($t)
     {
-        try {
-            /* Checking if the requested URI contains the string `$t`. */
-            /* A function that checks if the current request URI contains the given string. */
-            function checkUri($t)
-            {
-                /* Checking if the current request URI contains the given string. */
-                return str_contains($_SERVER['REQUEST_URI'], $t);
-            }
-
-            /* Reading the mime types from the file `./data/mime.types` and storing it in the array `$mimeTypes`. */
-            $array_map = array_map('str_getcsv', file($yxorp_root . DIRECTORY_SEPARATOR . 'inc' . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'mime.types'));
-            /* Reading the mime types from the file `./data/mime.types` and storing it in the array `$mimeTypes`. */
-            $mimeTypes = array_merge(...$array_map);
-            /* Getting the file extension of the requested file. */
-            $_ext = pathinfo(strtok($_SERVER['REQUEST_URI'], ' ? '), PATHINFO_EXTENSION);
-
-            /* Setting the content type of the response. */
-            if (checkUri('bundle.js')) define('MIME', 'application' . CHAR_SLASH . 'wasm');
-            /* Checking if the current request URI contains the string `sitemap`. */
-            else if (checkUri('sitemap'))
-                /* Checking if the current request URI contains the string `crop`. */
-                define('MIME', 'application' . CHAR_SLASH . 'xml');
-            else if (checkUri('crop'))
-                /* Setting the content type of the response to `image/png`. */
-                define('MIME', 'image' . CHAR_SLASH . 'png');
-            else if (checkUri('format'))
-                /* Checking if the current request URI contains the string `.mp4`. */
-                define('MIME', 'image' . CHAR_SLASH . 'png');
-            else if (checkUri('.mp4'))
-                define('MIME', 'video' . CHAR_SLASH . 'mp4');
-            /* Checking if the current request URI contains the string `.js.br`. */
-            else if (checkUri('.js.br'))
-                /* Checking if the file extension of the requested file is in the array `$mimeTypes`. */
-                define('MIME', 'br');
-            /* Checking if the file extension of the requested file is in the array `$mimeTypes`. */
-            else if (array_key_exists($_ext, $mimeTypes))
-                define('MIME', $mimeTypes[$_ext]);
-            else
-                define('MIME', 'text' . CHAR_SLASH . 'html');
-
-            /* Setting the content type of the response. */
-            header('Content-Type: ' . MIME . ';charset=UTF-8');
-
-            /* Including the file `../tmp` + `base64_encode($_SERVER['SITE_URL'] . $_SERVER['REQUEST_URI'])` + `.tmp`. */
-            include $yxorp_root . DIRECTORY_SEPARATOR . 'tmp' . rtrim(strtr(base64_encode($_SERVER['SITE_URL'] . $_SERVER['REQUEST_URI']), '+/=', '._-')) . '.tmp';
-
-        } catch
-        (Exception $e) {
-            /*swollow*/
-        }
+        /* Checking if the current request URI contains the given string. */
+        return str_contains($_SERVER['REQUEST_URI'], $t);
     }
+
+    /* Reading the mime types from the file `./data/mime.types` and storing it in the array `$mimeTypes`. */
+    $array_map = array_map('str_getcsv', file(__DIR__ . DIRECTORY_SEPARATOR . 'inc' . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'mime.types'));
+    /* Reading the mime types from the file `./data/mime.types` and storing it in the array `$mimeTypes`. */
+    $mimeTypes = array_merge(...$array_map);
+    /* Getting the file extension of the requested file. */
+    $_ext = pathinfo(strtok($_SERVER['REQUEST_URI'], ' ? '), PATHINFO_EXTENSION);
+
+    /* Setting the content type of the response. */
+    if (checkUri('bundle.js')) define('MIME', 'application' . CHAR_SLASH . 'wasm');
+    /* Checking if the current request URI contains the string `sitemap`. */
+    else if (checkUri('sitemap'))
+        /* Checking if the current request URI contains the string `crop`. */
+        define('MIME', 'application' . CHAR_SLASH . 'xml');
+    else if (checkUri('crop'))
+        /* Setting the content type of the response to `image/png`. */
+        define('MIME', 'image' . CHAR_SLASH . 'png');
+    else if (checkUri('format'))
+        /* Checking if the current request URI contains the string `.mp4`. */
+        define('MIME', 'image' . CHAR_SLASH . 'png');
+    else if (checkUri('.mp4'))
+        define('MIME', 'video' . CHAR_SLASH . 'mp4');
+    /* Checking if the current request URI contains the string `.js.br`. */
+    else if (checkUri('.js.br'))
+        /* Checking if the file extension of the requested file is in the array `$mimeTypes`. */
+        define('MIME', 'br');
+    /* Checking if the file extension of the requested file is in the array `$mimeTypes`. */
+    else if (array_key_exists($_ext, $mimeTypes))
+        define('MIME', $mimeTypes[$_ext]);
+    else
+        define('MIME', 'text' . CHAR_SLASH . 'html');
+
+    /* Setting the content type of the response. */
+    header('Content-Type: ' . MIME . ';charset=UTF-8');
+
+    /* Including the file `../tmp` + `base64_encode($_SERVER['SITE_URL'] . $_SERVER['REQUEST_URI'])` + `.tmp`. */
+    include __DIR__ . DIRECTORY_SEPARATOR . 'cache' . rtrim(strtr(base64_encode($_SERVER['SITE_URL'] . $_SERVER['REQUEST_URI']), '+/=', '._-')) . '.tmp';
+
+} catch
+(Exception $e) {
+    /*swollow*/
 }
