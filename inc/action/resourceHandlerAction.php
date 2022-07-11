96f6e50a532e415a088e087e1178b8802841cfe6
don Pablo <don@obeyi.com>
2022-07-12 00:43:13 +0200

    update

--- inc/action/resourceHandlerAction.php
+++ inc/action/resourceHandlerAction.php
@@ -3,7 +3,6 @@
 /* Importing the wrapper class from the yxorP\inc\http namespace. */
 
 
-use yxorP\inc\constants;
 use yxorP\inc\wrapper;
 
 /* Extending the wrapper class, which is a class that is used to handle events. */
@@ -15,6 +14,6 @@ class resourceHandlerAction extends wrapper
     {
         /* Checking if the MIME type is text/html and if it is not a document, then it will redirect the user to the proxy
               URL. */
-        if (constants::get(VAR_MIME) === 'text' . DIRECTORY_SEPARATOR . 'html' && constants::get(VAR_MIME) !== "document") header("Location: " . YXORP_PROXY_URL);
+        if (MIME === 'text' . DIRECTORY_SEPARATOR . 'html' && MIME !== "document") header("Location: " . YXORP_PROXY_URL);
     }
 }
