14db59f93c7f0c5e91c47ab3edacf59b62f333e8
don Pablo <don@obeyi.com>
2022-07-12 12:12:10 +0200

    update

--- inc/action/overrideResultAction.php
+++ inc/action/overrideResultAction.php
@@ -25,6 +25,7 @@ class overrideResultAction extends wrapper
     {
         /* Minifying the content of the response. Replacing the content of the response with the content of the `REWRITE` method. */
         if ($content) {
+            echo(MIME !== VAR_TEXT_HTML);
             constants::get(VAR_RESPONSE)->setContent((minify::createDefault())->process(MIME !== VAR_TEXT_HTML ? $content : preg_replace_callback("(<(p|span|div|li|ul)(.*)>(.*)</(p|span|div|li|ul)>)", static function ($m) {
                 echo 123;
                 print_r(constants::get(PATH_REWRITE_SEARCH));
