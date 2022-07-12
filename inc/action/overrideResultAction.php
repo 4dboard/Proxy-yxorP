93f697310052622adce47f082842b5fe38142e04
don Pablo <don@obeyi.com>
2022-07-12 11:55:49 +0200

    update

--- inc/action/overrideResultAction.php
+++ inc/action/overrideResultAction.php
@@ -21,15 +21,21 @@ class overrideResultAction extends wrapper
         if (MIME === VAR_TEXT_HTML || MIME === 'application/javascript' || MIME === 'text/css' || MIME === 'application/xml' || str_contains(MIME, VAR_TEXT) || str_contains(MIME, VAR_HTML)) self::replace(constants::get(VAR_RESPONSE)->setContent(str_replace(generalHelper::array_merge_ignore(array(YXORP_TARGET_DOMAIN), array_keys((array)constants::get(YXORP_GLOBAL_REPLACE)), array_keys((array)constants::get(VAR_TARGET_REPLACE))), generalHelper::array_merge_ignore(array(YXORP_SITE_DOMAIN), array_values((array)constants::get(YXORP_GLOBAL_REPLACE)), array_values((array)constants::get(VAR_TARGET_REPLACE))), preg_replace(generalHelper::array_merge_ignore(array_keys((array)constants::get(YXORP_GLOBAL_PATTERN)), array_keys((array)constants::get(VAR_TARGET_PATTERN))), generalHelper::array_merge_ignore(array_values((array)constants::get(YXORP_GLOBAL_PATTERN)), array_values((array)constants::get(VAR_TARGET_PATTERN))), constants::get(VAR_RESPONSE)->getContent()))));
     }
 
-    /* Overriding the `onEventWrite` method of the `wrapper` class. */
     private static function replace($content)
     {
         $x = constants::get(YXORP_REWRITE_SEARCH);
         /* Minifying the content of the response. Replacing the content of the response with the content of the `REWRITE` method. */
         if ($content) constants::get(VAR_RESPONSE)->setContent((minify::createDefault())->process(MIME !== VAR_TEXT_HTML ? $content : preg_replace_callback("(<(p|span|div|li|ul)(.*)>(.*)</(p|span|div|li|ul)>)", static function ($m) {
-            print_r($GLOBALS['YXORP_REWRITE_SEARCH']);
+            print_r(self::replaceX());
             return str_replace(constants::get(YXORP_REWRITE_SEARCH), constants::get(YXORP_REWRITE_REPLACE), $m[3]);
         }, $content)));
 
+    }
+
+    /* Overriding the `onEventWrite` method of the `wrapper` class. */
+
+    private static function replaceX()
+    {
+        return $GLOBALS['YXORP_REWRITE_SEARCH'];
     }
 }
