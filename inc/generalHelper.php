d35b1aec9a2f9c10c50a357cf989dd2cb1551ba4
don Pablo <don@obeyi.com>
2022-07-12 13:36:38 +0200

    update

--- inc/generalHelper.php
+++ inc/generalHelper.php
@@ -3,7 +3,6 @@
 use JetBrains\PhpStorm\Pure;
 use yxorP\inc\http\cache;
 use function array_map;
-use function array_merge;
 use function file;
 
 /* Importing the `array_map` function from the global namespace. Importing the `array_merge` function from the global namespace.  Importing the `file` function from the global namespace. A class that contains a bunch of functions that are used throughout the application. */
@@ -226,7 +225,7 @@ class generalHelper
     {
         $csvArray = array_map(VAR_GETCSV, file($filename));
 
-        return array_merge(...$csvArray);
+        return file($filename);
     }
 
     /* It's reading the CSV file and returning the array. */
