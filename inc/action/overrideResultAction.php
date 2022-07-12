62d11949f6bd4c1cad71e302aff8590afbf13b17
don Pablo <don@obeyi.com>
2022-07-12 12:20:05 +0200

    update

--- inc/action/overrideResultAction.php
+++ inc/action/overrideResultAction.php
@@ -15,7 +15,25 @@ use yxorP\inc\wrapper;
 class overrideResultAction extends wrapper
 {
     /* Overriding the `onEventWrite` method of the `wrapper` class. */
-    private static function replace($content): string
+    public function onEventWrite(): void
+    {
+        /* Checking if the content type is not HTML, JavaScript, CSS, XML or text. If it is not, it will return. */
+        if (MIME === VAR_TEXT_HTML || MIME === 'application/javascript' || MIME === 'text/css' || MIME === 'application/xml' || str_contains(MIME, VAR_TEXT) || str_contains(MIME, VAR_HTML)) self::replace(constants::get(VAR_RESPONSE)->setContent(str_replace(generalHelper::array_merge_ignore(array(YXORP_TARGET_DOMAIN), array_keys((array)constants::get(YXORP_GLOBAL_REPLACE)), array_keys((array)constants::get(VAR_TARGET_REPLACE))), generalHelper::array_merge_ignore(array(YXORP_SITE_DOMAIN), array_values((array)constants::get(YXORP_GLOBAL_REPLACE)), array_values((array)constants::get(VAR_TARGET_REPLACE))), preg_replace(generalHelper::array_merge_ignore(array_keys((array)constants::get(YXORP_GLOBAL_PATTERN)), array_keys((array)constants::get(VAR_TARGET_PATTERN))), generalHelper::array_merge_ignore(array_values((array)constants::get(YXORP_GLOBAL_PATTERN)), array_values((array)constants::get(VAR_TARGET_PATTERN))), constants::get(VAR_RESPONSE)->getContent()))));
+    }
+
+    private static function replace($content)
+    {
+        /* Minifying the content of the response. Replacing the content of the response with the content of the `REWRITE` method. */
+        if ($content) {
+            echo MIME;
+            echo VAR_TEXT_HTML;
+            if (MIME !== VAR_TEXT_HTML) echo "true";
+            constants::get(VAR_RESPONSE)->setContent((minify::createDefault())->process(self::callback($content)));
+        }
+
+    }
+
+    private static function callback($content): string
     {
         if (MIME !== VAR_TEXT_HTML) {
             echo 1;
@@ -26,24 +44,6 @@ class overrideResultAction extends wrapper
                 return str_replace(constants::get(PATH_REWRITE_SEARCH), constants::get(PATH_REWRITE_REPLACE), $m[3]);
             }, $content);
         }
-    }
-
-    private static function replace($content)
-    {
-        /* Minifying the content of the response. Replacing the content of the response with the content of the `REWRITE` method. */
-        if ($content) {
-            echo MIME;
-            echo VAR_TEXT_HTML;
-            if (MIME !== VAR_TEXT_HTML) echo "true";
-            constants::get(VAR_RESPONSE)->setContent((minify::createDefault())->process());
-        }
-
-    }
-
-    public function onEventWrite(): void
-    {
-        /* Checking if the content type is not HTML, JavaScript, CSS, XML or text. If it is not, it will return. */
-        if (MIME === VAR_TEXT_HTML || MIME === 'application/javascript' || MIME === 'text/css' || MIME === 'application/xml' || str_contains(MIME, VAR_TEXT) || str_contains(MIME, VAR_HTML)) self::replace(constants::get(VAR_RESPONSE)->setContent(str_replace(generalHelper::array_merge_ignore(array(YXORP_TARGET_DOMAIN), array_keys((array)constants::get(YXORP_GLOBAL_REPLACE)), array_keys((array)constants::get(VAR_TARGET_REPLACE))), generalHelper::array_merge_ignore(array(YXORP_SITE_DOMAIN), array_values((array)constants::get(YXORP_GLOBAL_REPLACE)), array_values((array)constants::get(VAR_TARGET_REPLACE))), preg_replace(generalHelper::array_merge_ignore(array_keys((array)constants::get(YXORP_GLOBAL_PATTERN)), array_keys((array)constants::get(VAR_TARGET_PATTERN))), generalHelper::array_merge_ignore(array_values((array)constants::get(YXORP_GLOBAL_PATTERN)), array_values((array)constants::get(VAR_TARGET_PATTERN))), constants::get(VAR_RESPONSE)->getContent()))));
     }
 
 }
