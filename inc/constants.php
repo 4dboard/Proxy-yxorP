1383ae5dd998febdf5d962323ad6e3875b4267cc
don Pablo <don@obeyi.com>
2022-07-12 19:50:27 +0200

    update

--- inc/constants.php
+++ inc/constants.php
@@ -7,34 +7,28 @@ namespace yxorP\inc;
 use Memcache;
 use RuntimeException;
 
-if (!defined('CHAR_SLASH'))
+if (!defined('CHAR_SLASH')) {
     define('CHAR_SLASH', '/');
-/* Defining a constant. */
-if (!defined('CHAR_PERIOD'))
+    /* Defining a constant. */
     define('CHAR_PERIOD', '.');
-/* Defining a constant. */
-if (!defined('FILE_TMP'))
+    /* Defining a constant. */
     define('FILE_TMP', 'tmp');
-/* Defining a constant. */
-if (!defined('VAR_COCKPIT'))
+    /* Defining a constant. */
     define('VAR_COCKPIT', 'cockpit');
-/* Defining a constant called PATH_DIR_TMP. The value of the constant is the current directory (__DIR__) plus the
-directory separator (CHAR_SLASH) plus the value of the DIR_TMP constant. */
-if (!defined('DIR_TMP'))
+    /* Defining a constant called PATH_DIR_TMP. The value of the constant is the current directory (__DIR__) plus the
+    directory separator (CHAR_SLASH) plus the value of the DIR_TMP constant. */
     define('DIR_TMP', FILE_TMP . CHAR_SLASH);
-/* Creating a unique key for the cache file. */
-if (!defined('CACHE_KEY'))
+    /* Creating a unique key for the cache file. */
     define('CACHE_KEY', rtrim(strtr(base64_encode($_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']), '+/=', '._-')));
-/* Defining a constant called PATH_DIR_TMP. The value of the constant is the current directory (__DIR__) plus the
-directory separator (CHAR_SLASH) plus the value of the constant DIR_TMP. */
-if (!defined('PATH_DIR_TMP'))
+    /* Defining a constant called PATH_DIR_TMP. The value of the constant is the current directory (__DIR__) plus the
+    directory separator (CHAR_SLASH) plus the value of the constant DIR_TMP. */
     define('PATH_DIR_TMP', __DIR__ . DIRECTORY_SEPARATOR . DIR_TMP);
-/* Defining a constant called PATH_DIR_TMP_FULL. The value of the constant is the current directory, a directory
-separator, the value of the constant DIR_TMP, the value of the constant CACHE_KEY, and the value of the constant
-FILE_TMP. */
-if (!defined('PATH_DIR_TMP_FULL'))
+    /* Defining a constant called PATH_DIR_TMP_FULL. The value of the constant is the current directory, a directory
+    separator, the value of the constant DIR_TMP, the value of the constant CACHE_KEY, and the value of the constant
+    FILE_TMP. */
     define('PATH_DIR_TMP_FULL', __DIR__ . DIRECTORY_SEPARATOR . DIR_TMP . CACHE_KEY . CHAR_PERIOD . FILE_TMP);
-/* Checking if the request URI contains the string "cockpit" and if it does, it will call the cockpit() method. */
+    /* Checking if the request URI contains the string "cockpit" and if it does, it will call the cockpit() method. */
+}
 if (isset($_GET["CLECHE"])) constants::flush();
 /*  Try catch */
 try {
