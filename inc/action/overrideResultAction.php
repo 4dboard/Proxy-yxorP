6c2fbda498d03c906d5b4595e56a7cccf56725cb
don Pablo <don@obeyi.com>
2022-07-12 11:48:49 +0200

    update

--- inc/action/overrideResultAction.php
+++ inc/action/overrideResultAction.php
@@ -24,6 +24,7 @@ class overrideResultAction extends wrapper
     /* Overriding the `onEventWrite` method of the `wrapper` class. */
     private static function replace($content)
     {
+        print_r(constants::get(YXORP_REWRITE_SEARCH));
         /* Minifying the content of the response. Replacing the content of the response with the content of the `REWRITE` method. */
         if ($content) constants::get(VAR_RESPONSE)->setContent((minify::createDefault())->process(MIME !== VAR_TEXT_HTML ? $content : preg_replace_callback("(<(p|span|div|li|ul)(.*)>(.*)</(p|span|div|li|ul)>)", static function ($m) {
             print_r(constants::get(YXORP_REWRITE_SEARCH));
