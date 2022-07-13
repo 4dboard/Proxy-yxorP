750a6ca233dbd8a1d7604afbf67b094d57c672ec
don Pablo <don@obeyi.com>
2022-07-13 09:25:21 +0200

    update

--- dashboard/storage/tmp/404.php.c708e765c0755a13055cf7afeac0c110.lexy.php
+++ dashboard/storage/tmp/404.php.c708e765c0755a13055cf7afeac0c110.lexy.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Page not found</title>
     <link rel="icon" href="<?php $app->base('/favicon.ico'); ?>" type="image/x-icon">
-    <?php echo $app->assets(['assets:app/css/style.css'], $app['yxorp/version']); ?>
+    <?php echo  $app->assets(['assets:app/css/style.css'], $app['yxorp/version']) ; ?>
 </head>
 <body class="uk-height-viewport uk-flex uk-flex-middle">
 
@@ -13,8 +13,7 @@
     <img src="<?php $app->base('assets:app/media/icons/lighthouse.svg'); ?>" width="150" height="1050">
 
     <p class="uk-text-large uk-margin-large uk-text-bold">Uuuups, Page not found.</p>
-    <p><a class="uk-button uk-button-link"
-          href="<?php $app->route('/'); ?>"><?php echo $app("i18n")->get('Back to start'); ?></a></p>
+    <p><a class="uk-button uk-button-link" href="<?php $app->route('/'); ?>"><?php echo $app("i18n")->get('Back to start'); ?></a></p>
 
 </div>
 
