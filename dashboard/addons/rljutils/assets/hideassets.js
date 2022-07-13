aed73ba914074ed522004e8e401c996814da8873
don Pablo <don@obeyi.com>
2022-07-13 05:30:37 +0200

    update

--- dashboard/addons/rljutils/assets/hideassets.js
+++
@@ -1,6 +0,0 @@
-// hide assets link in app header dropdown menu
-App.$('document').ready(function () {
-
-    App.$('.app-header a[href$="/assetsmanager"]').parent().hide();
-
-});
--- dashboard/addons/rljutils/helpers.php
+++
@@ -1,1 +0,0 @@
-<?php $this->on('admin.init',function()use($helpers){if(!empty($helpers['locked_entries_disabled'])){$keys=$this->memory->keys('locked:*');if(!empty($keys)){$this->memory->del(...$keys);}}});
