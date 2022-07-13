a7b3b2b18d206a1c893e9fcc49617916a7aeffe7
don Pablo <don@obeyi.com>
2022-07-13 05:22:12 +0200

    update

---
+++ dashboard/addons/rljutils/views/partials/settings.php
@@ -0,0 +1,10 @@
+<div>
+    <div class="uk-panel uk-panel-space uk-panel-box uk-panel-card">
+        <img src="@url('assets:app/media/icons/settings.svg')" width="50" height="50" alt="@lang('rljUtils')"/>
+
+        <div class="uk-text-truncate uk-margin">
+            @lang('rljUtils')
+        </div>
+        <a class="uk-position-cover" aria-label="@lang('rljUtils')" href="@route('/rljutils')"></a>
+    </div>
+</div>
