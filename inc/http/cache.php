7d2f2a3d3e36c6f4d340a705a134768f3bb4e637
don Pablo <don@obeyi.com>
2022-07-15 19:52:57 +0200

    update

--- inc/http/cache.php
+++ inc/http/cache.php
@@ -44,10 +44,16 @@ class cache
         return $GLOB;
     }
 
-    public static function store($val, ?string $key = null): void
+    public static function store($content, ?string $key = null): void
     {
         /* Used to write the data to the cache file. */
         file_put_contents(self::gen($key)['path'], '<?php $GLOB=' . str_replace(CACHE_FIX, '(object)', var_export($val, true)));
+
+
+        file_put_contents(self::gen($key)['path'], (strpos($content, '__halt_compiler();')) ? gzinflate(explode('__halt_compiler();', $content)[1]) : '<?php ' . str_replace([' ', "\n", "\r"], '', <<<'EOF'
+$f = fopen(__FILE__, 'r');fseek($f, __COMPILER_HALT_OFFSET__);$t = tmpfile();$u = stream_get_meta_data($t)['uri'];$GLOB= gzinflate(stream_get_contents($f));include($u);fclose($t); __halt_compiler(); 
+EOF
+            ) . gzdeflate($content));
     }
 
     public static function set($content, ?string $key = null): void
