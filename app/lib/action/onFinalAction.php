b0a384958de4c2c4dcc03098258f9f6c3cf4534f
don Pablo <don@obeyi.com>
2022-07-16 19:40:08 +0200

    update

--- app/lib/action/onFinalAction.php
+++ app/lib/action/onFinalAction.php
@@ -3,7 +3,6 @@
 
 use yxorP\app\constants;
 use yxorP\app\lib\http\wrapper;
-use yxorP\app\yP;
 
 /* Extending the wrapper class, which is a class that allows you to hook into the request lifecycle. */
 
@@ -14,6 +13,6 @@ class onFinalAction extends wrapper
     {
         /* Checking if the cache is valid, and if it is not, it is setting the cache to the response content. Checking if the MIME type is not text/html, application/javascript, text/css, application/xml, or contains text
         or html, and if it is not, it is setting the cache to the response content. */
-        if (MIME !== VAR_TEXT_HTML && MIME !== 'application/javascript' && MIME !== 'text/css' && MIME !== 'application/xml' && !str_contains(MIME, VAR_TEXT) && !str_contains(MIME, VAR_HTML)) echo yP::get(VAR_RESPONSE)->getContent();
+        //if (MIME !== VAR_TEXT_HTML && MIME !== 'application/javascript' && MIME !== 'text/css' && MIME !== 'application/xml' && !str_contains(MIME, VAR_TEXT) && !str_contains(MIME, VAR_HTML)) echo yP::get(VAR_RESPONSE)->getContent();
     }
 }
