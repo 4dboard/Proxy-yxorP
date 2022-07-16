623f25042db1b0a0e7d523178a594298107b14a4
don Pablo <don@obeyi.com>
2022-07-16 19:30:12 +0200

    update

--- app/lib/action/onBuildIncludesAction.php
+++ app/lib/action/onBuildIncludesAction.php
@@ -15,6 +15,7 @@ class onBuildIncludesAction extends wrapper
         yP::assetLoader(YXORP_DIR_FULL);
         /* Checking the files in the directory `DIR_ROOT . 'override' . CHAR_SLASH . 'global'` and it is not
         recursive. */
+        echo DIR_ROOT . DIR_APP . DIR_LIB . DIR_OVERRIDE . VAR_GLOBAL;
         yP::assetLoader(DIR_ROOT . DIR_APP . DIR_LIB . DIR_OVERRIDE . VAR_GLOBAL);
     }
 }
