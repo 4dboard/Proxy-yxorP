c5de35089f04517003343bdd3c8a5b6a6357fc5d
don Pablo <don@obeyi.com>
2022-07-16 17:18:37 +0200

    update

--- install/permissions.sh
+++ install/permissions.sh
@@ -1,7 +1,6 @@
-mkdir -p ../app/tmp/
-chown root: ../app/tmp/
-chmod 777 ../app/tmp/
-
-chmod 777 ../app/lib/cockpit/storage/tmp
-chown root: ../app/lib/cockpit/storage/tmp
-chmod 777 ../app/lib/cockpit/storage/tmp
+sudo mkdir -p ../app/tmp/
+sudo chown root: ../app/tmp/
+sudo chmod 777 ../app/tmp/
+sudo chmod 777 ../app/lib/cockpit/storage/tmp
+sudo chown root: ../app/lib/cockpit/storage/tmp
+sudo chmod 777 ../app/lib/cockpit/storage/tmp
