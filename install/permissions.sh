f145f7fbb48eb28ea1659044b88f8eac1cca6983
don Pablo <don@obeyi.com>
2022-07-16 17:12:21 +0200

    update

--- install/permissions.sh
+++ install/permissions.sh
@@ -2,4 +2,6 @@ mkdir -p ../app/tmp/
 chown root: ../app/tmp/
 chmod 777 ../app/tmp/
 
-chmod 777 ../app/tmp/
+chmod 777 ../app/lib/cockpit/storage/tmp
+chown root: ../app/lib/cockpit/storage/tmp
+chmod 777 ../app/lib/cockpit/storage/tmp
