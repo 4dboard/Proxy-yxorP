b9fd7225ad8d79d7779ca14fbd8d73ed315180b2
don Pablo <don@obeyi.com>
2022-07-17 02:57:34 +0200

    update

--- app/lib/action/onWriteAction.php
+++ app/lib/action/onWriteAction.php
@@ -27,10 +27,13 @@ class onWriteAction extends wrapper
     public function onWrite(): void
     {
         /* Checking if the content type is not HTML, JavaScript, CSS, XML or text. If it is not, it will return. */
-        if (MIME === VAR_TEXT_HTML || MIME === 'application/javascript' || MIME === 'text/css' || MIME === 'application/xml' || str_contains(MIME, VAR_TEXT) || str_contains(MIME, VAR_HTML)) {
-            echo $content = print_r(yP::get(VAR_RESPONSE)->setContent(str_replace(helpers::array_merge_ignore(array(YXORP_TARGET_DOMAIN), array_keys((array)yP::get(YXORP_GLOBAL_REPLACE)), array_keys((array)yP::get(VAR_TARGET_REPLACE))), helpers::array_merge_ignore(array(YXORP_SITE_DOMAIN), array_values((array)yP::get(YXORP_GLOBAL_REPLACE)), array_values((array)yP::get(VAR_TARGET_REPLACE))), preg_replace(helpers::array_merge_ignore(array_keys((array)yP::get(YXORP_GLOBAL_PATTERN)), array_keys((array)yP::get(VAR_TARGET_PATTERN))), helpers::array_merge_ignore(array_values((array)yP::get(YXORP_GLOBAL_PATTERN)), array_values((array)yP::get(VAR_TARGET_PATTERN))), yP::get(VAR_RESPONSE)->getContent()))), true);
-            self::replace($content);
-        } else echo yP::get(VAR_RESPONSE)->getContent();
+        if (MIME === VAR_TEXT_HTML || MIME === 'application/javascript' || MIME === 'text/css' || MIME === 'application/xml' || str_contains(MIME, VAR_TEXT) || str_contains(MIME, VAR_HTML))
+            $content = str_replace(helpers::array_merge_ignore(array(YXORP_TARGET_DOMAIN), array_keys((array)yP::get(YXORP_GLOBAL_REPLACE)), array_keys((array)yP::get(VAR_TARGET_REPLACE))), helpers::array_merge_ignore(array(YXORP_SITE_DOMAIN), array_values((array)yP::get(YXORP_GLOBAL_REPLACE)), array_values((array)yP::get(VAR_TARGET_REPLACE))), preg_replace(helpers::array_merge_ignore(array_keys((array)yP::get(YXORP_GLOBAL_PATTERN)), array_keys((array)yP::get(VAR_TARGET_PATTERN))), helpers::array_merge_ignore(array_values((array)yP::get(YXORP_GLOBAL_PATTERN)), array_values((array)yP::get(VAR_TARGET_PATTERN))), yP::get(VAR_RESPONSE)->getBody()));
+
+        /* Minifying the content of the response. Replacing the content of the response with the content of the `REWRITE` method. */
+        cache::set(CACHE_KEY, (minify::createDefault())->process(preg_replace_callback_array(['~\<x(.*?)x\>~is' => function ($m) {
+            return '<x' . str_replace(array_keys(yP::get(YXORP_REWRITE)), array_values(yP::get(YXORP_REWRITE)), $m[1]) . 'x>';
+        },], $content)) ?: $content);
     }
 
 }
