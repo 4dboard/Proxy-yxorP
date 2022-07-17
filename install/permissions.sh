70c95efa576d721d0152a31deb2998e400e22a81
don Pablo <don@obeyi.com>
2022-07-17 04:48:23 +0200

    update

--- install/permissions.sh
+++ install/permissions.sh
@@ -4,3 +4,5 @@ sudo chmod 777 ../app/tmp/
 sudo chmod 777 ../app/lib/cockpit/storage/tmp
 sudo chown root: ../app/lib/cockpit/storage/tmp
 sudo chmod 777 ../app/lib/cockpit/storage/tmp
+sudo chown root: ../app/lib/data/cookie_jar.txt
+sudo chmod 777 ../app/lib/data/cookie_jar.txt
