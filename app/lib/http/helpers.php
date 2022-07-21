bf203304b9ac0fbd8cec4111631c1572d479db0f
don Pablo <don@obeyi.com>
2022-07-21 04:46:07 +0200

    update

--- app/lib/http/helpers.php
+++ app/lib/http/helpers.php
@@ -420,7 +420,6 @@ class helpers
 
     final public static function assetLoader(string $root): void
     {
-        echo $root;
         /* Loading all the files in the root directory and subdirectories. */
         foreach (glob($root . DIRECTORY_SEPARATOR . "*") as $path) if (is_dir($path)) self::assetLoader($path); else if (str_contains(YXORP_REQUEST_URI, basename($path))) {
             self::setMimeType();
