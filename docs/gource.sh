f66daf3ae24302790aff8ac79199fcacc723f751
don Pablo <don@obeyi.com>
2022-07-21 05:27:03 +0200

    update

--- docs/gource.sh
+++ docs/gource.sh
@@ -1,10 +1,1 @@
-{
-  "</head>": "<link rel='stylesheet' media='all' type='text/css' href='/default.css?v1' /></link><link rel='stylesheet' media='all' type='text/css' href='/styles.css?v1' /><link rel='icon' type='image/png' href='/favicon.ico' /><script async type='text/javascript' src='/default.js?v1'></script><script async type='text/javascript' src='/scripts.js?v1'></script><script async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8114628379784713' crossorigin='anonymous'></script><script type='module'> import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate'; const el = document.createElement('pwa-update'); document.body.appendChild(el); </script><link rel='manifest' href='/manifest.json'></link></head>",
-  "<div": "<z",
-  "</div": "</z",
-  "<span": "<y",
-  "</span": "</y",
-  "<p": "<x",
-  "</p": "</x",
-  "<header></header>": "<canvas id='octo'></canvas><h><a href='/'><img height='20px' src='/don.png' /></a> <n id='nav'><a class='g'  href='https://www.xn--gme-nnb.com/'>Game.com</a><a class='p'  href='https://www.xn--ply-onb.com/'>Play.com</a></n><m>You make the impossible possible on our site every day, no matter where you are!<m></h><z id='clxuds'> <z class='clouds-1'></z> <z class='clouds-2'></z> <z class='clouds-3'></z> </z>"
-}
+gource -s 1 -e 0.1 --high-dpi   -800x600  --auto-skip-seconds 0.1  --bloom-multiplier 0.85  --bloom-intensity 0.85 --stop-at-end    --date-format "%d/%m/%y"  --hide mouse,filenames  --file-idle-time 0  --max-files 0      --background-image './world.jpg' --logo './logo-small.png'  --font-size 16  --output-ppm-stream -  --output-framerate 30  |  ffmpeg -y -r 30 -f image2pipe -vcodec ppm -i - -b 6536K './movie.mp4'
