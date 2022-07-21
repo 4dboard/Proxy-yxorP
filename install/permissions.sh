de20e93e712f6c0012a216e4c7693a2089652513
don Pablo <don@obeyi.com>
2022-07-21 23:01:43 +0200

    update

--- install/permissions.sh
+++ install/permissions.sh
@@ -2,9 +2,29 @@ sudo rm -r ../app/tmp/
 sudo mkdir -p ../app/tmp/
 sudo chown root: ../app/tmp/
 sudo chmod 777 ../app/tmp/
+
+sudo rm -r ../app/lib/cockpit/storage/tmp/
+sudo mkdir -p ../app/lib/cockpit/storage/tmp/
+sudo chown root: ../app/lib/cockpit/storage/tmp/
+sudo chmod 777 ../app/lib/cockpit/storage/tmp/
+
+sudo rm -r ../app/lib/cockpit/storage/tmp/
+sudo mkdir -p ../app/lib/cockpit/storage/tmp/
+sudo chown root: ../app/lib/cockpit/storage/tmp/
 sudo chmod 777 ../app/lib/cockpit/storage/tmp
-sudo chown root: ../app/lib/cockpit/storage/tmp
-sudo chmod 777 ../app/lib/cockpit/storage/tmp
+
+sudo rm ../app/lib/data/cookie_jar.txt
+sudo touch -p ../app/lib/data/cookie_jar.txt
 sudo chown root: ../app/lib/data/cookie_jar.txt
 sudo chmod 777 ../app/lib/data/cookie_jar.txt
+
+sudo rm ../app/lib/data/cookie_jar.txt
+sudo touch -p ../app/lib/data/cookie_jar.txt
+sudo chown root: ../app/lib/data/cookie_jar.txt
+sudo chmod 777 ../app/lib/data/cookie_jar.txt
+
+sudo chown root: ../app/lib/data/cookie_jar.txt
+sudo chmod 777 ../app/lib/data/cookie_jar.txt
+/var/cache/pagespeed/
 sudo tail -f /var/log/apache2/error.log
+
