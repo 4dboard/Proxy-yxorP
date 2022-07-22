b94b0f2d6a79473fe7609d670a32ea95d1c9d438
don Pablo <don@obeyi.com>
2022-07-22 03:32:24 +0200

    update

--- app/constants.php
+++ app/constants.php
@@ -1,8 +1,6 @@
 <?php
-/* Setting the headers for the response. */
-foreach (['Access-Control-Allow-Origin: "*" always', 'Access-Control-Allow-Methods: "POST,GET,OPTIONS" always', 'Access-Control-Allow-Credentials: true always', 'Access-Control-Allow-Headers: "Origin,Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With,Access-Control-Allow-Credentials" always', 'Cache-Control: "max-age=36000000" always'] as $head) header($head);
-/* Initialise minimum definable varibles */
-if (!defined('YXORP_HTTP_HOST')) {
+foreach (['Access-Control-Allow-Origin: "*" always', 'Access-Control-Allow-Methods: "POST,GET,OPTIONS" always', 'Access-Control-Allow-Credentials: true always', 'Access-Control-Allow-Headers: "Origin,Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With,Access-Control-Allow-Credentials" always', 'Cache-Control: "max-age=36000000" always'] as $head) header($head);//Setting the headers for the response.
+if (!defined('YXORP_HTTP_HOST')) {//Initialise minimum definable varibles
     define('YXORP_HTTP_HOST', $_SERVER['HTTP_HOST']);
     define('YXORP_REQUEST_URI', $_SERVER['REQUEST_URI']);
     define('CHAR_PERIOD', '.');
@@ -12,13 +10,11 @@ if (!defined('YXORP_HTTP_HOST')) {
     define('CACHE_KEY', rtrim(strtr(base64_encode(YXORP_REQUEST_URI), '+/=', '._-')));
     define('PATH_TMP_DIR', __DIR__ . DIRECTORY_SEPARATOR . DIR_TMP);
     define('PATH_TMP_FILE', __DIR__ . DIRECTORY_SEPARATOR . DIR_TMP . CACHE_KEY . FILE_TMP);
+    if ($cacheExits = file_exists(PATH_TMP_FILE)) @include PATH_TMP_FILE; //Render Cache if Exits: Including the file `/tmp` + `base64_encode(YXORP_HTTP_HOST . YXORP_REQUEST_URI)` + `.tmp`.
 }
 /* Checking if we must clear the cache */
 if (isset($_GET["CLECHE"])) foreach (glob(PATH_TMP_DIR . '*') as $file) unlink($file);
-/*  Set Header MimeType */
-
-/* Render Cache if Exits: Including the file `/tmp` + `base64_encode(YXORP_HTTP_HOST . YXORP_REQUEST_URI)` + `.tmp`. */
-if (file_exists(PATH_TMP_FILE)) exit(die(@include PATH_TMP_FILE));
+if ($cacheExits) exit(die());
 /* It defines constants and sets the value of the constants to the value of the arguments passed to the class.  Defining constants. Creating a class called constants.  Defining a constant named `CHAR_SLASH` with the value `/`. */
 
 if (!$GLOBALS[YXORP_HTTP_HOST]) {
