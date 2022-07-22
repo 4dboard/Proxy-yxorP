2cb93ad94e8072b07d151d00aec80c90404330cf
don Pablo <don@obeyi.com>
2022-07-22 02:05:56 +0200

    update

--- app/override/global/default.js
+++ app/override/global/default.js
@@ -546,3 +546,19 @@ window.onhashchange = function () {
 
 onChangePlay();
 
+var GLOBAL = (function () {
+    var my = {},
+        privateVariable = 1;
+
+    function privateMethod() {
+        // ...
+    }
+
+    my.moduleProperty = 1;
+    my.moduleMethod = function () {
+        // ...
+    };
+
+    return my;
+}());
+
