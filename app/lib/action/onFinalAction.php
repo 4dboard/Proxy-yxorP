3faa70324e91f5b20f94463c20b25498748d9d9c
don Pablo <don@obeyi.com>
2022-07-24 19:02:33 +0200

    update

--- app/lib/action/onFinalAction.php
+++ app/lib/action/onFinalAction.php
@@ -3,8 +3,8 @@
 
 use yxorP\app\constants;
 use yxorP\app\lib\http\cache;
+use yxorP\app\lib\http\store;
 use yxorP\app\lib\http\wrapper;
-use yxorP\app\yP;
 
 /* Extending the wrapper class, which is a class that allows you to hook into the request lifecycle. */
 
