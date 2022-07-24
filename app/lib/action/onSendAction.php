5f1734c081f41451657f7163e23e118dd4d95837
don Pablo <don@obeyi.com>
2022-07-24 12:36:26 +0200

    update

--- app/lib/action/onSendAction.php
+++ app/lib/action/onSendAction.php
@@ -16,11 +16,16 @@ class onSendAction extends wrapper
      */
     public function onSend(): void
     {
+        echo 1;
 
         /* Creating a new `yxorP\app\lib\proxy\Client` object, and then it is sending a request to the `\yxorP\app\yP::store(YXORP_FETCH)` URL,
         with the `\yxorP\app\yP::store(YXORP_REQUEST)->getMethod()` method, and the `$_REQUEST` array as the body.   Setting the content of the response to the content of the request. */
         $getUrl = yP::store(VAR_REQUEST)->getUrl();
+
+        echo 1;
         $getMethod = yP::store(VAR_REQUEST)->getMethod();
+
+        echo 1;
         $getMethod = yP::store(VAR_GUZZLE)->request($getMethod);
         echo 1;
         yP::store_tmp(VAR_RESPONSE, $getMethod, $getUrl, json_decode(json_encode($_REQUEST), true, 512, JSON_THROW_ON_ERROR));
