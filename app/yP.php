c21045005bb921fb7e6480aec0eb3b2a14006c87
don Pablo <don@obeyi.com>
2022-07-24 08:15:00 +0200

    update

--- app/yP.php
+++ app/yP.php
@@ -166,7 +166,7 @@ class yP
     {
 
         if (ENV_DEFAULT_TARGET == $name) echo 'get';
-        if (ENV_DEFAULT_TARGET == $name) echo self::get($type, $name);
+        if (ENV_DEFAULT_TARGET == $name) echo $type[$name];
         /* Checking if the argument already isset in the global scope and if it does, it throws an exception. If it
         doesn't, it adds the argument to the global scope . */
         return $type[$name];
