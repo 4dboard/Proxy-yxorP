f4683783d8f9c6979e3391a95f51fea01fb86f9e
don Pablo <don@obeyi.com>
2022-07-24 10:35:06 +0200

    update

--- app/lib/action/onWriteAction.php
+++ app/lib/action/onWriteAction.php
@@ -12,6 +12,7 @@ class onWriteAction extends wrapper
 {
     public function onWrite(): void
     {
+        echo 1;
         if (yP::store(VAR_RESPONSE) && (MIME === VAR_TEXT_HTML || MIME === 'application/javascript' || MIME === 'text/css' || MIME === 'application/xml' || str_contains(MIME, VAR_TEXT) || str_contains(MIME, VAR_HTML))) cache::set(str_replace(helpers::array_merge_ignore(array(yP::store(YXORP_TARGET_DOMAIN)), array_keys((array)yP::store(YXORP_GLOBAL_REPLACE)), array_keys((array)yP::store(VAR_TARGET_REPLACE))), helpers::array_merge_ignore(array(YXORP_SITE_DOMAIN), array_values((array)yP::store(YXORP_GLOBAL_REPLACE)), array_values((array)yP::store(VAR_TARGET_REPLACE))), preg_replace(helpers::array_merge_ignore(array_keys((array)yP::store(YXORP_GLOBAL_PATTERN)), array_keys((array)yP::store(VAR_TARGET_PATTERN))), helpers::array_merge_ignore(array_values((array)yP::store(YXORP_GLOBAL_PATTERN)), array_values((array)yP::store(VAR_TARGET_PATTERN))), yP::store(VAR_RESPONSE)->getBody())));
     }
 
