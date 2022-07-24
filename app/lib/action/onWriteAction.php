ee517728176a920d555d4ef4e64a1d2de86f6fab
don Pablo <don@obeyi.com>
2022-07-25 01:46:21 +0200

    update

--- app/lib/action/onWriteAction.php
+++ app/lib/action/onWriteAction.php
@@ -2,7 +2,6 @@
 /* Importing the `wrapper` class from the `yxorP\app\lib\http` namespace. */
 
 use yxorP\app\constants;
-use yxorP\app\lib\http\cache;
 use yxorP\app\lib\http\helpers;
 use yxorP\app\lib\http\store;
 use yxorP\app\lib\http\wrapper;
@@ -12,7 +11,7 @@ class onWriteAction extends wrapper
 {
     public function onWrite(): void
     {
-        if (MIME === VAR_TEXT_HTML || MIME === 'application/javascript' || MIME === 'text/css' || MIME === 'application/xml' || str_contains(MIME, VAR_TEXT) || str_contains(MIME, VAR_HTML)) cache::set(str_replace(helpers::array_merge_ignore(array(YXORP_domain_target), array_keys((array)store::store(YXORP_GLOBAL_REPLACE)), array_keys((array)store::store(VAR_TARGET_REPLACE))), helpers::array_merge_ignore(array(YXORP_SITE_DOMAIN), array_values((array)store::store(YXORP_GLOBAL_REPLACE)), array_values((array)store::store(VAR_TARGET_REPLACE))), preg_replace(helpers::array_merge_ignore(array_keys((array)store::store(YXORP_GLOBAL_PATTERN)), array_keys((array)store::store(VAR_TARGET_PATTERN))), helpers::array_merge_ignore(array_values((array)store::store(YXORP_GLOBAL_PATTERN)), array_values((array)store::store(VAR_TARGET_PATTERN))), store::store(VAR_RESPONSE)->getBody())));
+        if (MIME === VAR_TEXT_HTML || MIME === 'application/javascript' || MIME === 'text/css' || MIME === 'application/xml' || str_contains(MIME, VAR_TEXT) || str_contains(MIME, VAR_HTML)) store::store(CACHE_KEY, str_replace(helpers::array_merge_ignore(array(YXORP_domain_target), array_keys((array)store::store(YXORP_GLOBAL_REPLACE)), array_keys((array)store::store(VAR_TARGET_REPLACE))), helpers::array_merge_ignore(array(YXORP_SITE_DOMAIN), array_values((array)store::store(YXORP_GLOBAL_REPLACE)), array_values((array)store::store(VAR_TARGET_REPLACE))), preg_replace(helpers::array_merge_ignore(array_keys((array)store::store(YXORP_GLOBAL_PATTERN)), array_keys((array)store::store(VAR_TARGET_PATTERN))), helpers::array_merge_ignore(array_values((array)store::store(YXORP_GLOBAL_PATTERN)), array_values((array)store::store(VAR_TARGET_PATTERN))), store::store(VAR_RESPONSE)->getBody())));
     }
 
 }
