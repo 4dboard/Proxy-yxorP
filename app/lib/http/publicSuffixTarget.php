9bb05b83dee43cea8c3a5084e1ece68c644ff813
don Pablo <don@obeyi.com>
2022-07-24 09:23:52 +0200

    update

--- app/lib/http/publicSuffixTarget.php
+++ app/lib/http/publicSuffixTarget.php
@@ -1,10 +1,79 @@
 <?php namespace yxorP\app\lib\http;
 
-class publicSuffixTarget
+use yxorP\app\lib\parser\hostInterface;
+use yxorP\app\lib\parser\nameInterface;
+use yxorP\app\lib\parser\resolvedDomainNameInterface;
+use yxorP\app\lib\parser\topLevelDomainInterface;
+
+class publicSuffixTarget implements resolvedDomainNameInterface
 {
-    public function registrableDomain()
+    public function suffix(): topLevelDomainInterface
     {
-        return ENV_DEFAULT_TARGET;
+        return new topLevelDomain('com');
+    }
+
+    public function secondLevelDomain(): nameInterface
+    {
+        return new name('example');
+    }
+
+    public function registrableDomain(): nameInterface
+    {
+        return new name('example');
+    }
+
+    public function subDomain(): nameInterface
+    {
+        return new name('example');
+    }
+
+    public function withSubDomain(nameInterface $subDomain): self
+    {
+        return new publicSuffixHost($subDomain->toString());
+    }
+
+    public function toString(): string
+    {
+        // TODO: Implement toString() method.
+    }
+
+    public function withSecondLevelDomain(nameInterface $label): self
+    {
+        return new publicSuffixHost($label->toString());
+    }
+
+    public function withSuffix(topLevelDomainInterface $suffix): self
+    {
+        return new publicSuffixHost($suffix->toString());
+    }
+
+    public function domain(): nameInterface
+    {
+        // TODO: Implement domain() method.
+    }
+
+    public function value(): ?string
+    {
+        // TODO: Implement value() method.
+    }
+
+    public function jsonSerialize(): ?string
+    {
+        // TODO: Implement jsonSerialize() method.
+    }
+
+    public function count(): int
+    {
+        // TODO: Implement count() method.
+    }
+
+    public function toAscii(): hostInterface
+    {
+        // TODO: Implement toAscii() method.
+    }
+
+    public function toUnicode(): hostInterface
+    {
+        // TODO: Implement toUnicode() method.
     }
 }
-
