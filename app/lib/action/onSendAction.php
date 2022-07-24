be22ce98affc0e05ee9e37c9f7cf964f49225c00
don Pablo <don@obeyi.com>
2022-07-24 12:36:11 +0200

    update

--- app/lib/action/onSendAction.php
+++ app/lib/action/onSendAction.php
@@ -22,8 +22,8 @@ class onSendAction extends wrapper
         $getUrl = yP::store(VAR_REQUEST)->getUrl();
         $getMethod = yP::store(VAR_REQUEST)->getMethod();
         $getMethod = yP::store(VAR_GUZZLE)->request($getMethod);
-
+        echo 1;
         yP::store_tmp(VAR_RESPONSE, $getMethod, $getUrl, json_decode(json_encode($_REQUEST), true, 512, JSON_THROW_ON_ERROR));
-        
+
     }
 }
