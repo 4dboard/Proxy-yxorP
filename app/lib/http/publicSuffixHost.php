67c2347ea16d39f3c2c92abe81fad316d5ce89d9
don Pablo <don@obeyi.com>
2022-07-24 09:22:42 +0200

    update

--- app/lib/http/publicSuffixHost.php
+++ app/lib/http/publicSuffixHost.php
@@ -1,9 +1,43 @@
 <?php namespace yxorP\app\lib\http;
 
-class publicSuffixHost
+use yxorP\app\lib\parser\nameInterface;
+use yxorP\app\lib\parser\resolvedDomainNameInterface;
+use yxorP\app\lib\parser\topLevelDomainInterface;
+
+class publicSuffixHost implements resolvedDomainNameInterface
 {
-    public function registrableDomain()
+    public function suffix(): topLevelDomainInterface
     {
-        return YXORP_HTTP_HOST;
+        return new topLevelDomain('com');
+    }
+
+    public function secondLevelDomain(): nameInterface
+    {
+        return new name('example');
+    }
+
+    public function registrableDomain(): nameInterface
+    {
+        return new name('example');
+    }
+
+    public function subDomain(): nameInterface
+    {
+        return new name('example');
+    }
+
+    public function withSubDomain(nameInterface $subDomain)
+    {
+        return new publicSuffixHost($subDomain->toString());
+    }
+
+    public function withSecondLevelDomain(nameInterface $label): self
+    {
+        return new publicSuffixHost($label->toString());
+    }
+
+    public function withSuffix(topLevelDomainInterface $suffix): self
+    {
+        return new publicSuffixHost($suffix->toString());
     }
 }
