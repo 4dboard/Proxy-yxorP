b1ea254e786a1a9e11ede69e21010117a62a203a
don Pablo <don@obeyi.com>
2022-07-24 09:19:31 +0200

    update

---
+++ app/lib/http/publicSuffixTarget.php
@@ -0,0 +1,18 @@
+<?php namespace yxorP\app\lib\http;
+
+class defaults
+{
+    public function registrableDomain()
+    {
+        return ENV_DEFAULT_TARGET;
+    }
+}
+
+
+class defaults_target
+{
+    public function registrableDomain()
+    {
+        return ENV_DEFAULT_TARGET;
+    }
+}
