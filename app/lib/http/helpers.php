9ec1378bb58de0afc38a20d5189588dd20a8b888
don Pablo <don@obeyi.com>
2022-07-24 08:55:04 +0200

    update

--- app/lib/http/helpers.php
+++ app/lib/http/helpers.php
@@ -254,7 +254,7 @@ class helpers
         yP::store(TARGET_DOMAIN, self::target_suffix());
 
         /* Checking if the subdomain is set, if it is, it will use that, if not, it will use the domain.  Setting the `TARGET_DOMAIN` variable to the result of the `extractDomain` method. */
-        yP::store(YXORP_TARGET_DOMAIN, null, 'yxorP\app\lib\http\helpers::target_domain');
+        yP::store(YXORP_TARGET_DOMAIN, self::target_domain(), );
 
         /* Defining a constant. */
         define('VAR_FETCH', VAR_HTTPS . YXORP_SITE_SUB_DOMAIN . yP::store(YXORP_TARGET_DOMAIN));
