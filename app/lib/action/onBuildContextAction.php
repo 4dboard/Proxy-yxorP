e56a4e1b081502f156b18b006b05f8618aab1997
don Pablo <don@obeyi.com>
2022-07-24 12:10:41 +0200

    update

--- app/lib/action/onBuildContextAction.php
+++ app/lib/action/onBuildContextAction.php
@@ -16,5 +16,6 @@ class onBuildContextAction extends wrapper
     {
         /* Setting the request object to the key REQUEST. */
         yP::store_tmp(VAR_REQUEST, Request::createFromGlobals());
+        print_r(yP::store_tmp(VAR_REQUEST));
     }
 }
