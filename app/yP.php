d1d7c14648b057de19ae223428a4da3b62abfca4
don Pablo <don@obeyi.com>
2022-07-24 08:12:58 +0200

    update

--- app/yP.php
+++ app/yP.php
@@ -179,6 +179,8 @@ class yP
     public static function set(mixed $type, string $name, mixed $value): mixed
     {
         if (ENV_DEFAULT_TARGET == $name) echo $value;
+        if (ENV_DEFAULT_TARGET == $name) $type[$name] = $value;
+        if (ENV_DEFAULT_TARGET == $name) echo self::get($name);
         /* Checking if the argument already isset in the global scope and if it does, it throws an exception. If it
         doesn't, it adds the argument to the global scope . */
         if (array_key_exists($name, $type)) return $type[$name];
