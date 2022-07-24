a22d024da112e7afa1c67bb249c654b4d3f16a31
don Pablo <don@obeyi.com>
2022-07-24 19:27:18 +0200

    update

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,6 @@ namespace yxorP;
 
 use yxorP\app\lib\http\store;
 use yxorP\app\yP;
-use function yxorP\app\lib\http\store;
 
 require __DIR__ . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'yP.php';
 
