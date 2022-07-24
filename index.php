4f7287cc82f48d4df073585f9630ec522e8293ec
don Pablo <don@obeyi.com>
2022-07-25 01:51:52 +0200

    update

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 foreach (['Access-Control-Allow-Origin: "*" always', 'Access-Control-Allow-Methods: "POST,GET,OPTIONS" always', 'Access-Control-Allow-Credentials: true always', 'Access-Control-Allow-Headers: "Origin,Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With,Access-Control-Allow-Credentials" always', 'Cache-Control: "max-age=36000000" always'] as $head) header($head);
 $file = ($dir = __DIR__ . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'tmp' . DIRECTORY_SEPARATOR . ($key = urlencode($_SERVER['HTTP_HOST'])) . DIRECTORY_SEPARATOR) . rtrim(strtr(base64_encode($_SERVER['REQUEST_URI']), '+/=', '._-')) . '.tmp';
 session_name($_SERVER['HTTP_HOST']) . session_start();
-store::print($key);
+if (isset($_SERVER[$key])) exit(die($_SERVER[$key]));
 
 if (isset($_GET["CLECHE"])) {
     foreach (glob($dir . '*') as $file) unlink($file);
