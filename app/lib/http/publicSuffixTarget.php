4300e99e7e80f905d5adab314bda01ef9a9976b1
don Pablo <don@obeyi.com>
2022-07-24 09:56:50 +0200

    update

--- app/lib/http/publicSuffixTarget.php
+++ app/lib/http/publicSuffixTarget.php
@@ -1,11 +1,12 @@
 <?php namespace yxorP\app\lib\http;
 
+use yxorP\app\lib\parser\nameInterface;
 use yxorP\app\yP;
 
 class publicSuffixTarget
 {
 
-    public function registrableDomain(): string
+    public function registrableDomain(): nameInterface
     {
         return yP::store(ENV_DEFAULT_HOST);
     }
