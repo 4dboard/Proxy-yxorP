d365df5bbc6e77b57c42e6bbb125255393e397fb
don Pablo <don@obeyi.com>
2022-07-24 13:19:25 +0200

    update

--- app/lib/action/onSendAction.php
+++ app/lib/action/onSendAction.php
@@ -2,7 +2,6 @@
 /* Importing the wrapper class from the yxorP\app\lib\http namespace. */
 
 
-use GuzzleHttp\Client;
 use yxorP\app\constants;
 use yxorP\app\lib\http\wrapper;
 use yxorP\app\yP;
@@ -20,9 +19,7 @@ class onSendAction extends wrapper
         /* Creating a new `yxorP\app\lib\proxy\Client` object, and then it is sending a request to the `\yxorP\app\yP::store(YXORP_FETCH)` URL,
         with the `\yxorP\app\yP::store(YXORP_REQUEST)->getMethod()` method, and the `$_REQUEST` array as the body.   Setting the content of the response to the content of the request. */
 
-        //yP::store_tmp(VAR_RESPONSE,);
-        echo 1;
-        print_r((new Client())->request('GET', yP::store(VAR_REQUEST)->getUrl(), json_decode(json_encode(new GuzzleHttp\Psr7\Request(yP::store(VAR_REQUEST)->getMethod(), yP::store(VAR_REQUEST)->getUrl())), true, 512, JSON_THROW_ON_ERROR)));
-        echo 2;
+        yP::store_tmp(VAR_RESPONSE, yP::store(VAR_GUZZLE)->request(yP::store(VAR_REQUEST)->getMethod(), yP::store(VAR_REQUEST)->getUrl(), json_decode(json_encode(new GuzzleHttp\Psr7\Request(yP::store(VAR_REQUEST)->getMethod(), yP::store(VAR_REQUEST)->getUrl())), true, 512, JSON_THROW_ON_ERROR)));
+
     }
 }
