74d4375da6e293ed12fd866987ba791c85938cd8
don Pablo <don@obeyi.com>
2022-07-24 09:34:57 +0200

    update

--- app/lib/http/publicSuffixHost.php
+++ app/lib/http/publicSuffixHost.php
@@ -17,10 +17,6 @@ class publicSuffixHost
         return new name(explode('.', yP::store(ENV_DEFAULT_HOST))[0]);
     }
 
-    public function registrableDomain(): nameInterface
-    {
-        return new name(explode('.', yP::store(ENV_DEFAULT_HOST))[0]);
-    }
 
     public function subDomain(): nameInterface
     {
