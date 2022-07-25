522769852903a7955155a73dd96f532196787af8
don Pablo <don@obeyi.com>
2022-07-25 05:44:49 +0200

    update

--- app/lib/http/cache.php
+++ app/lib/http/cache.php
@@ -26,21 +26,24 @@ class cache
     }
 
     /* A PHPDoc annotation that is used to tell the IDE that the function returns an array with the keys `key` and `path`. */
-
     #[ArrayShape(['key' => "null|\yxorP\app\lib\http\string", 'path' => "string"])] private static function gen(?string $key): array
     {
         /* Returning an array with the keys `key` and `path`. */
         return ['key' => $key ?: CACHE_KEY, 'path' => ($key) ? PATH_TMP_DIR . $key . FILE_TMP : PATH_TMP_FILE];
     }
 
+    /* It's writing the content to the cache file. */
     #[NoReturn] public static function set($content, ?string $key = null): void
     {
+        /* It's printing the content to the screen. */
         echo $content;
+        /* It's checking if the `tmp` directory exists, and if it doesn't, it's creating it. */
         self::folder();
         /* Writing the content to the cache file. */
         exit(die(file_put_contents(self::gen($key)['path'], '<?php header("Content-type: ' . MIME . '"); exit(die( ' . var_export((minify::createDefault())->process(MIME === VAR_TEXT_HTML ? helpers::replace($content) : $content), true) . '));')));
     }
 
+    /* It's checking if the `tmp` directory exists, and if it doesn't, it's creating it. */
     #[NoReturn] private static function folder(): void
     {
         /* It's checking if the `tmp` directory exists, and if it doesn't, it's creating it. */
