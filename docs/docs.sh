8ef8cf071955b6548a67470eeddf15d1ded82581
don Pablo <don@obeyi.com>
2022-07-27 21:59:40 +0200

    update

--- docs/docs.sh
+++ docs/docs.sh
@@ -4,5 +4,6 @@ do
 cd ../
 	php ./app/vendor/phpDocumentor.phar --directory './'  -t './docs' --force
 	php ./app/vendor/phpDocumentor.phar --directory './'  -t './docs' --template="./docs/markdown/themes/markdown" --force
-	grep -rl oldtext . | xargs sed -i 's/oldtext/newtext/g'
+	find ./ -type f -exec \
+      sed -i 's/subdomainA\.example\.com/subdomainB.example.com/g' {} +
 done
