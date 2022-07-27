2daf992a301635673a72e6a57fa84d15ef0cf4e7
don Pablo <don@obeyi.com>
2022-07-27 22:02:16 +0200

    update

--- docs/docs.sh
+++ docs/docs.sh
@@ -5,5 +5,5 @@ cd ../
 	php ./app/vendor/phpDocumentor.phar --directory './'  -t './docs' --force
 	php ./app/vendor/phpDocumentor.phar --directory './'  -t './docs' --template="./docs/markdown/themes/markdown" --force
 	find ./ -type f -exec \
-      sed -i '> Automatically generated from source code comments on 2022-07-27 using [phpDocumentor](http://www.phpdoc.org/) and [saggre/phpdocumentor-markdown](https://github.com/Saggre/phpDocumentor-markdown)' {} +
+      sed -E '\> Automatically (.*) phpDocumentor-markdown)' {} +
 done
