f5399ae24d6984c875605a16d143715f793a1357
don Pablo <don@obeyi.com>
2022-07-27 22:15:52 +0200

    update

--- docs/docs.sh
+++ docs/docs.sh
@@ -4,5 +4,5 @@ do
 cd ../
 	php ./app/vendor/phpDocumentor.phar --directory './'  -t './docs' --force
 	php ./app/vendor/phpDocumentor.phar --directory './'  -t './docs' --template="./docs/markdown/themes/markdown" --force
-	find ./ -type f -exec sed '\> Automatically[.*]markdown/i' {} +
+	find ./ -type f -exec sed '\> Automatically[.*]markdown/g' {} +
 done
