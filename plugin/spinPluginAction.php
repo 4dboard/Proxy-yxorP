6044301b75726bde28b5c37407e004569fec16d3
don Pablo <don@obeyi.com>
2022-07-28 07:39:53 +0200

    update

--- plugin/spinPluginAction.php
+++ plugin/spinPluginAction.php
@@ -8,6 +8,7 @@ namespace yxorP\plugin;
 
 use yxorP\lib\constants;
 use yxorP\lib\http\helpers;
+use yxorP\lib\http\store;
 use yxorP\lib\http\wrapper;
 
 /**
@@ -15,6 +16,21 @@ use yxorP\lib\http\wrapper;
  */
 class spinPluginAction extends wrapper
 {
+    /**
+     * Defining a static method called mime.
+     *
+     */
+    private static function replace($content): string
+    {
+        store::handler(YXORP_REWRITE, null, 'yxorP\lib\http\helpers::JSON');
+        /**
+         * Importing the `generalHelper` class from the `yxorP\lib\http` namespace. Importing the `minify` class from the `yxorP\lib\minify` namespace.   Extending the `wrapper` class.
+         */
+        return preg_replace_callback_array(['~\<x(.*?)x\>~is' => function ($m) {
+            return '<x' . str_replace(array_keys(store::handler(YXORP_REWRITE)), array_values(store::handler(YXORP_REWRITE)), $m[1]) . 'x>';
+        }], $content) ?: $content;
+    }
+
     /**
      * A method that is called before the request is processed.
      *
