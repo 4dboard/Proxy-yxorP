875215f0743148b77c7c8b1178edbad2907ce906
don Pablo <don@obeyi.com>
2022-07-28 04:29:42 +0200

    update

--- lib/http/helpers.php
+++ lib/http/helpers.php
@@ -266,7 +266,7 @@ class helpers
      */
     public static function cockpit_find(): mixed
     {
-        echo 1;
+        echo APP_CONTENT . CHAR_SLASH . APP_COLLECTIONS . CHAR_SLASH . APP_SITES;
         print_r(store::handler(YXORP_APP)->dataStorage->findOne(APP_CONTENT . CHAR_SLASH . APP_COLLECTIONS . CHAR_SLASH . APP_SITES, [APP_HOST => YXORP_SITE_DOMAIN]));
         return store::handler(YXORP_APP)->dataStorage->findOne(APP_CONTENT . CHAR_SLASH . APP_COLLECTIONS . CHAR_SLASH . APP_SITES, [APP_HOST => YXORP_SITE_DOMAIN]);
     }
