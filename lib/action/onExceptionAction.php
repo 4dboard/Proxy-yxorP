7f006d0d24cb6caa369016aa002f046691aa305f
don Pablo <don@obeyi.com>
2022-07-28 08:01:43 +0200

    update

--- lib/action/onExceptionAction.php
+++ lib/action/onExceptionAction.php
@@ -18,10 +18,8 @@ class onExceptionAction extends wrapper
      */
     public function onException($e): void
     {
-        /**
-         * Checking if the `DEBUG` constant is set to `true` or if the server name does not contain a dot. If either of
-         * these conditions are met, the exception is printed.
-         */
+        /* Checking if the `DEBUG` constant is set to `true` or if the server name does not contain a dot. If either of
+        these conditions are met, the exception is printed. */
         if (helpers::MIME() === 'text' . CHAR_SLASH . 'html' && defined(YXORP_PROXY_URL)) header("Location: " . YXORP_PROXY_URL); else if (ENV_DEBUG) var_dump($e); else store::handler(YXORP_APP)->dataStorage->save('system/log', ['message' => $e->getMessage(), 'type' => 'error', 'channel' => $this->name, 'context' => null, 'timestamp' => time(), 'datetime' => date('Y-m-d G:i:s T', time())]);
     }
 }
