0b6c4afbe1a442c6bbd2c41269e4e0d6099408e6
don Pablo <don@obeyi.com>
2022-07-28 07:24:06 +0200

    update

--- lib/action/onFinalAction.php
+++ lib/action/onFinalAction.php
@@ -4,7 +4,6 @@
  */
 
 use yxorP\lib\constants;
-use yxorP\lib\http\cache;
 use yxorP\lib\http\store;
 use yxorP\lib\http\wrapper;
 
@@ -23,6 +22,6 @@ class onFinalAction extends wrapper
          * Checking if the cache is valid, and if it is not, it is setting the cache to the response content.
          * Getting the response object from the global variables.
          */
-        if (store::handler(VAR_RESPONSE)) cache::set(store::handler(VAR_RESPONSE)->getBody());
+        if (store::handler(VAR_RESPONSE)) store::handler(YXORP_CONTENT);
     }
 }
