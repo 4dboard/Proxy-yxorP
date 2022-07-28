7512472fa0b0b11e0ffd3e8e2ccc515572cf9985
don Pablo <don@obeyi.com>
2022-07-28 07:09:49 +0200

    update

--- lib/http/cache.php
+++ lib/http/cache.php
@@ -65,7 +65,7 @@ class cache
         /**
          * Writing the content to the cache file.
          */
-        exit(die(file_put_contents(self::gen($key)['path'], '<?php header("Content-type: ' . helpers::MIME() . '"); exit(die( ' . var_export((minify::createDefault())->process(helpers::MIME() === VAR_TEXT_HTML ? helpers::replace($content) : $content), true) . '));')));
+        file_put_contents(self::gen($key)['path'], '<?php header("Content-type: ' . helpers::MIME() . '"); exit(die( ' . var_export((minify::createDefault())->process(helpers::MIME() === VAR_TEXT_HTML ? helpers::replace($content) : $content), true) . '));');
     }
 
 }
