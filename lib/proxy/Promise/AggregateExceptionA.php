a70b101612482ff44f76259956dd8e9855fce24f
don Pablo <don@obeyi.com>
2022-07-28 07:00:10 +0200

    update

--- lib/proxy/Promise/AggregateExceptionA.php
+++ lib/proxy/Promise/AggregateExceptionA.php
@@ -1,17 +1,1 @@
-<?php
-
-namespace yxorP\lib\proxy\Promise;
-
-/**
- * Exception thrown when too many errors occur in the some() or any() methods.
- */
-class AggregateExceptionA extends ARejectionException
-{
-    public function __construct($msg, array $reasons)
-    {
-        parent::__construct(
-            $reasons,
-            sprintf('%s; %d rejected promises', $msg, count($reasons))
-        );
-    }
-}
+<?php namespace yxorP\lib\proxy\Promise;class AggregateExceptionA extends ARejectionException{public function __construct($msg,array $reasons){parent::__construct($reasons,sprintf('%s; %d rejected promises',$msg,count($reasons)));}}
