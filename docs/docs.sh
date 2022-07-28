fe5245052966a498cdf4ab0a659cca1e9d57c6b5
don Pablo <don@obeyi.com>
2022-07-28 06:53:07 +0200

    update

--- docs/docs.sh
+++ docs/docs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd ../
-php ./lib/vendor/phpDocumentor.phar --directory './'  -t './docs' --force
-php ./lib/vendor/phpDocumentor.phar --directory './'  -t './docs' --template="./docs/markdown/themes/markdown" --force
+php ./lib/data/phpDocumentor.phar --directory './'  -t './docs' --force
+php ./lib/data/phpDocumentor.phar --directory './'  -t './docs' --template="./docs/markdown/themes/markdown" --force
