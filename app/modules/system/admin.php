9aec4052c8263740d9b4425a1a628e9d24b49dc1
don Pablo <don@obeyi.com>
2022-08-01 21:48:48 +0200

    update

--- app/modules/system/admin.php
+++ app/modules/system/admin.php
@@ -1,20 +1,24 @@
 <?php
 
-// Register Helpers
+/* Adding a helper to the system. */
 $this->helpers['settings'] = 'System\\Helper\\settings';
-
-// Register routes
+/* Binding the class `System\Controller\api` to the route `/system/api`. */
+/* Binding the class `System\Controller\locales` to the route `/system/locales`. */
 $this->bindClass('System\\Controller\\api', '/system/api');
+/* Binding the class `System\Controller\logs` to the route `/system/logs`. */
 $this->bindClass('System\\Controller\\locales', '/system/locales');
+/* Binding the class `System\Controller\Users\roles` to the route `/system/users/roles`. */
 $this->bindClass('System\\Controller\\logs', '/system/logs');
+/* Binding the class `System\Controller\users` to the route `/system/users`. */
 $this->bindClass('System\\Controller\\Users\\roles', '/system/users/roles');
+/* Binding the class `System\Controller\users` to the route `/system/users`. */
 $this->bindClass('System\\Controller\\users', '/system/users');
+/* Binding the class `System\Controller\utils` to the route `/system/utils`. */
 $this->bindClass('System\\Controller\\utils', '/system/utils');
+/* Binding the class `System\Controller\settings` to the route `/system`. */
 $this->bindClass('System\\Controller\\settings', '/system');
 
 $this->helper('menus')->addLink('modules', ['label' => 'Api', 'icon' => 'system:assets/icons/api.svg', 'route' => '/system/api', 'active' => false]);
-
-// events
 
 $this->on('app.permissions.collect', function (ArrayObject $permissions) {
 
