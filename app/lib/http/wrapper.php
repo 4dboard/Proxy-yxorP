74b2f89dbe22cac54503a48c0c5abb53833cac1c
don Pablo <don@obeyi.com>
2022-08-01 16:47:11 +0200

    update

--- app/lib/http/wrapper.php
+++ app/lib/http/wrapper.php
@@ -53,160 +53,173 @@ abstract class wrapper
         /**
          * Used to catch exceptions.
          */
-        try {
+        //try {
 
-            /* Used to filter the events. */
-            if ($this->url_pattern && helpers::starts_with($this->url_pattern, CHAR_SLASH) && preg_match($this->url_pattern, YXORP_REQUEST_URI_FULL) !== 1 && stripos(YXORP_REQUEST_URI_FULL, $this->url_pattern) === 0) return;
+        /* Used to filter the events. */
+        if ($this->url_pattern && helpers::starts_with($this->url_pattern, CHAR_SLASH) && preg_match($this->url_pattern, YXORP_REQUEST_URI_FULL) !== 1 && stripos(YXORP_REQUEST_URI_FULL, $this->url_pattern) === 0) return;
 
-            /**
-             * Used to route the events to the appropriate methods.
-             */
+        /**
+         * Used to route the events to the appropriate methods.
+         */
 
-            switch ($event_name) {
-                case EVENT_BUILD_CACHE: /* Calling the `onCheck` method and then the `onCache` method. */
-                    $this->onCheck()->onCache();
-                    break;
-                case EVENT_BUILD_CONTEXT: /* Calling the `onCheck` method and then the `onContext` method. */
-                    $this->onCheck()->onContext();
-                    break;
-                case EVENT_BUILD_INCLUDES: /* Calling the `onCheck` method and then the `onIncludes` method. */
-                    $this->onCheck()->onIncludes();
-                    break;
-                case EVENT_BUILD_HEADERS: /* Calling the `onCheck` method and then the `onHeaders` method. */
-                    $this->onCheck()->onHeaders();
-                    break;
-                case EVENT_BUILD_REQUEST: /* Calling the `onCheck` method and then the `onRequest` method. */
-                    $this->onCheck()->onRequest();
-                    break;
-                case EVENT_BEFORE_SEND: /* Calling the `onCheck` method and then the `onBeforeSend` method. */
-                    $this->onCheck()->onBeforeSend();
-                    break;
-                case EVENT_SEND: /* Calling the `onCheck` method and then the `onSend` method. */
-                    $this->onCheck()->onSend();
-                    break;
-                case EVENT_SENT: /* Calling the `onCheck` method and then the `onSent` method. */
-                    $this->onCheck()->onSent();
-                    break;
-                case EVENT_WRITE: /* Calling the `onCheck` method and then the `onWrite` method. */
-                    $this->onCheck()->onWrite();
-                    break;
-                case EVENT_COMPLETE: /* Calling the `onComplete` method. */
-                    $this->onComplete();
-                    break;
-                case EVENT_FINAL: /* Calling the `onFinal` method. */
-                    $this->onFinal();
-                    break;
-            }
-        } catch (Exception $e) {
-            /**
-             * Calling the `onException` method.
-             */
-            $this->onException($e);
+        switch ($event_name) {
+            case EVENT_BUILD_CACHE: /* Calling the `onCheck` method and then the `onCache` method. */
+                $this->onCheck()->onCache();
+                break;
+            case EVENT_BUILD_CONTEXT: /* Calling the `onCheck` method and then the `onContext` method. */
+                $this->onCheck()->onContext();
+                break;
+            case EVENT_BUILD_INCLUDES: /* Calling the `onCheck` method and then the `onIncludes` method. */
+                $this->onCheck()->onIncludes();
+                break;
+            case EVENT_BUILD_HEADERS: /* Calling the `onCheck` method and then the `onHeaders` method. */
+                $this->onCheck()->onHeaders();
+                break;
+            case EVENT_BUILD_REQUEST: /* Calling the `onCheck` method and then the `onRequest` method. */
+                $this->onCheck()->onRequest();
+                break;
+            case EVENT_BEFORE_SEND: /* Calling the `onCheck` method and then the `onBeforeSend` method. */
+                $this->onCheck()->onBeforeSend();
+                break;
+            case EVENT_SEND: /* Calling the `onCheck` method and then the `onSend` method. */
+                $this->onCheck()->onSend();
+                break;
+            case EVENT_SENT: /* Calling the `onCheck` method and then the `onSent` method. */
+                $this->onCheck()->onSent();
+                break;
+            case EVENT_WRITE: /* Calling the `onCheck` method and then the `onWrite` method. */
+                $this->onCheck()->onWrite();
+                break;
+            case EVENT_COMPLETE: /* Calling the `onComplete` method. */
+                $this->onComplete();
+                break;
+            case EVENT_FINAL: /* Calling the `onFinal` method. */
+                $this->onFinal();
+                break;
         }
-    }
+        /* } catch (Exception $e) {
+             /**
+              * Calling the `onException` method.
+              */
+        $this->onException($e);
+    }*/
+}
 
-    /**
-     * A method that is called when the event `EVENT_BUILD_CACHE` is triggered.
-     *
-     */
-    public function onCache(): void
-    {
-    }
+/**
+ * A method that is called when the event `EVENT_BUILD_CACHE` is triggered.
+ *
+ */
+public
+function onCache(): void
+{
+}
 
-    /**
-     * Used to check if the event should be executed.
-     *
-     */
-    public function onCheck(): self
-    {
-        return $this;
-    }
+/**
+ * Used to check if the event should be executed.
+ *
+ */
+public
+function onCheck(): self
+{
+    return $this;
+}
 
-    /**
-     * A method that is called when the event `EVENT_BUILD_CONTEXT` is triggered.
-     *
-     */
-    public function onContext(): void
-    {
-    }
+/**
+ * A method that is called when the event `EVENT_BUILD_CONTEXT` is triggered.
+ *
+ */
+public
+function onContext(): void
+{
+}
 
-    /**
-     * A method that is called when the event `EVENT_BUILD_INCLUDES` is triggered.
-     *
-     */
-    public function onIncludes(): void
-    {
-    }
+/**
+ * A method that is called when the event `EVENT_BUILD_INCLUDES` is triggered.
+ *
+ */
+public
+function onIncludes(): void
+{
+}
 
-    /**
-     * A method that is called when the event `EVENT_BUILD_HEADERS` is triggered.
-     *
-     */
-    public function onHeaders(): void
-    {
-    }
+/**
+ * A method that is called when the event `EVENT_BUILD_HEADERS` is triggered.
+ *
+ */
+public
+function onHeaders(): void
+{
+}
 
-    /**
-     * A method that is called when the event `EVENT_BUILD_REQUEST` is triggered.
-     *
-     */
-    public function onRequest(): void
-    {
-    }
+/**
+ * A method that is called when the event `EVENT_BUILD_REQUEST` is triggered.
+ *
+ */
+public
+function onRequest(): void
+{
+}
 
-    /**
-     * Used to build the result.
-     *
-     */
-    public function onBeforeSend(): void
-    {
-    }
+/**
+ * Used to build the result.
+ *
+ */
+public
+function onBeforeSend(): void
+{
+}
 
-    /**
-     * Used to execute code when the request is sent.
-     *
-     */
-    public function onSend(): void
-    {
-    }
+/**
+ * Used to execute code when the request is sent.
+ *
+ */
+public
+function onSend(): void
+{
+}
 
-    /**
-     * Used to execute code before the request is sent.
-     *
-     */
-    public function onSent(): void
-    {
-    }
+/**
+ * Used to execute code before the request is sent.
+ *
+ */
+public
+function onSent(): void
+{
+}
 
-    /**
-     * Used to execute code when the headers are received.
-     *
-     */
-    public function onWrite(): void
-    {
-    }
+/**
+ * Used to execute code when the headers are received.
+ *
+ */
+public
+function onWrite(): void
+{
+}
 
-    /**
-     * Used to execute code when the headers are received.
-     *
-     */
-    public function onComplete(): void
-    {
-    }
+/**
+ * Used to execute code when the headers are received.
+ *
+ */
+public
+function onComplete(): void
+{
+}
 
-    /**
-     * Used to execute code when the request is completed.
-     *
-     */
-    public function onFinal(): void
-    {
-    }
+/**
+ * Used to execute code when the request is completed.
+ *
+ */
+public
+function onFinal(): void
+{
+}
 
-    /**
-     * Used to execute code when an exception is thrown.
-     *
-     */
-    public function onException(Exception $e): void
-    {
-    }
+/**
+ * Used to execute code when an exception is thrown.
+ *
+ */
+public
+function onException(Exception $e): void
+{
+}
 }
