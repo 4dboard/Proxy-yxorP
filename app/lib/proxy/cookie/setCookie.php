98fd88422f4b23c2943e9f12a5152e74ed9d712c
don Pablo <don@obeyi.com>
2022-08-01 17:13:04 +0200

    update

--- app/lib/proxy/cookie/setCookie.php
+++ app/lib/proxy/cookie/setCookie.php
@@ -4,18 +4,6 @@ echo 1;
 
 class sessionCookieJar
 {
-    private static $defaults = ['Name' => null, 'Value' => null, 'Domain' => null, 'Path' => '/', 'Max-Age' => null, 'Expires' => null, 'Secure' => false, 'Discard' => false, 'HttpOnly' => false];
-    private $data;
-
-    public function __construct(array $data = [])
-    {
-        $this->data = array_replace(self::$defaults, $data);
-        if (!$this->getExpires() && $this->getMaxAge()) {
-            $this->setExpires(time() + $this->getMaxAge());
-        } elseif ($this->getExpires() && !is_numeric($this->getExpires())) {
-            $this->setExpires($this->getExpires());
-        }
-    }
 }
 
 echo 1;
