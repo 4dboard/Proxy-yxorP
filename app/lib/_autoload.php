713cd99f8914a6cbff83f18353cd8db52ebc3952
don Pablo <don@obeyi.com>
2022-08-01 22:09:56 +0200

    update

--- app/lib/_autoload.php
+++
@@ -1,5 +0,0 @@
-<?php include(__DIR__ . '/vendor/autoload.php');
-spl_autoload_register(function ($class) {
-    $class_path = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
-    if (file_exists($class_path)) include_once($class_path);
-});
