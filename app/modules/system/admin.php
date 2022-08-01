9dba8a41234e41566b001619955f57941a5fcec1
don Pablo <don@obeyi.com>
2022-08-01 21:50:00 +0200

    update

--- app/modules/system/admin.php
+++ app/modules/system/admin.php
@@ -18,31 +18,42 @@ $this->bindClass('System\\Controller\\utils', '/system/utils');
 /* Binding the class `System\Controller\settings` to the route `/system`. */
 $this->bindClass('System\\Controller\\settings', '/system');
 
+/* Adding a link to the modules menu. */
 $this->helper('menus')->addLink('modules', ['label' => 'Api', 'icon' => 'system:assets/icons/api.svg', 'route' => '/system/api', 'active' => false]);
 
+/* Adding permissions to the system. */
 $this->on('app.permissions.collect', function (ArrayObject $permissions) {
 
+    /* Adding a permission to the system. */
     $permissions['Locales'] = ['app/locales/manage' => 'Manage locales'];
 
+    /* Adding a permission to the system. */
     $permissions['Api & Security'] = ['app/api/manage' => 'Manage Api access'];
 
+    /* Adding a permission to the system. */
     $permissions['Logs'] = ['app/logs' => 'View app logs'];
 
 });
 
+/* Adding a listener to the event `app.user.login` and when the event is triggered it will execute the function. */
 $this->on('app.user.login', function ($user) {
 
+    /* Logging the user login. */
     $this->module('system')->log("User Login: {$user['user']}", type: 'info', context: ['_id' => $user['_id'], 'user' => $user['user'], 'name' => $user['name'], 'email' => $user['email'], 'ip' => $this->getClientIp()]);
 
 });
 
 
+/* Adding a listener to the event `app.search` and when the event is triggered it will execute the function. */
 $this->on('app.search', function ($search, $findings) {
 
+    /* Checking if the user has the permission to manage users. */
     if (!$this->helper('acl')->isAllowed('app/users/manage')) return;
 
+    /* Searching for users that match the search criteria. */
     $users = $this->dataStorage->find('system/users', ['filter' => ['$or' => [['name' => ['$regex' => $search, '$options' => 'i']], ['label' => ['$regex' => $search, '$options' => 'i']]]], 'limit' => 5])->toArray();
 
+    /* Adding the users to the findings array. */
     foreach ($users as $user) $findings[] = ['title' => isset($user['name']) && $user['name'] ? "{$user['name']} ({$user['user']})" : $user['user'], 'route' => $this->routeUrl("/system/users/user/{$user['_id']}"), 'group' => 'Users', 'icon' => 'system:assets/icons/users.svg'];
 
 });
