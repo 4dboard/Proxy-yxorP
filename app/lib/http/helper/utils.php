28aa60b149ec0717028174be66811fdd1726d8b4
don Pablo <don@obeyi.com>
2022-08-02 16:29:21 +0200

    update

---
+++ app/lib/http/helper/utils.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace yxorP\app\lib\lime\helper;
+
+use function preg_match;
+
+/**
+ * Class Utils
+ * @package \yxorP\app\lib\lime\helper
+ */
+class utils extends Helper
+{
+
+    /**
+     * Converts many english words that equate to true or false to boolean.
+     *
+     * Supports 'y', 'n', 'yes', 'no' and a few other variations.
+     *
+     * @param string $string The string to convert to boolean
+     * @param bool $default The value to return if we can't match any
+     *                          yes/no words
+     * @return boolean
+     */
+    public function strToBool(string $string, bool $default = false): bool
+    {
+
+        $yes_words = 'affirmative|all right|aye|indubitably|most assuredly|ok|of course|okay|sure thing|y|yes+|yea|yep|sure|yeah|true|t|on|1|oui|vrai';
+        $no_words = 'no*|no way|nope|nah|na|never|absolutely not|by no means|negative|never ever|false|f|off|0|non|faux';
+
+        if (preg_match('/^(' . $yes_words . ')$/i', $string)) {
+            return true;
+        } else if (preg_match('/^(' . $no_words . ')$/i', $string)) {
+            return false;
+        }
+
+        return $default;
+    }
+
+}
