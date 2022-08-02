07e93955d2f42befbbcae547a7800c8660a92daf
don Pablo <don@obeyi.com>
2022-08-02 23:30:06 +0200

    update

--- app/lib/data/graphQL/Server/StandardServer.php
+++ app/lib/data/graphQL/Server/StandardServer.php
@@ -14,8 +14,6 @@ use yxorP\app\lib\data\graphQL\Error\InvariantViolation;
 use yxorP\app\lib\data\graphQL\Executor\ExecutionResult;
 use yxorP\app\lib\data\graphQL\Executor\Promise\Promise;
 use yxorP\app\lib\data\graphQL\Utils\Utils;
-use yxorP\app\lib\psr\http\message\requestInterface;
-use yxorP\app\lib\psr\http\message\responseInterface;
 use yxorP\app\lib\psr\http\message\streamInterface;
 use function is_array;
 
