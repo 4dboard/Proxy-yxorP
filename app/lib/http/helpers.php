c20fd9934f098bd7a7b6c89b1f28ccf5974a2677
don Pablo <don@obeyi.com>
2022-08-02 19:22:27 +0200

    update

--- app/lib/http/helpers.php
+++ app/lib/http/helpers.php
@@ -179,6 +179,45 @@ class helpers
         store::handler(($name . EXT_ENV), $value);
     }
 
+
+    public static function envLoad(string $dir = '.'): bool
+    {
+        $config = is_file($dir) ? $dir : "{DIR_ROOT}/.env";
+        if (file_exists($config)) {
+            $vars = self::envParse(file_get_contents($config));
+            foreach ($vars as $key => $value) {
+                $_ENV[$key] = $value;
+                putenv("{$key}={$value}");
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public static function envParse(string $str, bool $expand = true): array
+    {
+        $lines = explode("\n", $str);
+        $vars = [];
+        foreach ($lines as &$line) {
+            $line = trim($line);
+            if (!$line) continue;
+            if ($line[0] === '#') continue;
+            if (!strpos($line, '=')) continue;
+            list($name, $value) = explode('=', $line, 2);
+            $value = trim($value, '"\' ');
+            $name = trim($name);
+            $vars[$name] = $value;
+        }
+        if ($expand) {
+            $envs = array_merge(getenv(), $vars);
+            foreach ($envs as $key => $value) {
+                $str = str_replace('${' . $key . '}', $value, $str);
+            }
+            $vars = self::envParse($str, false);
+        }
+        return $vars;
+    }
+
     public static function migrate(string $src, string $dst): void
     {
         $root = opendir($src);
