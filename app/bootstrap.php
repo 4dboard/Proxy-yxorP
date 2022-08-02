e9e8551182fba2b1231cd5e81651e8777414d089
don Pablo <don@obeyi.com>
2022-08-02 02:13:05 +0200

    update

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -18,7 +18,7 @@ spl_autoload_register(function ($class) {
     if (file_exists($class_path)) include_once($class_path);
 });
 
-yxorPapp\lib/yP::autoLoader(__DIR__ . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR);
+yxorPapp\lib / yP::autoLoader(__DIR__ . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR);
 
 class App
 {
