cf0d90d9196b4c20042c553e5540b15075d9a3d5
don Pablo <don@obeyi.com>
2022-08-02 15:21:13 +0200

    update

--- app/yP.php
+++ app/yP.php
@@ -114,7 +114,7 @@ class yP
         /**
          * Loading all the PHP files in the directory.
          */
-        foreach (glob("$root/*") as $path) if (is_dir($path)) self::autoLoader($path); else if (str_contains($path, 'Interface') && str_contains($path, EXT_PHP)) require_once $path; else $classes[] = $path;
+        foreach (glob("$root/*") as $path) if (is_dir($path)) self::autoLoader($path); else if ((str_contains($path, 'Interface') || str_contains($path, 'Interface') )&& str_contains($path, EXT_PHP)) require_once $path; else $classes[] = $path;
         /**
          * Loading all the classes in the classes folder.
          */
