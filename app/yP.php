a783e30eaa43401cc063ac8099d21fe95ec8033b
don Pablo <don@obeyi.com>
2022-08-06 07:41:40 +0200

    update

--- app/yP.php
+++ app/yP.php
@@ -52,8 +52,8 @@ class yP
          * Defining the root directory of the website.
          */
         define('DIR_ROOT', $root . DIRECTORY_SEPARATOR);
-        define('PATH_SITE_BOOTSTRAP', DIR_ROOT . DIR_APP . DIR_STORAGE . DIR_TMP . FILE_SITE_BOOTSTRAP);
-        define('PATH_COOKIE_JAR', DIR_ROOT . DIR_APP . DIR_LIB . DIR_DATA . FILE_COOKIE_JAR);
+        define('PATH_SITE_BOOTSTRAP', DIR_ROOT . DIR_APP .  FILE_SITE_BOOTSTRAP);
+        define('PATH_COOKIE_JAR', DIR_ROOT . DIR_APP . DIR_STORAGE . DIR_TMP . FILE_COOKIE_JAR);
         define('PATH_DIR_APP', DIR_ROOT . DIR_APP);
         define('PATH_SITE_LOCAL', DIR_ROOT . DIR_APP . DIR_LIB . DIR_INSTALL . DIR_APP);
         define('PATH_SITE_INDEX', DIR_ROOT . DIR_APP . FILE_INDEX);
