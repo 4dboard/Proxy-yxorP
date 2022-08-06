6e2f323d287380c7fe74ecf2a66ade304188a603
don Pablo <don@obeyi.com>
2022-08-06 08:11:16 +0200

    update

--- app/lib/action/onWriteAction.php
+++ app/lib/action/onWriteAction.php
@@ -42,25 +42,25 @@ class onWriteAction extends wrapper
             /* Adding a CSS rule to the `$css` array. */
             if (store::handler(TARGET_HEAD)) $css[] = ['header' => 'display:none'];
             /* Checking if the `TARGET_BG_COLOR` variable is set and if it is, it is adding a CSS rule to the `$css` array. */
-            if (store::handler(TARGET_BG_COLOR)) $css[] = ['body' => 'background-color:' . store::handler(TARGET_BG_COLOR)];
+            if (store::handler(TARGET_BG_COLOR)) $css[] = ['body' => 'background-color:' . store::handler(TARGET_BG_COLOR) . ''];
             /* Adding a CSS rule to the `$css` array. */
             if (store::handler(TARGET_BG_IMG)) $css[] = ['body' => 'background-image:url(' . store::handler(TARGET_BG_IMG) . ')'];
 
             /* Replacing the content of the `<nav>` tag with the content of the `TARGET_HTML_MENU` variable. */
             if (store::handler(TARGET_HTML_MENU)) $content = preg_replace('/<nav[^>]*[^>]*>(.*?)<\/nav>/is', '<nav>' . store::handler(TARGET_HTML_MENU) . '</nav>', $content);
-            if (store::handler(TARGET_MENU)) $css[] = ['nav' => 'display:none'];
+            if (!store::handler(TARGET_MENU)) $css[] = ['nav' => 'display:none'];
 
             /* Replacing the content of the `<footer>` tag with the content of the `TARGET_FOOT_HTML` variable. */
             if (store::handler(TARGET_HTML_MENU)) $content = preg_replace('/<footer[^>]*[^>]*>(.*?)<\/footer>/is', '<footer>' . store::handler(TARGET_FOOT_HTML) . '</footer>', $content);
-            if (store::handler(TARGET_FOOT)) $css[] = ['footer' => 'display:none'];
+            if (!store::handler(TARGET_FOOT)) $css[] = ['footer' => 'display:none'];
 
             /* Replacing the content of the `<header>` tag with the content of the `TARGET_HEAD_HTML` variable. */
             if (store::handler(TARGET_HTML_MENU)) $content = preg_replace('/<header[^>]*[^>]*>(.*?)<\/header>/is', '<header>' . store::handler(TARGET_HEAD_HTML) . '</header>', $content);
-            if (store::handler(TARGET_HEAD)) $css[] = ['header' => 'display:none'];
+            if (!store::handler(TARGET_HEAD)) $css[] = ['header' => 'display:none'];
 
 
             /* Creating a CSS string from an array. */
-            if ($css) foreach ($css as $style) $styles .= sprintf("%s{%s;}", array_keys($style)[0], array_values($style)[0]);
+            if ($css) foreach ($css as $style) $styles .= sprintf("%s{%s!important;}", array_keys($style)[0], array_values($style)[0]);
         }
 
         /* Checking if the response is not empty and if the MIME type is HTML, JavaScript, CSS, XML, text or HTML. If it
