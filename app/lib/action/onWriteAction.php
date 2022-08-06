fe9d9c4480a1997d33f843ac1e1320ba9646a823
don Pablo <don@obeyi.com>
2022-08-06 08:17:22 +0200

    update

--- app/lib/action/onWriteAction.php
+++ app/lib/action/onWriteAction.php
@@ -37,7 +37,7 @@ class onWriteAction extends wrapper
             if (store::handler(TARGET_LINK_VISITED)) $css[] = ['a:visited' => 'color:' . store::handler(TARGET_LINK_VISITED)];
             /* Adding a CSS rule to the `$css` array. */
             if (store::handler(TARGET_LINK)) $css[] = ['a' => 'color:' . store::handler(TARGET_LINK)];
-            
+
             /* Checking if the `TARGET_BG_COLOR` variable is set and if it is, it is adding a CSS rule to the `$css` array. */
             if (store::handler(TARGET_BG_COLOR)) $css[] = ['body' => 'background-color:' . store::handler(TARGET_BG_COLOR) . ''];
             /* Adding a CSS rule to the `$css` array. */
