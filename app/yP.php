0d8f4a142383189d96c493c5fa16341f7811024c
don Pablo <don@obeyi.com>
2022-10-04 19:48:42 +0200

    update

--- app/yP.php
+++ app/yP.php
@@ -93,11 +93,11 @@ class yP
          */
         self::loadActions();
 
+        exit('1');
         /**
          * It's looping through all the events in the `init()` function and dispatching them to the `yxorP()` function */
         foreach (store::handler(YXORP_EVENT_LIST) as $event) self::$instance->dispatch($event);
 
-        exit('1');
     }
 
     /**
