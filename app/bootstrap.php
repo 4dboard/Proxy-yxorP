2cdc926ed5911fc6d0aea79e9c8929ae9da0882e
don Pablo <don@obeyi.com>
2022-10-04 20:59:47 +0200

    update

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -17,6 +17,7 @@ include_once(__DIR__.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'vendor'.DIRE
 spl_autoload_register(function($class) {
     $class_path = __DIR__.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.str_replace('\\', DIRECTORY_SEPARATOR, $class).'.php';
     if (file_exists($class_path)) include_once($class_path);
+    if (file_exists($class_path)) print_r($class_path);
 });
 
 // load .env file if exists
