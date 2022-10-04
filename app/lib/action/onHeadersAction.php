8ab9949992d0890c61b0987cf8d8ddd00068d039
don Pablo <don@obeyi.com>
2022-10-04 20:12:28 +0200

    update

--- app/lib/action/onHeadersAction.php
+++ app/lib/action/onHeadersAction.php
@@ -21,6 +21,10 @@ class onHeadersAction extends wrapper
         ini_set('display_startup_errors', ENV_DEBUG ? 1 : 0);
         ini_set('display_errors', ENV_DEBUG ? 1 : 0);
         error_reporting(ENV_DEBUG ? 1 : 0);
+
+        ini_set('display_startup_errors', 1);
+        ini_set('display_errors', 1);
+        error_reporting(1);
         if (helpers::contains($_SERVER['REQUEST_URI'], ['login/', 'register/', 'signup/', 'pay/', 'payout/', 'withdraw/', 'deposit/', 'transfer/', 'deposit/', 'transfer/', 'withdraw/', 'pay/', 'payout/', 'login/', 'register/', 'signup/'])) exit(header("Location: HTTPS://" . YXORP_SITE_DOMAIN, true, 301));
 
     }
