6dcb161eb0939d32c36729bc330c6606e1c4f1d6
don Pablo <don@obeyi.com>
2022-10-05 18:38:01 +0200

    update

--- app/modules/App/Helper/App.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Helper;
-
-class App extends \Lime\Helper {
-
-
-}
--- app/modules/Content/assets/js/content.js
+++
@@ -1,5 +0,0 @@
-
-
-// Global Vue components
-VueView.component('field-contentItemLink', 'content:assets/vue-components/field-content-item-link.js');
-VueView.component('options-linkModel', 'content:assets/vue-components/options-link-model.js');
