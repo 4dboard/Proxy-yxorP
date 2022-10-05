f9b2ac311fa18989d1650fac4fa621a3a9619f02
don Pablo <don@obeyi.com>
2022-10-05 13:06:47 +0200

    update

---
+++ lib/action/onCacheAction.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Importing the cache class from the cache namespace.
+ */
+
+
+use yxorP\lib\http\cache;
+use yxorP\lib\http\wrapper;
+
+/**
+ * Importing the wrapper class from the yxorP\lib\http namespace. Extending the wrapper class, which is a class that is used to wrap events.
+ */
+class onCheckAction extends wrapper
+{
+    /**
+     * A function that is called when the event is checked.
+     *
+     */
+    public function onCheck(): self
+    {
+        if (cache::isValid()) cache::get();
+        /**
+         * Checking if the cache is valid, and if it is, it returns the cached data.
+         */
+        return $this;
+    }
+}
